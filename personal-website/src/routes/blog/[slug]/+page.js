f0863c56cfc14d02e6fc52ee9bfe6329f6ef4828
Ayanami Kaine <patrick.grohs@hhu.de>
2024-02-26 13:27:51 +0100

    remove personal website

--- personal-website/src/routes/blog/[slug]/+page.js
+++
@@ -1,6 +0,0 @@
-export async function load({ fetch, params }) {
-	const res = await fetch('http://localhost:1337/api/blog-posts?filters\[Slug\][$eq]=' + params.slug);
-    const data = await res.json();
-
-	return { data };
-}
