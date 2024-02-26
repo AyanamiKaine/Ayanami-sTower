b81fe5a43b8e4e854098ef8c9c38b72b4790f530
AyanamiKaine <88559194+AyanamiKaine@users.noreply.github.com>
2024-02-26 12:46:23 +0100

    remove duplicated folder and add test files

---
+++ hack_assemblerV2/example_asm_files/max/Max.asm
@@ -0,0 +1,23 @@
+   // D = R0 - R1
+   @R0
+   D=M
+   @R1
+   D=D-M
+   // If (D > 0) goto ITSR0
+   @ITSR0
+   D;JGT
+   // Its R1
+   @R1
+   D=M
+   @R2
+   M=D
+   @END
+   0;JMP
+(ITSR0)
+   @R0             
+   D=M
+   @R2
+   M=D
+(END)
+   @END
+   0;JMP
---
+++ hack_assemblerV2/example_asm_files/rect/Rect.asm
@@ -0,0 +1,33 @@
+   // If (R0 <= 0) goto END else n = R0
+   @R0
+   D=M
+   @END
+   D;JLE 
+   @n
+   M=D
+   // addr = base address of first screen row
+   @SCREEN
+   D=A
+   @addr
+   M=D
+(LOOP)
+   // RAM[addr] = -1
+   @addr
+   A=M
+   M=-1
+   // addr = base address of next screen row
+   @addr
+   D=M
+   @32
+   D=D+A
+   @addr
+   M=D
+   // decrements n and loops
+   @n
+   M=M-1
+   D=M
+   @LOOP
+   D;JGT
+(END)
+   @END
+   0;JMP
