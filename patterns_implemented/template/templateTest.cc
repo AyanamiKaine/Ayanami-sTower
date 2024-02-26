9024847f18d47a1d1f37f73bfaa7f55a7b712d93
Ayanami Kaine <patrick.grohs@hhu.de>
2024-02-26 23:49:07 +0100

    removing test content

--- patterns_implemented/template/templateTest.cc
+++ patterns_implemented/template/templateTest.cc
@@ -1,10 +1,12 @@
 #include "template.h"
 #include <gtest/gtest.h>
 
+// The Test file is irrelevant. 
+
 TEST(TemplateTest, TemplateExample) {
-    CSVProcessor csvProcessor;
-    csvProcessor.processFile("data.csv");
+    //CSVProcessor csvProcessor;
+    //csvProcessor.processFile("data.csv");
     
-    XMLProcessor xmlProcessor;
-    xmlProcessor.processFile("data.xml");
+    //XMLProcessor xmlProcessor;
+    //xmlProcessor.processFile("data.xml");
 }
