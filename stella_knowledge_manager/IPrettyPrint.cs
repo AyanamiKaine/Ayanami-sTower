f3233b68e8eb5402ad0bd6062753671107a89b65
Ayanami Kaine <patrick.grohs@hhu.de>
2024-03-10 21:39:22 +0100

    Creating a Pretty Print Interface, for pretty printing in the console

--- stella_knowledge_manager/IPrettyPrint.cs
+++ stella_knowledge_manager/IPrettyPrint.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace stella_knowledge_manager
 {
+    /// <summary>
+    /// A simple pretty print interface for an object to implement, so it looks nicely displayed in the terminal.
+    /// </summary>
     public interface IPrettyPrint
     {
         public void PrettyPrint();
