b35a0de0089e2730d11973467e7b72d545d10c97
Ayanami Kaine <patrick.grohs@hhu.de>
2024-03-13 15:07:34 +0100

    refactor srs functionality in a separate library

---
+++ SRS/ISRS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AT.SRS
+{
+        /// <summary>
+        /// This interface defined what an item as to implemnt for fields to be used in Spaced Repetition Algorithms
+        /// SRS Algorithms only depend on this interface
+        /// </summary>
+        public interface ISRS
+        {
+            public abstract double EaseFactor { get; set; }
+            public DateTime NextReviewDate { get; set; }
+            public int NumberOfTimeSeen { get; set; }
+        }
+    }
---
+++ SRS/README.md
@@ -0,0 +1,3 @@
+﻿## Introduction
+- The SRS library represents various functions and objects related to the domain of using Spaced Repetition in various forms
+- It profits SRS algorithms and other abstractions related to SRS, etc.
