04f3ce0d5eab264a33b47c89f02a9c0b986c30ca
AyanamiKaine <88559194+AyanamiKaine@users.noreply.github.com>
2024-07-11 13:05:54 +0200

    Implementing the handling of CRUD server requests

--- sl_quiz_database/Server.cs
+++ sl_quiz_database/Server.cs
@@ -12,31 +12,34 @@ namespace sl_quiz_database
     public class Server
     {
 
-        public ResponseSocket QuizDatabase = new("@tcp://localhost:60020");
-
+        public ResponseSocket QuizDatabaseServer = new("@tcp://localhost:60020");
+        private readonly QuizDatabase _quizDatabase = new();
         public void Run()
         {
             Console.WriteLine("Starting Quiz Database");
 
             while (true)
             {
-                string json_request = QuizDatabase.ReceiveFrameString();
+                string json_request = QuizDatabaseServer.ReceiveFrameString();
                 Request request = JsonSerializer.Deserialize<Request>(json_request);
             
                 switch (request.Command)
                 {
                     case Command.Create:
+                        _quizDatabase.CreateQuestion(request.question.QuestionText);
                         break;
 
                     case Command.Update:
+                        _quizDatabase.UpdateQuestion(request.question);
                         break;
 
                     case Command.Delete:
+                        _quizDatabase.DeleteQuestion(request.question);
                         break;
 
                     case Command.Retrieve:
+                        QuizDatabaseServer.SendFrame(_quizDatabase.RetrieveQuestionsAsJson());
                         break;
-
 
                     default:
                         Console.WriteLine("Invalid request");
