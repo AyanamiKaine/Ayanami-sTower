6087ad07477657a052534662a8775465ff5c4f69
AyanamiKaine <patrick.grohs@hhu.de>
2024-07-11 20:02:03 +0200

    Adding quiz database tests

--- sl_quiz_database/Program.cs
+++ sl_quiz_database/Program.cs
@@ -6,12 +6,17 @@ using System.Collections.Generic;
 using static NetMQ.NetMQSelector;
 
 
+// Instead of doing this we could add the ability to parse the argument 'test'
+// to run our unit tests
 #if DEBUG // If we are not in debug mode than instead we run our tests.
     Console.WriteLine("Running in Debug mode");
     Console.WriteLine("Running Tests");
     
     QuestionTests test = new();
     QuestionTests.Run();
+
+    QuizDatabaseTests quizDatabaseTests = new();
+    quizDatabaseTests.Run();
 #else 
     Server server = new();
     server.Run();
