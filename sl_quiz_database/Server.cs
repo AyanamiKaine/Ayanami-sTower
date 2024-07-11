1106235190a2955a0171fe8b9130694e1a974407
AyanamiKaine <88559194+AyanamiKaine@users.noreply.github.com>
2024-07-11 11:06:30 +0200

    Refactoring  from the server class to the logic class

--- sl_quiz_database/Server.cs
+++ sl_quiz_database/Server.cs
@@ -46,55 +46,6 @@ namespace sl_quiz_database
                 }
             
             }
-
-        }
-        public void CreateQuestion(string questionText)
-        {
-            Question question = new(questionText);
-            Questions.Add(question);
-        }
-
-        public List<Question> RetrieveQuestion()
-        {
-            if (Questions.Count == 0)
-            {
-                return null; // Or throw an exception
-            }
-            // For simplicity, always return the first question
-            return Questions; 
-        }
-
-        public void DeleteQuestion(string questionText)
-        {
-            Question questionToRemove = Questions.FirstOrDefault(q => q.QuestionText == questionText);
-            if (questionToRemove != null)
-            {
-                Questions.Remove(questionToRemove);
-            }
-            else
-            {
-                Console.WriteLine($"Question not found: {questionText}");
-            }
-        }
-
-        public void UpdateQuestion(string questionData)
-        {
-            // Deserialize the updated question data (JSON)
-            Question updatedQuestion = JsonSerializer.Deserialize<Question>(questionData);
-
-            // Find the question to update (using ID, text, or other identifier)
-            Question questionToUpdate = Questions.FirstOrDefault(q => q.QuestionText == updatedQuestion.QuestionText);
-
-            if (questionToUpdate != null)
-            {
-                // Update the properties of the existing question
-                questionToUpdate.AnswerOptions = updatedQuestion.AnswerOptions;
-                questionToUpdate.CorrectAnswer = updatedQuestion.CorrectAnswer;
-            }
-            else
-            {
-                Console.WriteLine($"Question not found: {updatedQuestion.QuestionText}");
-            }
         }
     }
 }
