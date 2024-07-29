e633d5903ec78cf4b25023eea2339b86fd85d831
Ayanami Kaine <patrick.grohs@hhu.de>
2024-07-29 11:27:55 +0200

    1.0 Release

--- stella_messaging/README.md
+++ stella_messaging/README.md
@@ -3,5 +3,39 @@
 - A thin wrapper around NNG
 - Higher Level Abstraction for ease of use
 - Easier for other language bindings
+- Based on sending strings
 - Less flexible
 - Less performance
+
+Its expected that each server/client synchronously works on each message one by one but that the servers and clients work parrallel to each other similar how you do it in Elixir.
+
+## Example Client
+
+```C
+int main() {
+    nng_socket sock = create_pair_socket();
+
+    connect_dial(sock, "ipc:///hello_world");
+    send_msg(sock, "Hello World! from a C Client!");
+
+    nng_close(sock);
+    return 0;
+}
+```
+
+## Example Server
+
+```C
+int main() {
+
+    nng_socket sock = create_pair_socket();
+    socket_bind(sock, "ipc:///hello_world");
+
+    while (1) {
+        printf("Received request: %s\n", socket_receive_string_message(sock));
+    }
+
+    nng_close(sock);
+    return 0;
+}
+```
--- stella_messaging/src/stellaMessaging.h
+++ stella_messaging/src/stellaMessaging.h
@@ -5,16 +5,51 @@
 #include <string.h>
 
 #include <nng/nng.h>
+#include <nng/protocol/pair1/pair.h> 
+#include <nng/protocol/bus0/bus.h>
+
 #include <nng/protocol/reqrep0/rep.h>
-#include <nng/protocol/pair0/pair.h> // Or your preferred protocol
+#include <nng/protocol/reqrep0/req.h>
 
-nng_socket create_pair_socket();
-void connect_dial(nng_socket sock, const char* address);
-void listen_for_connections(nng_socket sock, const char *address);
+#include <nng/protocol/pubsub0/pub.h>
+#include <nng/protocol/pubsub0/sub.h>
 
-void close_socket(nng_socket);
+#include <nng/protocol/pipeline0/push.h>
+#include <nng/protocol/pipeline0/pull.h>
 
-void send_string_message(nng_socket sock, char *message);
-char *receive_string_message(nng_socket sock);
+#include <nng/protocol/survey0/respond.h>
+#include <nng/protocol/survey0/survey.h>
+
+// Export macros (as you provided)
+#ifdef BUILDING_STELLA_MESSAGING
+    #define STELLA_API extern __declspec(dllexport)
+#else
+    #define STELLA_API extern __declspec(dllimport)
+#endif
+
+STELLA_API nng_socket create_pair_socket();
+STELLA_API nng_socket create_bus_socket();
+
+STELLA_API nng_socket create_reponse_socket();
+STELLA_API nng_socket create_request_socket();
+
+STELLA_API nng_socket create_sub_socket();
+STELLA_API nng_socket create_pub_socket();
+
+STELLA_API nng_socket create_pull_socket();
+STELLA_API nng_socket create_push_socket();
+
+STELLA_API nng_socket create_respondent_socket();
+STELLA_API nng_socket create_surveyor_socket();
+
+STELLA_API void socket_connect(nng_socket sock, const char* address);
+
+STELLA_API void socket_bind(nng_socket sock, const char *address);
+
+STELLA_API void socket_close(nng_socket);
+
+STELLA_API void socket_send_string_message(nng_socket sock, char *message);
+STELLA_API char* socket_receive_string_message(nng_socket sock);
+STELLA_API void free_received_message(char *message);
 
 #endif
