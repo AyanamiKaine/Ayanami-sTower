cf3be39d10e215fd2cbc62178b155fa65b049541
Ayanami Kaine <patrick.grohs@hhu.de>
2024-07-31 00:06:35 +0200

    init

---
+++ st_again_recall/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using SlAgainRecall;
+// See https://aka.ms/new-console-template for more information
+// Check if the current build configuration is Debug
+#if DEBUG
+// Run your tests only if in Debug mode 
+Stella.Testing.StellaTesting.RunTests();
+#else
+    StellaSRAAgain Server = new();
+    Server.Run();
+#endif
