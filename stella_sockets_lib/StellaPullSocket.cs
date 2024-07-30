f0c0199fb97bbfcd1b0f94b660c876c0ffc3bbfb
Ayanami Kaine <patrick.grohs@hhu.de>
2024-07-30 09:41:56 +0200

    adding comments

--- stella_sockets_lib/StellaPullSocket.cs
+++ stella_sockets_lib/StellaPullSocket.cs
@@ -6,12 +6,24 @@ namespace StellaSockets
     /// 
     /// Ideal For: Building data pipelines, processing streams of data, 
     /// or any scenario where you need a linear flow of information.
+    /// Use Push and Pull Sockets if you want that many clients
+    /// can send messages to a server without expecting a message back
+    /// 
+    /// Pull is the counterpart to Push.
+    /// 
+    /// Pushers distribute messages to pullers. 
+    /// Each message sent by a pusher will be sent to one of its 
+    /// peer pullers, chosen in a round-robin fashion from the 
+    /// set of connected peers available for receiving.
+    /// 
+    /// This property makes this pattern useful in load-balancing 
+    /// scenarios.
     /// </summary>
-    public class StellaPushSocket : StellaSocket
+    public class StellaPullSocket : StellaSocket
     {
-        public StellaPushSocket(string address) : base(SocketType.Push)
+        public StellaPullSocket(string address) : base(SocketType.Pull)
         {
-            Connect(address);
+            Bind(address);
         }
     }
 }
