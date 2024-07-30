5f109aca5068ab1e9995c3978d961d066b19d180
Ayanami Kaine <patrick.grohs@hhu.de>
2024-07-30 11:41:29 +0200

    Adding documentation

--- stella_sockets_lib/StellaRequestSocket.cs
+++ stella_sockets_lib/StellaRequestSocket.cs
@@ -2,6 +2,14 @@ namespace StellaSockets
 {
     public class StellaRequestSocket : StellaSocket
     {
+        /// <summary>
+        ///  In this pattern, a requester sends a message to one replier, who is expected to reply. 
+        ///  The request is resent if no reply arrives, until a reply is received or the request times out.
+        /// 
+        /// This protocol is useful in setting up RPC-like services. 
+        /// It is also "reliable", in that a the requester will keep retrying until a reply is received.
+        /// </summary>
+        /// <param name="address"></param>
         public StellaRequestSocket(string address) : base(SocketType.Request)
         {
             Connect(address);
--- stella_sockets_lib/StellaResponseSocket.cs
+++ stella_sockets_lib/StellaResponseSocket.cs
@@ -2,6 +2,14 @@ namespace StellaSockets
 {
     public class StellaResponseSocket : StellaSocket
     {
+        /// <summary>
+        /// a requester sends a message to one replier, who is expected to reply. 
+        /// The request is resent if no reply arrives, until a reply is received or the request times out.
+        /// 
+        /// This protocol is useful in setting up RPC-like services. 
+        /// It is also reliable, in that a requester will keep retrying until a reply is received.
+        /// </summary>
+        /// <param name="address"></param>
         public StellaResponseSocket(string address) : base(SocketType.Response)
         {
             Bind(address);
