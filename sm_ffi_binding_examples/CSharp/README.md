36732a45cef168ae23f57f771537c5226c9f2377
Ayanami Kaine <patrick.grohs@hhu.de>
2024-08-01 08:19:29 +0200

    init

---
+++ sm_ffi_binding_examples/CSharp/README.md
@@ -0,0 +1,5 @@
+# Stella Messaging C# Bindings
+
+## Stella Sockets (Higher Level API Example)
+
+`StellaSockets.cs` shows an example for an Higher-Level API wrapper, here we use a more idomatic OOP way.
---
+++ sm_ffi_binding_examples/CSharp/StellaMessagingInterop.cs
@@ -0,0 +1,72 @@
+using System.Runtime.InteropServices;
+// NNG socket handle type
+using nng_socket = System.UIntPtr;
+
+partial class StellaMessagingInterop
+{
+    // Function imports from stella_messaging
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial nng_socket create_pair_socket();
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial nng_socket create_bus_socket();
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial nng_socket create_reponse_socket();
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial nng_socket create_request_socket();
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial nng_socket create_sub_socket();
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial nng_socket create_pub_socket();
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial nng_socket create_pull_socket();
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial nng_socket create_push_socket();
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial nng_socket create_respondent_socket();
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial nng_socket create_surveyor_socket();
+
+    [LibraryImport("stella_messaging", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial void socket_connect(nng_socket sock, string address);
+
+    [LibraryImport("stella_messaging", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial void socket_bind(nng_socket sock, string address);
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial void socket_close(nng_socket sock);
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    public static partial void socket_send_string_message(nng_socket sock,
+                                                        [MarshalAs(UnmanagedType.LPStr)] string message);
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    [return: MarshalAs(UnmanagedType.LPStr)]
+    public static partial string socket_receive_string_message(nng_socket sock);
+
+    [LibraryImport("stella_messaging")]
+    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    public static partial void free_received_message(IntPtr message);
+}
