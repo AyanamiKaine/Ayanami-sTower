fd4d94a33fbc28384a4daf86e4ee9524b1127442
AyanamiKaine <88559194+AyanamiKaine@users.noreply.github.com>
2024-08-08 11:15:12 +0200

    Using ColorPrint module to color print test results to the terminal

--- StellaLuaSTL/StellaTesting/StellaTesting.lua
+++ StellaLuaSTL/StellaTesting/StellaTesting.lua
@@ -1,3 +1,5 @@
+local ColorPrint = require "Terminal.StellaColorPrint"
+
 local StellaTesting = {}
 -- Store test results
 StellaTesting.results = {
@@ -12,17 +14,30 @@ function StellaTesting.assertEqual(expected, actual, message)
 
     if expected == actual then
         StellaTesting.results.passed = StellaTesting.results.passed + 1
-        print(string.format("[O] Test Passed: %s", message or ""))
+        ColorPrint.Green(string.format("[O] Test Passed: %s", message or ""))
     else
         StellaTesting.results.failed = StellaTesting.results.failed + 1
-        print(string.format("[X] Test Failed: %s (Expected: %s, Got: %s)", message or "", expected, actual))
+        ColorPrint.Red(string.format("[X] Test Failed: %s (Expected: %s, Got: %s)", message or "", expected, actual))
+    end
+end
+
+-- Assertion Functions (Core Testing Building Blocks)
+function StellaTesting.assertTrue(actual, message)
+    StellaTesting.results.total = StellaTesting.results.total + 1
+
+    if not actual then
+        StellaTesting.results.failed = StellaTesting.results.failed + 1
+        ColorPrint.Red(string.format("[X] Test Failed: %s (Expected: %s, Got: %s)", message or "", expected, actual))
+    else
+        StellaTesting.results.passed = StellaTesting.results.passed + 1
+        ColorPrint.Green(string.format("[O] Test Passed: %s", message or ""))
     end
 end
 
 -- Function for running a test case
 function StellaTesting.runTest(name, func)
     print(string.format("\n--- Running Test: %s ---", name))
-    pcall(func)  -- Run the test function, handling errors
+    pcall(func) -- Run the test function, handling errors
 end
 
 -- Function to display test results summary
