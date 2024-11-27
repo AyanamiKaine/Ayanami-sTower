d2dc575ff1f680c8a88baa5d05018fbdbef048ee
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-27 20:15:50 +0100

    Add ECSSelectingItemsControl module and implement selection change event handling

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSComboBox.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSComboBox.cs
@@ -12,6 +12,12 @@ namespace Avalonia.Flecs.Controls.ECS
                             {
                                 e.Set<ItemsControl>(comboBox);
 
+                                comboBox.SelectionChanged += (object sender, SelectionChangedEventArgs args) =>
+                                {
+                                    e.Set(new Events.SelectionChanged(sender, args));
+                                    e.Emit<Events.SelectionChanged>();
+                                };
+
                             }).OnRemove((Entity e, ref ComboBox comboBox) =>
                             {
                                 var parent = e.Parent();
