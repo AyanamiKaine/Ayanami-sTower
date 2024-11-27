6a7720ee4f6b7b98b14266c31397bdff70174541
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-27 23:15:25 +0100

    Add ECSRadioButton module for radio button functionality in Flecs world

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSRadioButton.cs
@@ -0,0 +1,34 @@
+using Flecs.NET.Core;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Avalonia.Input;
+using Avalonia.Layout;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input.TextInput;
+using Avalonia.Flecs.Controls.ECS.Events;
+namespace Avalonia.Flecs.Controls.ECS
+{
+    public class ECSRadioButton : IFlecsModule
+    {
+        public void InitModule(World world)
+        {
+            world.Module<ECSRadioButton>();
+
+            world.Component<RadioButton>("RadioButton")
+                .OnSet((Entity e, ref RadioButton radioButton) =>
+                {
+
+                    // We set the contentControl component so systems and queries in general can more easily
+                    // access the generic .content property of the button.
+                    // This is good so queries can be more generic and not have to check for every possible control type.
+                    e.Set<ToggleButton>(radioButton);
+
+                })
+                .OnRemove((Entity e, ref RadioButton radioButton) =>
+                {
+                    e.Remove<ToggleButton>();
+                });
+        }
+    }
+}
