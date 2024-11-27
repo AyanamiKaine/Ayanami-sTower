71512c0a4624026071fb00530fa334aeebb5954f
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-27 14:42:56 +0100

    Add event handling for navigation view interactions in ECSNavigationView

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.FluentUI.Controls/ECS/ECSNavigationView.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.FluentUI.Controls/ECS/ECSNavigationView.cs
@@ -14,6 +14,42 @@ namespace Avalonia.Flecs.FluentUI.Controls.ECS
                            {
                                e.Set<ContentControl>(navigationView);
 
+                               navigationView.BackRequested += (object sender, NavigationViewBackRequestedEventArgs args) =>
+                               {
+                                   e.Set(new Events.OnBackRequested(sender, args));
+                                   e.Emit<Events.OnBackRequested>();
+                               };
+
+                               navigationView.ItemCollapsed += (object sender, NavigationViewItemCollapsedEventArgs args) =>
+                               {
+                                   e.Set(new Events.OnItemCollapsed(sender, args));
+                                   e.Emit<Events.OnItemCollapsed>();
+                               };
+
+                               navigationView.ItemExpanding += (object sender, NavigationViewItemExpandingEventArgs args) =>
+                               {
+                                   e.Set(new Events.OnItemExpanding(sender, args));
+                                   e.Emit<Events.OnItemExpanding>();
+                               };
+
+                               navigationView.ItemInvoked += (object sender, NavigationViewItemInvokedEventArgs args) =>
+                               {
+                                   e.Set(new Events.OnItemInvoked(sender, args));
+                                   e.Emit<Events.OnItemInvoked>();
+                               };
+
+                               navigationView.PaneClosing += (NavigationView sender, NavigationViewPaneClosingEventArgs args) =>
+                               {
+                                   e.Set(new Events.OnPaneClosing(sender, args));
+                                   e.Emit<Events.OnPaneClosing>();
+                               };
+
+                               navigationView.DisplayModeChanged += (object sender, NavigationViewDisplayModeChangedEventArgs args) =>
+                               {
+                                   e.Set(new Events.OnDisplayModeChanged(sender, args));
+                                   e.Emit<Events.OnDisplayModeChanged>();
+                               };
+
                                navigationView.SelectionChanged += (object sender, NavigationViewSelectionChangedEventArgs args) =>
                                {
                                    e.Set(new Events.OnSelectionChanged(sender, args));
