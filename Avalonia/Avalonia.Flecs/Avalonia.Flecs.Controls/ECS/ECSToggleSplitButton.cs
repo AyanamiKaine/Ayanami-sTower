63be303ca5ec447c017422f490065ae28b25e2e4
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-27 23:15:37 +0100

    Add ECSToggleSplitButton module for toggle split button functionality in Flecs world

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSToggleSplitButton.cs
@@ -0,0 +1,30 @@
+using Flecs.NET.Core;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Avalonia.Input;
+using Avalonia.Layout;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input.TextInput;
+using Avalonia.Flecs.Controls.ECS.Events;
+namespace Avalonia.Flecs.Controls.ECS
+{
+    public class ECSToggleSplitButton : IFlecsModule
+    {
+        public void InitModule(World world)
+        {
+            world.Module<ECSToggleSplitButton>();
+
+            world.Component<ToggleSplitButton>("ToggleSplitButton")
+                .OnSet((Entity e, ref ToggleSplitButton toggleSplitButton) =>
+                {
+                    e.Set<SplitButton>(toggleSplitButton);
+
+                })
+                .OnRemove((Entity e, ref ToggleSplitButton toggleSplitButton) =>
+                {
+                    e.Remove<SplitButton>();
+                });
+        }
+    }
+}
