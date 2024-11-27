a1ce19666fbc7ab0c9d64b76a8851d4c9fd238c4
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-27 23:15:34 +0100

    Add ECSToggleButton module for toggle button functionality in Flecs world

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSToggleButton.cs
@@ -0,0 +1,51 @@
+using Flecs.NET.Core;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Avalonia.Input;
+using Avalonia.Layout;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input.TextInput;
+using Avalonia.Flecs.Controls.ECS.Events;
+namespace Avalonia.Flecs.Controls.ECS
+{
+    public class ECSToggleButton : IFlecsModule
+    {
+        public void InitModule(World world)
+        {
+            world.Module<ECSToggleButton>();
+
+            world.Component<ToggleButton>("ToggleButton")
+                .OnSet((Entity e, ref ToggleButton toggleButton) =>
+                {
+
+                    // We set the contentControl component so systems and queries in general can more easily
+                    // access the generic .content property of the button.
+                    // This is good so queries can be more generic and not have to check for every possible control type.
+                    e.Set<Button>(toggleButton);
+
+                    toggleButton.Checked += (object sender, RoutedEventArgs args) =>
+                    {
+                        e.Set(new Checked(sender, args));
+                        e.Emit<Checked>();
+                    };
+
+                    toggleButton.Unchecked += (object sender, RoutedEventArgs args) =>
+                    {
+                        e.Set(new Unchecked(sender, args));
+                        e.Emit<Unchecked>();
+                    };
+
+                    toggleButton.Indeterminate += (object sender, RoutedEventArgs args) =>
+                    {
+                        e.Set(new Indeterminate(sender, args));
+                        e.Emit<Indeterminate>();
+                    };
+                })
+                .OnRemove((Entity e, ref ToggleButton toggleButton) =>
+                {
+                    e.Remove<Button>();
+                });
+        }
+    }
+}
