9fe0770b835cf8197f414cb25e950bcdf1061bdd
Ayanami Kaine <ayanami@ayanamis-codex.net>
2024-11-28 09:42:01 +0100

    Add ECSToggleSwitch module to integrate ToggleSwitch functionality with ECS

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSToggleSwitch.cs
@@ -0,0 +1,34 @@
+using Flecs.NET.Core;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Avalonia.Input;
+using Avalonia.Layout;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input.TextInput;
+using Avalonia.Flecs.Controls.ECS.Events;
+namespace Avalonia.Flecs.Controls.ECS
+{
+    public class ECSToggleSwitch : IFlecsModule
+    {
+        public void InitModule(World world)
+        {
+            world.Module<ECSToggleSwitch>();
+
+            world.Component<ToggleSwitch>("ToggleSwitch")
+                .OnSet((Entity e, ref ToggleSwitch toggleSwitch) =>
+                {
+
+                    // We set the contentControl component so systems and queries in general can more easily
+                    // access the generic .content property of the button.
+                    // This is good so queries can be more generic and not have to check for every possible control type.
+                    e.Set<ToggleButton>(toggleSwitch);
+
+                })
+                .OnRemove((Entity e, ref ToggleSwitch toggleSwitch) =>
+                {
+                    e.Remove<ToggleButton>();
+                });
+        }
+    }
+}
