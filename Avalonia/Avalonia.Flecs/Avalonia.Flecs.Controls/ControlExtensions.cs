e3248dd71fa6cba49222458a85a9e0230d2c3fa7
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-29 12:15:49 +0100

    Add ControlExtensions class for helper methods to simplify control property management

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ControlExtensions.cs
@@ -0,0 +1,42 @@
+using Avalonia.Controls;
+
+namespace Avalonia.Flecs.Controls
+{
+    public static class ControlExtensions
+    {
+        /// <summary>
+        /// Helper for setting Row property on a Control component for an entity.
+        /// </summary>
+        public static Control SetRow(this Control control, int value)
+        {
+            Grid.SetRow(control, value);
+            return control;
+        }
+        /// <summary>
+        /// Helper for setting ColumnSpan property on a Control component for an entity.
+        /// </summary>
+        public static Control SetColumnSpan(this Control control, int value)
+        {
+            Grid.SetColumnSpan(control, value);
+            return control;
+        }
+
+        /// <summary>
+        /// Helper for setting Column property on a Control component for an entity.
+        /// </summary>
+        public static Control SetColumn(this Control control, int value)
+        {
+            Grid.SetColumn(control, value);
+            return control;
+        }
+
+        /// <summary>
+        /// Helper for setting RowSpan property on a Control component for an entity.
+        /// </summary>
+        public static Control SetRowSpan(this Control control, int value)
+        {
+            Grid.SetRowSpan(control, value);
+            return control;
+        }
+    }
+}
