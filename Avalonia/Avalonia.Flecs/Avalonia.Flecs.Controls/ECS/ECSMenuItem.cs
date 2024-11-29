f53e2c2885408e4ba1560fb7f6d39d2b6b5e9e39
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-29 12:34:36 +0100

    Improve null safety in ECSMenuItem by using nullable reference types for event handler parameters

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSMenuItem.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSMenuItem.cs
@@ -36,163 +36,163 @@ namespace Avalonia.Flecs.Controls.ECS
                             }).OnRemove((Entity e, ref MenuItem menuItem) =>
                             {
 
-                                menuItem.Click += (object sender, RoutedEventArgs args) =>
+                                menuItem.Click += (object? sender, RoutedEventArgs args) =>
                                 {
                                     e.Set(new Click(sender, args));
                                     e.Emit<Click>();
                                 };
 
-                                menuItem.AttachedToLogicalTree += (object sender, LogicalTreeAttachmentEventArgs args) =>
+                                menuItem.AttachedToLogicalTree += (object? sender, LogicalTreeAttachmentEventArgs args) =>
                                 {
                                     e.Set(new AttachedToLogicalTree(sender, args));
                                     e.Emit<AttachedToLogicalTree>();
                                 };
 
-                                menuItem.AttachedToVisualTree += (object sender, VisualTreeAttachmentEventArgs args) =>
+                                menuItem.AttachedToVisualTree += (object? sender, VisualTreeAttachmentEventArgs args) =>
                                 {
                                     e.Set(new AttachedToVisualTree(sender, args));
                                     e.Emit<AttachedToVisualTree>();
                                 };
 
-                                menuItem.DataContextChanged += (object sender, EventArgs args) =>
+                                menuItem.DataContextChanged += (object? sender, EventArgs args) =>
                                 {
                                     e.Set(new DataContextChanged(sender, args));
                                     e.Emit<DataContextChanged>();
                                 };
 
-                                menuItem.DetachedFromLogicalTree += (object sender, LogicalTreeAttachmentEventArgs args) =>
+                                menuItem.DetachedFromLogicalTree += (object? sender, LogicalTreeAttachmentEventArgs args) =>
                                 {
                                     e.Set(new DetachedFromLogicalTree(sender, args));
                                     e.Emit<DetachedFromLogicalTree>();
                                 };
 
-                                menuItem.DetachedFromVisualTree += (object sender, VisualTreeAttachmentEventArgs args) =>
+                                menuItem.DetachedFromVisualTree += (object? sender, VisualTreeAttachmentEventArgs args) =>
                                 {
                                     e.Set(new DetachedFromVisualTree(sender, args));
                                     e.Emit<DetachedFromVisualTree>();
                                 };
 
-                                menuItem.DoubleTapped += (object sender, TappedEventArgs args) =>
+                                menuItem.DoubleTapped += (object? sender, TappedEventArgs args) =>
                                 {
                                     e.Set(new DoubleTapped(sender, args));
                                     e.Emit<DoubleTapped>();
                                 };
 
-                                menuItem.EffectiveViewportChanged += (object sender, EffectiveViewportChangedEventArgs args) =>
+                                menuItem.EffectiveViewportChanged += (object? sender, EffectiveViewportChangedEventArgs args) =>
                                 {
                                     e.Set(new EffectiveViewportChanged(sender, args));
                                     e.Emit<EffectiveViewportChanged>();
                                 };
 
-                                menuItem.GotFocus += (object sender, GotFocusEventArgs args) =>
+                                menuItem.GotFocus += (object? sender, GotFocusEventArgs args) =>
                                 {
                                     e.Set(new GotFocus(sender, args));
                                     e.Emit<GotFocus>();
                                 };
 
-                                menuItem.Initialized += (object sender, EventArgs args) =>
+                                menuItem.Initialized += (object? sender, EventArgs args) =>
                                 {
                                     e.Set(new Initialized(sender, args));
                                     e.Emit<Initialized>();
                                 };
 
-                                menuItem.KeyDown += (object sender, KeyEventArgs args) =>
+                                menuItem.KeyDown += (object? sender, KeyEventArgs args) =>
                                 {
                                     e.Set(new KeyDown(sender, args));
                                     e.Emit<KeyDown>();
                                 };
 
-                                menuItem.KeyUp += (object sender, KeyEventArgs args) =>
+                                menuItem.KeyUp += (object? sender, KeyEventArgs args) =>
                                 {
                                     e.Set(new KeyUp(sender, args));
                                     e.Emit<KeyUp>();
                                 };
 
-                                menuItem.LayoutUpdated += (object sender, EventArgs args) =>
+                                menuItem.LayoutUpdated += (object? sender, EventArgs args) =>
                                 {
                                     e.Set(new LayoutUpdated(sender, args));
                                     e.Emit<LayoutUpdated>();
                                 };
 
-                                menuItem.LostFocus += (object sender, RoutedEventArgs args) =>
+                                menuItem.LostFocus += (object? sender, RoutedEventArgs args) =>
                                 {
                                     e.Set(new LostFocus(sender, args));
                                     e.Emit<LostFocus>();
                                 };
 
-                                menuItem.PointerCaptureLost += (object sender, PointerCaptureLostEventArgs args) =>
+                                menuItem.PointerCaptureLost += (object? sender, PointerCaptureLostEventArgs args) =>
                                 {
                                     e.Set(new PointerCaptureLost(sender, args));
                                     e.Emit<PointerCaptureLost>();
                                 };
 
-                                menuItem.PointerEntered += (object sender, PointerEventArgs args) =>
+                                menuItem.PointerEntered += (object? sender, PointerEventArgs args) =>
                                 {
                                     e.Set(new PointerEnter(sender, args));
                                     e.Emit<PointerEnter>();
                                 };
 
-                                menuItem.PointerExited += (object sender, PointerEventArgs args) =>
+                                menuItem.PointerExited += (object? sender, PointerEventArgs args) =>
                                 {
                                     e.Set(new PointerLeave(sender, args));
                                     e.Emit<PointerLeave>();
                                 };
 
-                                menuItem.PointerMoved += (object sender, PointerEventArgs args) =>
+                                menuItem.PointerMoved += (object? sender, PointerEventArgs args) =>
                                 {
                                     e.Set(new PointerMoved(sender, args));
                                     e.Emit<PointerMoved>();
                                 };
 
-                                menuItem.PointerPressed += (object sender, PointerPressedEventArgs args) =>
+                                menuItem.PointerPressed += (object? sender, PointerPressedEventArgs args) =>
                                 {
                                     e.Set(new PointerPressed(sender, args));
                                     e.Emit<PointerPressed>();
                                 };
 
-                                menuItem.PointerReleased += (object sender, PointerReleasedEventArgs args) =>
+                                menuItem.PointerReleased += (object? sender, PointerReleasedEventArgs args) =>
                                 {
                                     e.Set(new PointerReleased(sender, args));
                                     e.Emit<PointerReleased>();
                                 };
 
-                                menuItem.PointerWheelChanged += (object sender, PointerWheelEventArgs args) =>
+                                menuItem.PointerWheelChanged += (object? sender, PointerWheelEventArgs args) =>
                                 {
                                     e.Set(new PointerWheelChanged(sender, args));
                                     e.Emit<PointerWheelChanged>();
                                 };
 
-                                menuItem.PropertyChanged += (object sender, AvaloniaPropertyChangedEventArgs args) =>
+                                menuItem.PropertyChanged += (object? sender, AvaloniaPropertyChangedEventArgs args) =>
                                 {
                                     e.Set(new PropertyChanged(sender, args));
                                     e.Emit<PropertyChanged>();
                                 };
 
-                                menuItem.ResourcesChanged += (object sender, ResourcesChangedEventArgs args) =>
+                                menuItem.ResourcesChanged += (object? sender, ResourcesChangedEventArgs args) =>
                                 {
                                     e.Set(new ResourcesChanged(sender, args));
                                     e.Emit<ResourcesChanged>();
                                 };
 
-                                menuItem.Tapped += (object sender, TappedEventArgs args) =>
+                                menuItem.Tapped += (object? sender, TappedEventArgs args) =>
                                 {
                                     e.Set(new Tapped(sender, args));
                                     e.Emit<Tapped>();
                                 };
 
-                                menuItem.TemplateApplied += (object sender, TemplateAppliedEventArgs args) =>
+                                menuItem.TemplateApplied += (object? sender, TemplateAppliedEventArgs args) =>
                                 {
                                     e.Set(new TemplateApplied(sender, args));
                                     e.Emit<TemplateApplied>();
                                 };
 
-                                menuItem.TextInput += (object sender, TextInputEventArgs args) =>
+                                menuItem.TextInput += (object? sender, TextInputEventArgs args) =>
                                 {
                                     e.Set(new TextInput(sender, args));
                                     e.Emit<TextInput>();
                                 };
 
-                                menuItem.TextInputMethodClientRequested += (object sender, TextInputMethodClientRequestedEventArgs args) =>
+                                menuItem.TextInputMethodClientRequested += (object? sender, TextInputMethodClientRequestedEventArgs args) =>
                                 {
                                     e.Set(new TextInputMethodClientRequested(sender, args));
                                     e.Emit<TextInputMethodClientRequested>();
