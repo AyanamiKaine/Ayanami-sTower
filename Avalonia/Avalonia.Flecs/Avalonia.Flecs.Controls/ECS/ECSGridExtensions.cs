e0e7bc607b4f3d223ff8054c692290c413f2eac6
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-29 16:44:53 +0100

    Add error handling for missing Grid component in ECSGridExtensions methods

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSGridExtensions.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSGridExtensions.cs
@@ -9,24 +9,40 @@ namespace Avalonia.Flecs.Controls.ECS
     {
         public static Entity SetRowDefinitions(this Entity entity, RowDefinitions rowDefinitions)
         {
-            entity.Get<Grid>().RowDefinitions = rowDefinitions;
-            return entity;
+            if (entity.Has<Grid>())
+            {
+                entity.Get<Grid>().RowDefinitions = rowDefinitions;
+                return entity;
+            }
+            throw new Exception("Entity does not have a Grid component. Try adding a control element that is an Grid control to the entity.");
         }
 
         public static RowDefinitions GetRowDefinitions(this Entity entity)
         {
-            return entity.Get<Grid>().RowDefinitions;
+            if (entity.Has<Grid>())
+            {
+                return entity.Get<Grid>().RowDefinitions;
+            }
+            throw new Exception("Entity does not have a Grid component. Try adding a control element that is an Grid control to the entity.");
         }
 
         public static Entity SetColumnDefinitions(this Entity entity, ColumnDefinitions columnDefinitions)
         {
-            entity.Get<Grid>().ColumnDefinitions = columnDefinitions;
-            return entity;
+            if (entity.Has<Grid>())
+            {
+                entity.Get<Grid>().ColumnDefinitions = columnDefinitions;
+                return entity;
+            }
+            throw new Exception("Entity does not have a Grid component. Try adding a control element that is an Grid control to the entity.");
         }
 
         public static ColumnDefinitions GetColumnDefinitions(this Entity entity)
         {
-            return entity.Get<Grid>().ColumnDefinitions;
+            if (entity.Has<Grid>())
+            {
+                return entity.Get<Grid>().ColumnDefinitions;
+            }
+            throw new Exception("Entity does not have a Grid component. Try adding a control element that is an Grid control to the entity.");
         }
 
     }
