368ac8b0fc84b2fb1c711575b2834f936059d0be
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-29 12:34:41 +0100

    Improve null safety in ECSScrollViewer by using nullable reference types for event handler parameters

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSScrollViewer.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSScrollViewer.cs
@@ -19,49 +19,49 @@ namespace Avalonia.Flecs.Controls.ECS
                                 // Adding event handlers
                                 // https://reference.avaloniaui.net/api/Avalonia.Controls.ScrollViewer/#Events
 
-                                scrollViewer.AttachedToLogicalTree += (object sender, LogicalTreeAttachmentEventArgs args) =>
+                                scrollViewer.AttachedToLogicalTree += (object? sender, LogicalTreeAttachmentEventArgs args) =>
                                 {
                                     e.Set(new AttachedToLogicalTree(sender, args));
                                     e.Emit<AttachedToLogicalTree>();
                                 };
 
-                                scrollViewer.AttachedToVisualTree += (object sender, VisualTreeAttachmentEventArgs args) =>
+                                scrollViewer.AttachedToVisualTree += (object? sender, VisualTreeAttachmentEventArgs args) =>
                                 {
                                     e.Set(new AttachedToVisualTree(sender, args));
                                     e.Emit<AttachedToVisualTree>();
                                 };
 
-                                scrollViewer.DataContextChanged += (object sender, EventArgs args) =>
+                                scrollViewer.DataContextChanged += (object? sender, EventArgs args) =>
                                 {
                                     e.Set(new DataContextChanged(sender, args));
                                     e.Emit<DataContextChanged>();
                                 };
 
-                                scrollViewer.DetachedFromLogicalTree += (object sender, LogicalTreeAttachmentEventArgs args) =>
+                                scrollViewer.DetachedFromLogicalTree += (object? sender, LogicalTreeAttachmentEventArgs args) =>
                                 {
                                     e.Set(new DetachedFromLogicalTree(sender, args));
                                     e.Emit<DetachedFromLogicalTree>();
                                 };
 
-                                scrollViewer.DetachedFromVisualTree += (object sender, VisualTreeAttachmentEventArgs args) =>
+                                scrollViewer.DetachedFromVisualTree += (object? sender, VisualTreeAttachmentEventArgs args) =>
                                 {
                                     e.Set(new DetachedFromVisualTree(sender, args));
                                     e.Emit<DetachedFromVisualTree>();
                                 };
 
-                                scrollViewer.DoubleTapped += (object sender, TappedEventArgs args) =>
+                                scrollViewer.DoubleTapped += (object? sender, TappedEventArgs args) =>
                                 {
                                     e.Set(new DoubleTapped(sender, args));
                                     e.Emit<DoubleTapped>();
                                 };
 
-                                scrollViewer.EffectiveViewportChanged += (object sender, EffectiveViewportChangedEventArgs args) =>
+                                scrollViewer.EffectiveViewportChanged += (object? sender, EffectiveViewportChangedEventArgs args) =>
                                 {
                                     e.Set(new EffectiveViewportChanged(sender, args));
                                     e.Emit<EffectiveViewportChanged>();
                                 };
 
-                                scrollViewer.GotFocus += (object sender, GotFocusEventArgs args) =>
+                                scrollViewer.GotFocus += (object? sender, GotFocusEventArgs args) =>
                                 {
                                     e.Set(new GotFocus(sender, args));
                                     e.Emit<GotFocus>();
