b04a830307334b6be18539138ca0fe3af493c4ef
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-29 16:01:16 +0100

    Add ECSAvaloniaObjectExtensions for extending Avalonia controls

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSAvaloniaObjectExtensions.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml.Templates;
+using Flecs.NET.Core;
+
+namespace Avalonia.Flecs.Controls.ECS
+{
+    public static class ECSAvaloniaObjectExtensions
+    {
+
+    }
+}
