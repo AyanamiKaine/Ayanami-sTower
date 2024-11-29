55796961857e273557718e77ca5779a87a6f8a79
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-29 12:35:03 +0100

    Improve null safety in ECSWindow by using nullable reference types for event handler parameters

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSWindow.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSWindow.cs
@@ -19,151 +19,151 @@ namespace Avalonia.Flecs.Controls.ECS
                            {
                                e.Set<ContentControl>(window);
 
-                               window.Activated += (object sender, EventArgs args) =>
+                               window.Activated += (object? sender, EventArgs args) =>
                                {
                                    e.Set(new Activated(sender, args));
                                    e.Emit<Activated>();
                                };
 
-                               window.AttachedToLogicalTree += (object sender, LogicalTreeAttachmentEventArgs args) =>
+                               window.AttachedToLogicalTree += (object? sender, LogicalTreeAttachmentEventArgs args) =>
                                {
                                    e.Set(new AttachedToLogicalTree(sender, args));
                                    e.Emit<AttachedToLogicalTree>();
                                };
 
-                               window.AttachedToVisualTree += (object sender, VisualTreeAttachmentEventArgs args) =>
+                               window.AttachedToVisualTree += (object? sender, VisualTreeAttachmentEventArgs args) =>
                                {
                                    e.Set(new AttachedToVisualTree(sender, args));
                                    e.Emit<AttachedToVisualTree>();
                                };
 
-                               window.Closed += (object sender, EventArgs args) =>
+                               window.Closed += (object? sender, EventArgs args) =>
                                {
                                    e.Set(new Closed(sender, args));
                                    e.Emit<Closed>();
                                };
 
-                               window.DataContextChanged += (object sender, EventArgs args) =>
+                               window.DataContextChanged += (object? sender, EventArgs args) =>
                                {
                                    e.Set(new DataContextChanged(sender, args));
                                    e.Emit<DataContextChanged>();
                                };
 
-                               window.Deactivated += (object sender, EventArgs args) =>
+                               window.Deactivated += (object? sender, EventArgs args) =>
                                {
                                    e.Set(new Deactivated(sender, args));
                                    e.Emit<Deactivated>();
                                };
 
-                               window.DetachedFromLogicalTree += (object sender, LogicalTreeAttachmentEventArgs args) =>
+                               window.DetachedFromLogicalTree += (object? sender, LogicalTreeAttachmentEventArgs args) =>
                                {
                                    e.Set(new DetachedFromLogicalTree(sender, args));
                                    e.Emit<DetachedFromLogicalTree>();
                                };
 
-                               window.DetachedFromVisualTree += (object sender, VisualTreeAttachmentEventArgs args) =>
+                               window.DetachedFromVisualTree += (object? sender, VisualTreeAttachmentEventArgs args) =>
                                {
                                    e.Set(new DetachedFromVisualTree(sender, args));
                                    e.Emit<DetachedFromVisualTree>();
                                };
 
-                               window.DoubleTapped += (object sender, TappedEventArgs args) =>
+                               window.DoubleTapped += (object? sender, TappedEventArgs args) =>
                                {
                                    e.Set(new DoubleTapped(sender, args));
                                    e.Emit<DoubleTapped>();
                                };
 
-                               window.EffectiveViewportChanged += (object sender, EffectiveViewportChangedEventArgs args) =>
+                               window.EffectiveViewportChanged += (object? sender, EffectiveViewportChangedEventArgs args) =>
                                {
                                    e.Set(new EffectiveViewportChanged(sender, args));
                                    e.Emit<EffectiveViewportChanged>();
                                };
 
-                               window.PositionChanged += (object sender, PixelPointEventArgs args) =>
+                               window.PositionChanged += (object? sender, PixelPointEventArgs args) =>
                                {
                                    e.Set(new PositionChanged(sender, args));
                                    e.Emit<PositionChanged>();
                                };
 
-                               window.GotFocus += (object sender, GotFocusEventArgs args) =>
+                               window.GotFocus += (object? sender, GotFocusEventArgs args) =>
                                {
                                    e.Set(new GotFocus(sender, args));
                                    e.Emit<GotFocus>();
                                };
 
-                               window.Initialized += (object sender, EventArgs args) =>
+                               window.Initialized += (object? sender, EventArgs args) =>
                                {
                                    e.Set(new Initialized(sender, args));
                                    e.Emit<Initialized>();
                                };
 
-                               window.KeyDown += (object sender, KeyEventArgs args) =>
+                               window.KeyDown += (object? sender, KeyEventArgs args) =>
                                {
                                    e.Set(new KeyDown(sender, args));
                                    e.Emit<KeyDown>();
                                };
 
-                               window.KeyUp += (object sender, KeyEventArgs args) =>
+                               window.KeyUp += (object? sender, KeyEventArgs args) =>
                                {
                                    e.Set(new KeyUp(sender, args));
                                    e.Emit<KeyUp>();
                                };
 
-                               window.LayoutUpdated += (object sender, EventArgs args) =>
+                               window.LayoutUpdated += (object? sender, EventArgs args) =>
                                {
                                    e.Set(new LayoutUpdated(sender, args));
                                    e.Emit<LayoutUpdated>();
                                };
 
-                               window.LostFocus += (object sender, RoutedEventArgs args) =>
+                               window.LostFocus += (object? sender, RoutedEventArgs args) =>
                                {
                                    e.Set(new LostFocus(sender, args));
                                    e.Emit<LostFocus>();
                                };
 
-                               window.PointerCaptureLost += (object sender, PointerCaptureLostEventArgs args) =>
+                               window.PointerCaptureLost += (object? sender, PointerCaptureLostEventArgs args) =>
                                {
                                    e.Set(new PointerCaptureLost(sender, args));
                                    e.Emit<PointerCaptureLost>();
                                };
 
-                               window.PointerEntered += (object sender, PointerEventArgs args) =>
+                               window.PointerEntered += (object? sender, PointerEventArgs args) =>
                                {
                                    e.Set(new PointerEnter(sender, args));
                                    e.Emit<PointerEnter>();
                                };
 
-                               window.PointerExited += (object sender, PointerEventArgs args) =>
+                               window.PointerExited += (object? sender, PointerEventArgs args) =>
                                {
                                    e.Set(new PointerLeave(sender, args));
                                    e.Emit<PointerLeave>();
                                };
 
-                               window.PointerMoved += (object sender, PointerEventArgs args) =>
+                               window.PointerMoved += (object? sender, PointerEventArgs args) =>
                                {
                                    e.Set(new PointerMoved(sender, args));
                                    e.Emit<PointerMoved>();
                                };
 
-                               window.PointerPressed += (object sender, PointerPressedEventArgs args) =>
+                               window.PointerPressed += (object? sender, PointerPressedEventArgs args) =>
                                {
                                    e.Set(new PointerPressed(sender, args));
                                    e.Emit<PointerPressed>();
                                };
 
-                               window.PointerReleased += (object sender, PointerReleasedEventArgs args) =>
+                               window.PointerReleased += (object? sender, PointerReleasedEventArgs args) =>
                                {
                                    e.Set(new PointerReleased(sender, args));
                                    e.Emit<PointerReleased>();
                                };
 
-                               window.PointerWheelChanged += (object sender, PointerWheelEventArgs args) =>
+                               window.PointerWheelChanged += (object? sender, PointerWheelEventArgs args) =>
                                {
                                    e.Set(new PointerWheelChanged(sender, args));
                                    e.Emit<PointerWheelChanged>();
                                };
 
-                               window.PropertyChanged += (object sender, AvaloniaPropertyChangedEventArgs args) =>
+                               window.PropertyChanged += (object? sender, AvaloniaPropertyChangedEventArgs args) =>
                                {
                                    e.Set(new PropertyChanged(sender, args));
                                    e.Emit<PropertyChanged>();
@@ -179,31 +179,31 @@ namespace Avalonia.Flecs.Controls.ECS
                                    }
                                };
 
-                               window.Tapped += (object sender, TappedEventArgs args) =>
+                               window.Tapped += (object? sender, TappedEventArgs args) =>
                                {
                                    e.Set(new Tapped(sender, args));
                                    e.Emit<Tapped>();
                                };
 
-                               window.TextInput += (object sender, TextInputEventArgs args) =>
+                               window.TextInput += (object? sender, TextInputEventArgs args) =>
                                {
                                    e.Set(new TextInput(sender, args));
                                    e.Emit<TextInput>();
                                };
 
-                               window.TextInputMethodClientRequested += (object sender, TextInputMethodClientRequestedEventArgs args) =>
+                               window.TextInputMethodClientRequested += (object? sender, TextInputMethodClientRequestedEventArgs args) =>
                                {
                                    e.Set(new TextInputMethodClientRequested(sender, args));
                                    e.Emit<TextInputMethodClientRequested>();
                                };
 
-                               window.TemplateApplied += (object sender, TemplateAppliedEventArgs args) =>
+                               window.TemplateApplied += (object? sender, TemplateAppliedEventArgs args) =>
                                {
                                    e.Set(new TemplateApplied(sender, args));
                                    e.Emit<TemplateApplied>();
                                };
 
-                               window.Opened += (object sender, EventArgs args) =>
+                               window.Opened += (object? sender, EventArgs args) =>
                                {
                                    e.Set(new Opened(sender, args));
                                    e.Emit<Opened>();
