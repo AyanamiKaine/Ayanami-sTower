ce499d62be3507660bdfa586171c1048cb5c9c63
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-29 16:01:58 +0100

    Refactor SpacedRepetitionPage to utilize ECS methods for setting properties and improve code clarity

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/Pages/SpacedRepetitionPage.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/Pages/SpacedRepetitionPage.cs
@@ -18,61 +18,54 @@ public class SpacedRepetitionPage
     {
         var spacedRepetitionPage = world.Entity("SpacedRepetitionPage")
             .Add<Page>()
-                    .Set(new Grid()
-                    {
-                        /*
+            .Set(new Grid())
+            /*
+            *: This represents a "star" column. 
+            It means this column will take up as much available space as 
+            possible after any fixed-size or Auto columns have been accounted for. 
+            Think of it as flexible or "greedy". 
+            In this case, the first column will grab most of the grid's width.
 
-                        *: This represents a "star" column. 
-                        It means this column will take up as much available space as 
-                        possible after any fixed-size or Auto columns have been accounted for. 
-                        Think of it as flexible or "greedy". 
-                        In this case, the first column will grab most of the grid's width.
+            Auto: This means the column's width will adjust automatically to 
+            fit the content within it. If you place a button in this column, 
+            the column will be just wide enough to accommodate the button's size.
+            */
+            .SetColumnDefinitions(new ColumnDefinitions("*, Auto, Auto"))
+            .SetRowDefinitions(new RowDefinitions("Auto, *, Auto"));
 
-                        Auto: This means the column's width will adjust automatically to 
-                        fit the content within it. If you place a button in this column, 
-                        the column will be just wide enough to accommodate the button's size.
-                        */
-                        ColumnDefinitions = new ColumnDefinitions("*, Auto, Auto"),
-                        RowDefinitions = new RowDefinitions("Auto, *, Auto"),
-                    });
+
 
         var listSearchSpacedRepetition = world.Entity("ListSearchSpacedRepetition")
-                    .ChildOf(spacedRepetitionPage)
-                    .Set(new TextBox()
-                    {
-                        Watermark = "Search Entries",
-                    });
-
+            .ChildOf(spacedRepetitionPage)
+            .Set(new TextBox())
+            .SetWatermark("Search Entries");
 
 
         var totalItems = world.Entity("TotalItems")
             .ChildOf(spacedRepetitionPage)
-            .Set(new TextBlock()
-            {
-                Text = "Total Items: 0",
-                VerticalAlignment = VerticalAlignment.Center,
-                Margin = new Thickness(10, 0)
-            })
+            .Set(new TextBlock())
+            .SetVerticalAlignment(VerticalAlignment.Center)
+            .SetMargin(new Thickness(10, 0))
+            .SetText("Total Items: 0")
             .SetColumn(1);
 
 
+        List<string> sortItems = ["Sort By Date", "Sort By Priority", "Sort By Name"];
         var sortItemsButton = world.Entity("SortItemsButton")
             .ChildOf(spacedRepetitionPage)
             .Set(new ComboBox() { })
-            .SetColumn(2);
-
-        List<string> sortItems = ["Sort By Date", "Sort By Priority", "Sort By Name"];
-        sortItemsButton.SetItemsSource(sortItems);
+            .SetColumn(2)
+            .SetItemsSource(sortItems);
 
         /*
-        I believe that entites should know the exact control type but
+        I believe that entites should not know the exact control type but
         all other entities should only care for the base classes like
         Control, Panel, ItemsControl, TemplatedControl, Etc. They should
         always take the lowest common denominator.
 
         No need to depend on things that we dont care for 
         */
-        ObservableCollection<string> dummyItems = [];
+
 
         var scrollViewer = world.Entity("ScrollViewer")
             .ChildOf(spacedRepetitionPage)
@@ -80,13 +73,12 @@ public class SpacedRepetitionPage
             .SetRow(1)
             .SetColumnSpan(3);
 
+        ObservableCollection<string> dummyItems = [];
         var srItems = world.Entity("SpaceRepetitionList")
             .ChildOf(scrollViewer)
-            .Set(new ListBox()
-            {
-                SelectionMode = SelectionMode.Multiple,
-                ItemsSource = dummyItems,
-            });
+            .Set(new ListBox())
+            .SetItemsSource(dummyItems)
+            .SetSelectionMode(SelectionMode.Multiple);
 
         listSearchSpacedRepetition.Observe<TextChanged>(() =>
         {
@@ -103,27 +95,40 @@ public class SpacedRepetitionPage
             .ChildOf(spacedRepetitionPage)
             .Set(new MenuFlyout());
 
-        var flyout = contextFlyout.Get<MenuFlyout>();
 
-        flyout.Items.Add(new MenuItem() { Header = "Open" });
-        flyout.Items.Add(new MenuItem() { Header = "Edit" });
-        flyout.Items.Add(new MenuItem() { Header = "Delete" });
+        var openMenuItem = world.Entity("OpenMenuItem")
+            .ChildOf(contextFlyout)
+            .Set(new MenuItem()
+            {
+                Header = "Open"
+            })
+            .Observe<Click>((Entity e) =>
+            {
+                Console.WriteLine("Open Clicked");
+            });
+
+        var editMenuItem = world.Entity("EditMenuItem")
+            .ChildOf(contextFlyout)
+            .Set(new MenuItem()
+            {
+                Header = "Edit"
+            })
+            .Observe<Click>((Entity e) =>
+            {
+                Console.WriteLine("Edit Clicked");
+            }); ;
 
 
-        ((MenuItem)contextFlyout.Get<MenuFlyout>().Items[0]!).Click += (sender, e) =>
-        {
-            Console.WriteLine("Open Clicked");
-        };
-
-        ((MenuItem)contextFlyout.Get<MenuFlyout>().Items[1]!).Click += (sender, e) =>
-        {
-            Console.WriteLine("Edit Clicked");
-        };
-
-        ((MenuItem)contextFlyout.Get<MenuFlyout>().Items[2]!).Click += (sender, e) =>
-        {
-            Console.WriteLine("Delete Clicked");
-        };
+        var deleteMenuItem = world.Entity("DeleteMenuItem")
+            .ChildOf(contextFlyout)
+            .Set(new MenuItem()
+            {
+                Header = "Delete"
+            })
+            .Observe<Click>((Entity e) =>
+            {
+                Console.WriteLine("Delete Clicked");
+            });
 
 
         dummyItems.Add("algorithm");
