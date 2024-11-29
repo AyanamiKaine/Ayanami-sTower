9f0cba1e26a1889e7e6d06f1dfe035669042ecc1
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-29 13:57:49 +0100

    Add methods to set ItemsSource and ItemTemplate in ECSItemsControlExtensions

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSItemsControlExtensions.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSItemsControlExtensions.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml.Templates;
 using Flecs.NET.Core;
 
 namespace Avalonia.Flecs.Controls.ECS
@@ -12,19 +13,41 @@ namespace Avalonia.Flecs.Controls.ECS
     {
 
         /// <summary>
-        /// Adds an item to the ItemsControl of an entity.
+        /// Gets or sets a collection used to generate the content of the <see cref="ItemsControl"/>.
         /// </summary>
-        /// <param name="entity">entity with an ItemsControl component</param>
-        /// <param name="value">object to be added to the items control</param>
-        /// <returns></returns>
-        /// <exception cref="Exception"></exception>
-        public static Entity AddItem(this Entity entity, object? value)
+        /// <remarks>
+        /// A common scenario is to use an <see cref="ItemsControl"/> such as a 
+        /// <see cref="ListBox"/> to display a data collection, or to bind an
+        /// <see cref="ItemsControl"/> to a collection object. To bind an <see cref="ItemsControl"/>
+        /// to a collection object, use the <see cref="ItemsSource"/> property.
+        /// 
+        /// When the <see cref="ItemsSource"/> property is set, the <see cref="Items"/> collection
+        /// is made read-only and fixed-size.
+        ///
+        /// When <see cref="ItemsSource"/> is in use, setting the property to null removes the
+        /// collection and restores usage to <see cref="Items"/>, which will be an empty 
+        /// <see cref="ItemCollection"/>.
+        /// </remarks>
+        public static Entity SetItemsSource(this Entity entity, System.Collections.IEnumerable? collection)
         {
-            var index = entity.Get<ItemsControl>().Items.Add(value);
-            if (index == -1)
-            {
-                throw new Exception("Failed to add item to ItemsControl");
-            }
+            entity.Get<ItemsControl>().ItemsSource = collection;
+            return entity;
+        }
+
+        public static System.Collections.IEnumerable? GetItemsSource(this Entity entity)
+        {
+            return entity.Get<ItemsControl>().ItemsSource;
+        }
+
+        /// <summary>
+        /// Sets the data template used to display the items in the ItemsControl.
+        /// </summary>
+        /// <param name="entity">Entity with an ItemsControl Component</param>
+        /// <param name="template"></param>
+        /// <returns></returns>
+        public static Entity SetItemTemplate(this Entity entity, DataTemplate? template)
+        {
+            entity.Get<ItemsControl>().ItemTemplate = template;
             return entity;
         }
     }
