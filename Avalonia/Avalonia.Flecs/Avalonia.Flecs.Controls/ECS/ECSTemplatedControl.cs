cc13f0c7134a109cea7fe05b3407285d352a6f73
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-30 01:35:10 +0100

    Refactor ECSTemplatedControl to streamline parent control management and remove redundant logic

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSTemplatedControl.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSTemplatedControl.cs
@@ -19,42 +19,11 @@ namespace Avalonia.Flecs.Controls.ECS
                         e.Set(new TemplateApplied(sender, args));
                         e.Emit<AttachedToVisualTree>();
                     };
-                    
-                    var parent = e.Parent();
-                    if (parent == 0)
-                    {
-                        return;
-                    }
-                    if (parent.Has<Panel>())
-                    {
-                        //We dont want to add the control twice
-                        if (parent.Get<Panel>().Children.Contains(templatedControl))
-                        {
-                            return;
-                        }
 
-                        parent.Get<Panel>().Children.Add(templatedControl);
-                    }
-                    else if (parent.Has<ContentControl>())
-                    {
-                        parent.Get<ContentControl>().Content = templatedControl;
-                    }
                 })
                 .OnRemove((Entity e, ref TemplatedControl templatedControl) =>
                 {
-                    var parent = e.Parent();
-                    if (parent == 0)
-                    {
-                        return;
-                    }
-                    if (parent.Has<Panel>())
-                    {
-                        parent.Get<Panel>().Children.Remove(templatedControl);
-                    }
-                    if (parent.Has<ContentControl>())
-                    {
-                        parent.Get<ContentControl>().Content = null;
-                    }
+                    e.Remove<Control>();
                 });
         }
     }
