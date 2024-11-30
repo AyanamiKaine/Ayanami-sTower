46fd12ec4f7dbc2d83cd5975cee885049ae69240
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-30 12:26:29 +0100

    Add SRItem and SRItemState for spaced repetition system

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/Data/SRItem.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Avalonia.Flecs.StellaLearning.Data
+{
+
+    public enum SRItemState
+    {
+        NewState,
+        Learning,
+        Review,
+        Relearning,
+    }
+
+
+    public record struct SRItem(
+        Guid Id,
+        string Name,
+        string Description,
+        double Stability,
+        double Difficulty,
+        double Priority,
+        int Repetitions,
+        int Lapsed,
+        DateTime LastReviewDate,
+        DateTime NextReviewDate,
+        int NumberOfTimesReviewed,
+        int ElapsedDays,
+        int ScheduledDays);
+
+}
