524b078bbc787e159b3342b0a0669564beb58c99
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-30 11:30:12 +0100

    Add ComponentNotFoundException for handling missing components on entities

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/AvaloniaComponentNotFoundException.cs
@@ -0,0 +1,38 @@
+using Flecs.NET.Core;
+
+namespace Avalonia.Flecs.Controls
+{
+
+    /*
+    DESIGN NOTES:
+    - Maybe we want to include all currently attached components in the exception message.
+    */
+
+    /// <summary>
+    /// Exception thrown when a component is not found on an entity.
+    /// </summary>
+    public class ComponentNotFoundException : Exception
+    {
+        public ComponentNotFoundException(Entity entity, Type componentType, string methodName)
+                    : base(@$"Method '{methodName}' expected component '{componentType}' on entity:
+
+               ID: {entity}
+               Name: {(entity.Name() != "" ? entity.Name() : "<no name>")}
+               Path: {entity.Path()}
+               Parent: {(entity.Parent() != 0 ? entity.Parent().ToString() : "<no parent>")}
+
+            Try adding '{componentType}' as a component to this entity.")
+        {
+            ComponentType = componentType;
+            Entity = entity;
+        }
+        public ComponentNotFoundException(string message, Type componentType, Entity entity) : base(message)
+        {
+            ComponentType = componentType;
+            Entity = entity;
+        }
+
+        public object ComponentType { get; set; }
+        public Entity Entity { get; set; }
+    }
+}
