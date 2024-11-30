ff805b8e069ca8daf1dcd2652e68e4df9540221e
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-30 12:26:22 +0100

    Throw ComponentNotFoundException for missing Window components in SetWindowTitle and ShowWindow methods

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSWindowExtensions.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSWindowExtensions.cs
@@ -8,16 +8,29 @@ namespace Avalonia.Flecs.Controls.ECS
 
     public static class ECSWindowExtensions
     {
+        /// <summary>
+        /// Helper function to set the title of a window component on an entity.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="title"></param>
+        /// <returns></returns>
         public static Entity SetWindowTitle(this Entity entity, string title)
         {
             if (entity.Has<Window>())
             {
                 entity.Get<Window>().Title = title;
+                return entity;
             }
-            return entity;
+            throw new ComponentNotFoundException(entity, typeof(Window), nameof(SetWindowTitle));
         }
 
 
+        /// <summary>
+        /// Helper function to open a window attached to an entity.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public static Entity ShowWindow(this Entity entity)
         {
             if (entity.Has<Window>())
@@ -25,7 +38,7 @@ namespace Avalonia.Flecs.Controls.ECS
                 entity.Get<Window>().Show();
                 return entity;
             }
-            throw new Exception("Entity does not have a Window component");
+            throw new ComponentNotFoundException(entity, typeof(Window), nameof(ShowWindow));
         }
 
     }
