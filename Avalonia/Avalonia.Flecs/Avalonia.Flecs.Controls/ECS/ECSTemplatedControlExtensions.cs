08a4137ade645677b12d5377a3ffd4be6527c47a
Ayanami Kaine <patrick.grohs@hhu.de>
2024-11-30 12:26:14 +0100

    Throw ComponentNotFoundException for missing TemplatedControl components in SetPadding and GetPadding methods

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSTemplatedControlExtensions.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSTemplatedControlExtensions.cs
@@ -9,6 +9,13 @@ namespace Avalonia.Flecs.Controls.ECS
     public static class ECSTemplatedControlExtensions
     {
 
+        /// <summary>
+        /// Helper function to set the padding of a TemplatedControl control component of an entity.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="padding"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public static Entity SetPadding(this Entity entity, Thickness padding)
         {
             if (entity.Has<TemplatedControl>())
@@ -16,15 +23,21 @@ namespace Avalonia.Flecs.Controls.ECS
                 entity.Get<TemplatedControl>().Padding = padding;
                 return entity;
             }
-            throw new Exception("Entity does not have a TemplatedControl component with a Padding property");
+            throw new ComponentNotFoundException(entity, typeof(TemplatedControl), nameof(SetPadding));
         }
 
+        /// <summary>
+        /// Helper function to get the padding of a TemplatedControl control component of an entity.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public static Thickness GetPadding(this Entity entity)
         {
             if (entity.Has<TemplatedControl>())
                 return entity.Get<TemplatedControl>().Padding;
 
-            throw new Exception("Entity does not have a TemplatedControl component with a Padding property");
+            throw new ComponentNotFoundException(entity, typeof(TemplatedControl), nameof(GetPadding));
         }
     }
 }
