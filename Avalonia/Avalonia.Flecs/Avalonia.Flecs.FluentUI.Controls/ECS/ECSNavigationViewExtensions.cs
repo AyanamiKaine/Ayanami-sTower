400ec151c2113b5ae49de168c0f932bd671d34dc
Ayanami Kaine <patrick.grohs@hhu.de>
2024-12-02 23:12:49 +0100

    Add ECSNavigationViewExtensions for setting pane titles in NavigationView

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.FluentUI.Controls/ECS/ECSNavigationViewExtensions.cs
@@ -0,0 +1,22 @@
+using Avalonia.Controls;
+using Avalonia.Layout;
+using Flecs.NET.Core;
+using Avalonia.Flecs.Controls;
+using FluentAvalonia.UI.Controls;
+namespace Avalonia.Flecs.FluentUI.Controls.ECS
+{
+
+    public static class ECSNavigationViewExtensions
+    {
+        public static Entity SetPaneTitle(this Entity entity, string paneTitle)
+        {
+            if (entity.Has<NavigationView>())
+            {
+                entity.Get<NavigationView>().PaneTitle = paneTitle;
+                return entity;
+            }
+
+            throw new ComponentNotFoundException(entity, typeof(ContentControl), nameof(NavigationView));
+        }
+    }
+}
