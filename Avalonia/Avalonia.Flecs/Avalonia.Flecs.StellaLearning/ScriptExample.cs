ef6ab92453115532b5a79190044a420f9a62f153
Ayanami Kaine <patrick.grohs@hhu.de>
2024-12-02 22:43:40 +0100

    Ensure ECS scripts run on the UI thread to prevent deferred execution

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/ScriptExample.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/ScriptExample.cs
@@ -18,7 +18,18 @@ public static class ScriptExample
 
     public static async Task RunScriptAsync(World world)
     {
-        var scriptManager = world.Get<ScriptManager>();
-        await scriptManager.RunScriptAsync("ChangeWindowTitle");
+        /*
+        We have to run scripts that access the ECS world in the UI Thread otherwise
+        functions related to flecs and the ECS world are defered until the function returns.
+
+        For example saying entity.Set(new Component()) will set the component only AFTER
+        the script has returned. This is because sharing the ECS world over different threads
+        does not work. Or as far as I know it does not work.
+        */
+        await Avalonia.Threading.Dispatcher.UIThread.InvokeAsync(async () =>
+        {
+            var scriptManager = world.Get<ScriptManager>();
+            await scriptManager.RunScriptAsync("ChangeWindowTitle");
+        });
     }
 }
