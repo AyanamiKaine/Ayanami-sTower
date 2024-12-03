3b380f75b6431a7d6cc54558ab07ca861bc10545
Ayanami Kaine <ayanami@ayanamis-codex.net>
2024-12-03 10:01:01 +0100

    Add ECSMethodExtension for dynamic method invocation on entity controls

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSMethodExtension.cs
@@ -0,0 +1,83 @@
+using System.Reflection;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml.Templates;
+using Flecs.NET.Core;
+
+namespace Avalonia.Flecs.Controls.ECS
+{
+    /// <summary>
+    /// This extension methods all relate to getting and running methods
+    /// on specific avalonia controls where we dont know the exact type
+    /// but know an specific method exists.
+    /// </summary>
+    public static class ECSMethodExtension
+    {
+
+        /// <summary>
+        /// Tries running a method on a control component of an entity.
+        /// 
+        /// If you set a window component on an entity and say:
+        /// entity.RunMethod("Show"); is equivalent to entity.Get<Window>().Show();
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="methodName"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        /// <exception cref="MissingMethodException"></exception>
+        /// <exception cref="ComponentNotFoundException"></exception>
+        public static Entity RunMethod(this Entity entity, string methodName, params object[] args)
+        {
+            if (entity.Has<object>())
+            {
+                var control = entity.Get<object>();
+                // I personally dont really know how reflection works and
+                // what happens using GetMethod
+                var method = control.GetType().GetMethod(
+                    methodName,
+                    BindingFlags.Public | BindingFlags.Instance,
+                    null,
+                    args?.Select(a => a?.GetType() ?? typeof(object)).ToArray() ?? Type.EmptyTypes,
+                    null);
+
+                if (method != null)
+                {
+                    method.Invoke(control, args);
+                }
+                else
+                {
+                    throw new MissingMethodException($"Method {methodName} not found on {control.GetType().Name}");
+                }
+
+                return entity;
+            }
+            throw new ComponentNotFoundException(entity, typeof(object), nameof(RunMethod));
+        }
+
+        public static MethodInfo GetMethod(this Entity entity, string methodName)
+        {
+            if (entity.Has<object>())
+            {
+                var control = entity.Get<object>();
+
+                var methodInfo = control.GetType().GetMethod(methodName);
+                if (methodInfo != null)
+                    return methodInfo;
+            }
+            throw new ComponentNotFoundException(entity, typeof(object), nameof(GetMethod));
+        }
+
+        public static MethodInfo GetMethod(this Entity entity, string methodName, params Type[] parameterTypes)
+        {
+            if (entity.Has<object>())
+            {
+                var control = entity.Get<object>();
+                var methodInfo = control.GetType().GetMethod(methodName, parameterTypes);
+                if (methodInfo != null)
+                    return methodInfo;
+                throw new MissingMethodException($"Method {methodName} not found on {control.GetType().Name}");
+            }
+            throw new ComponentNotFoundException(entity, typeof(object), nameof(GetMethod));
+        }
+    }
+}
