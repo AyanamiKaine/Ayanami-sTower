885da5c6d35d2ffb07e6ec5e535f36673a6a7720
Ayanami Kaine <ayanami@ayanamis-codex.net>
2024-12-03 11:10:52 +0100

    Add nullability to sender parameter in event record structs for improved safety

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.FluentUI.Controls/ECS/Events.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.FluentUI.Controls/ECS/Events.cs
@@ -4,12 +4,12 @@ using FluentAvalonia.UI.Controls;
 
 namespace Avalonia.Flecs.FluentUI.Controls.ECS.Events
 {
-    public record struct OnSelectionChanged(object Sender, NavigationViewSelectionChangedEventArgs Args);
-    public record struct OnDisplayModeChanged(object Sender, NavigationViewDisplayModeChangedEventArgs Args);
-    public record struct OnBackRequested(object Sender, NavigationViewBackRequestedEventArgs Args);
-    public record struct OnItemCollapsed(object Sender, NavigationViewItemCollapsedEventArgs Args);
-    public record struct OnItemExpanding(object Sender, NavigationViewItemExpandingEventArgs Args);
-    public record struct OnItemInvoked(object Sender, NavigationViewItemInvokedEventArgs Args);
-    public record struct OnPaneClosing(object Sender, NavigationViewPaneClosingEventArgs Args);
+    public record struct OnSelectionChanged(object? Sender, NavigationViewSelectionChangedEventArgs Args);
+    public record struct OnDisplayModeChanged(object? Sender, NavigationViewDisplayModeChangedEventArgs Args);
+    public record struct OnBackRequested(object? Sender, NavigationViewBackRequestedEventArgs Args);
+    public record struct OnItemCollapsed(object? Sender, NavigationViewItemCollapsedEventArgs Args);
+    public record struct OnItemExpanding(object? Sender, NavigationViewItemExpandingEventArgs Args);
+    public record struct OnItemInvoked(object? Sender, NavigationViewItemInvokedEventArgs Args);
+    public record struct OnPaneClosing(object? Sender, NavigationViewPaneClosingEventArgs Args);
 
 }
