c1d03cb62a7e595d5cf39ea2465afd4a15e3a5eb
Ayanami Kaine <patrick.grohs@hhu.de>
2024-12-06 15:54:34 +0100

    Add PropertyNotFoundException class for handling missing properties in Avalonia entities

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/PropertyNotFoundException.cs
@@ -0,0 +1,42 @@
+using Flecs.NET.Core;
+
+/// <summary>
+/// Should be thrown when we want to use a field/property of an avalonia 
+/// and expect it to be there.
+/// </summary>
+public class PropertyNotFoundException : Exception
+{
+    /// <summary>
+    /// Constructor for PropertyNotFoundException
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <param name="componentType"></param>
+    /// <param name="expectedProperty"></param>
+    /// <param name="methodName"></param>
+    public PropertyNotFoundException(Entity entity, Type componentType, string expectedProperty, string methodName)
+                    : base(@$"Method '{methodName}' expected property'{expectedProperty}' not found in component {componentType} on entity:
+
+               ID: {entity}
+               Name: {(entity.Name() != "" ? entity.Name() : "<no name>")}
+               Path: {entity.Path()}
+               Parent: {(entity.Parent() != 0 ? entity.Parent().ToString() : "<no parent>")}
+               Components: {entity.Type().Str() + "\n"}
+
+            Try adding a avalonia object that has the property:'{expectedProperty}' as a component to this entity.")
+
+    {
+        ExpectedProperty = expectedProperty;
+        Entity = entity;
+    }
+
+    /// <summary>
+    /// The exepected property that was not found.
+    /// </summary>
+    public string ExpectedProperty { get; set; }
+
+    /// <summary>
+    /// The entity that the property was not found on.
+    /// </summary>
+    public Entity Entity { get; set; }
+}
+
