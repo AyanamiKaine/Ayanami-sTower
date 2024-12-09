80f3c62787dd11a7a226b19eafb5390b5c58d38d
Ayanami Kaine <patrick.grohs@hhu.de>
2024-12-09 13:21:08 +0100

    Add unit tests for ECSContentControlExtensions to validate SetContent and GetContent methods

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls.Tests/ECSContentControlExtensionsTests.cs
@@ -0,0 +1,39 @@
+using Avalonia.Controls;
+using Avalonia.Flecs.Controls.ECS;
+using Flecs.NET.Core;
+
+namespace Avalonia.Flecs.Controls.Tests;
+
+public class ECSContentControlExtensionsTests
+{
+
+    [Fact]
+    public void SetContent()
+    {
+        var world = World.Create();
+        world.Import<Avalonia.Flecs.Controls.ECS.Module>();
+
+        var entity = world.Entity("TestEntity")
+            .Set(new ContentControl())
+            .SetContent(new Button());
+
+        Assert.True(entity.Get<ContentControl>().Content is Button);
+    }
+
+    [Fact]
+    public void GetContent()
+    {
+        var world = World.Create();
+        world.Import<Avalonia.Flecs.Controls.ECS.Module>();
+
+        var entity = world.Entity("TestEntity")
+            .Set(new ContentControl()
+            {
+                Content = new Button()
+            });
+
+        Assert.True(entity.GetContent() is Button);
+    }
+
+
+}
