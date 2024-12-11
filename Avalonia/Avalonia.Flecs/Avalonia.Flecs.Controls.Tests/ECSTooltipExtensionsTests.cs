d4e03296731c62499b45faeab916234ae728224b
Ayanami Kaine <patrick.grohs@hhu.de>
2024-12-12 00:12:57 +0100

    Add unit tests for attaching tooltips to entities in ECSTooltipExtensions

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls.Tests/ECSTooltipExtensionsTests.cs
@@ -0,0 +1,37 @@
+using Avalonia.Controls;
+using Avalonia.Flecs.Controls.ECS;
+using Flecs.NET.Core;
+
+namespace Avalonia.Flecs.Controls.Tests;
+
+public class ECSTooltipExtensionsTests
+{
+
+    /// <summary>
+    /// We want to be able to attach a tooltip to an entity
+    /// </summary>
+    [Fact]
+    public void AttachToolTipToEntity()
+    {
+        var world = World.Create();
+        world.Import<Avalonia.Flecs.Controls.ECS.Module>();
+
+        var tooltip = world.Entity("ToolTip")
+            .Set(new ToolTip())
+            .SetContent(new TextBlock()
+            {
+                Text = "Hello World"
+            });
+
+        var entity = world.Entity("Button")
+            .Set(new Button())
+            .AttachToolTip(tooltip);
+
+
+        //Here we test what tooltip is now attached to the button class
+        //We get the tooltip from the button class and compare it to the tooltip we attached
+        //We expect that they are the same
+        ToolTip foundTooltip = (ToolTip)ToolTip.GetTip(entity.Get<Control>());
+        Assert.Equal(tooltip.Get<ToolTip>(), foundTooltip);
+    }
+}
