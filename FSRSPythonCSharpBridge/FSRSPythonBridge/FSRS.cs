f1765d40e1eb943bd3075a61bda06eb07474be65
Ayanami Kaine <ayanami@ayanamis-codex.net>
2024-12-17 12:25:14 +0100

    Add FSRSPythonBridge project to integrate Python FSRS package

---
+++ FSRSPythonCSharpBridge/FSRSPythonBridge/FSRS.cs
@@ -0,0 +1,16 @@
+﻿using Python.Runtime;
+namespace FSRSPythonBridge;
+
+public class FSRS
+{
+    public FSRS()
+    {
+        // Set the path to your Python DLL
+        Runtime.PythonDLL = @"/lib64/libpython3.so"; // Replace with your actual path
+
+        // Initialize the Python engine
+        PythonEngine.Initialize();
+
+        Py.Import("FSRS");
+    }
+}
---
+++ FSRSPythonCSharpBridge/README.md
@@ -0,0 +1,3 @@
+## FSRS Python Brige 
+This library makes it possible to use the fsrs python package in dotnet
+it embeeds the python interpreter in the library.
