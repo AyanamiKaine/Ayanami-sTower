958c41221548eb9eb161b1ba8cc0bf6d741b9bbc
Ayanami Kaine <patrick.grohs@hhu.de>
2024-12-21 12:58:54 +0100

    Add Avalonia.Flecs.DebugWindow project with initial application structure and main window

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.DebugWindow/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.Flecs.DebugWindow;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
