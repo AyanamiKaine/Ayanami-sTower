f74b98d32687639c5b397aab3c0ff2b964f87c17
Ayanami Kaine <patrick.grohs@hhu.de>
2024-12-22 15:48:21 +0100

    Add AddFile window for creating spaced repetition file items

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/Windows/AddFile.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Flecs.Controls.ECS;
+using Avalonia.Flecs.StellaLearning.Data;
+using Avalonia.Flecs.Util;
+using Avalonia.Platform.Storage;
+using Flecs.NET.Core;
+
+namespace Avalonia.Flecs.StellaLearning.Windows;
+
+/// <summary>
+/// Represents the window to add spaced repetition items of the type file
+/// </summary>
+public static class AddFile
+{
+
+    /// <summary>
+    /// Create the Add File Window
+    /// </summary>
+    /// <param name="entities"></param>
+    /// <returns></returns>
+    public static Entity Create(NamedEntities entities)
+    {
+        var addFileWindow = entities.GetEntityCreateIfNotExist("AddFileWindow")
+            .Set(new Window())
+            .SetWindowTitle("Add File")
+            .SetWidth(400)
+            .SetHeight(400);
+
+        DefineWindowContents(entities).ChildOf(addFileWindow);
+
+        return addFileWindow;
+    }
+
+    private static Entity DefineWindowContents(NamedEntities entities)
+    {
+        var layout = entities.GetEntityCreateIfNotExist("AddFileLayout")
+            .Set(new StackPanel())
+            .SetOrientation(Layout.Orientation.Vertical)
+            .SetSpacing(10)
+            .SetMargin(20);
+
+        var nameTextBox = entities.GetEntityCreateIfNotExist("nameTextBox")
+            .ChildOf(layout)
+            .Set(new TextBox())
+            .SetWatermark("Name");
+
+        var filePickerButton = FilePickerButton(entities);
+
+        var filePath = entities.GetEntityCreateIfNotExist("filePathTextBox")
+            .ChildOf(layout)
+            .Set(new TextBox())
+            .SetWatermark("FilePath")
+            .SetInnerRightContent(filePickerButton);
+
+        filePickerButton.OnClick(async (e, args) => filePath.SetText(await FilePickerAsync(entities)));
+
+        var createFileButton = entities.GetEntityCreateIfNotExist("createFileButton")
+            .ChildOf(layout)
+            .Set(new Button())
+            .SetContent("Create Item")
+            .OnClick((sender, args) =>
+            {
+                entities["SpacedRepetitionItems"].Get<ObservableCollection<SpacedRepetitionItem>>().Add(new SpacedRepetitionFile()
+                {
+                    Name = nameTextBox.GetText(),
+                    FilePath = filePath.GetText(),
+                    SpacedRepetitionItemType = SpacedRepetitionItemType.File
+                });
+            });
+
+        return layout;
+    }
+
+    private static Entity FilePickerButton(NamedEntities entities)
+    {
+        var browseForFileButton = entities.GetEntityCreateIfNotExist("BrowseForFileButton")
+            .Set(new Button());
+
+        var browseForFileButtonContent = entities.GetEntityCreateIfNotExist("BrowseForFileButtonContent")
+            .ChildOf(browseForFileButton)
+            .Set(new TextBlock())
+            .SetText("Browse");
+
+        return browseForFileButton;
+    }
+
+    private static async Task<string> FilePickerAsync(NamedEntities entities)
+    {
+        // Create and configure the file picker options
+        var options = new FilePickerOpenOptions
+        {
+            Title = "Select the obsidian executable",
+            AllowMultiple = false, // Set to true if you want to allow multiple file selections
+        };
+
+        // Create an OpenFileDialog instance
+        IReadOnlyList<IStorageFile> result = await entities["MainWindow"].Get<Window>().StorageProvider.OpenFilePickerAsync(options);
+
+        if (result != null && result.Count > 0)
+        {
+            // Get the selected file
+            IStorageFile file = result[0];
+
+            return file.TryGetLocalPath()!;
+        }
+        return string.Empty;
+    }
+}
