9a9881bd35100707c9c9f5a248c4195ab38cbd93
Ayanami Kaine <patrick.grohs@hhu.de>
2024-12-27 13:47:09 +0100

    Add Ayanami's C# Library Template with project files and initial configuration

---
+++ ayanamis_csharp_library_template/AyanamisCSharpLibraryTemplate/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace AyanamisCSharpLibraryTemplate;
+
+/// <summary>
+/// Your awesome class, depth, useful class documentation.
+/// That is not just the class name and a one liner.
+/// </summary>
+/// /// <remarks>
+/// Your awesome remarks.
+/// </remarks>
+/// <example>
+/// <code>
+/// System.Console.WriteLine("Hello, World!");
+/// </code>
+/// </example>
+public class Class1
+{
+
+}
---
+++ ayanamis_csharp_library_template/README.md
@@ -0,0 +1,3 @@
+## Ayanami's CSharp Library Template
+
+I personally use a much more stricter C# standard, treating warning as errors and adding more analysers to improve code quality.
