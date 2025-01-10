e4e1663e6e4085f1d5cedc0b312426776ef07984
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-10 11:00:20 +0100

    Add TrafficLight state machine implementation and unit tests

---
+++ AsyncDistributedSystems/StateMachine/Program.cs
@@ -0,0 +1,79 @@
+﻿TrafficLight light = new();
+light.TimerElapsed(); // Green
+light.TimerElapsed(); // Yellow
+light.SensorTriggered(); // Red (Sensor)
+
+
+public interface ITrafficLightState
+{
+    void HandleTimerElapsed(TrafficLight context);
+    void HandleSensorTriggered(TrafficLight context);
+}
+
+public class RedState : ITrafficLightState
+{
+    public void HandleTimerElapsed(TrafficLight context)
+    {
+        context.ChangeState(new GreenState());
+        Console.WriteLine("Red -> Green");
+    }
+
+    public void HandleSensorTriggered(TrafficLight context)
+    {
+        // Optional: Do nothing or handle special cases
+    }
+}
+
+public class GreenState : ITrafficLightState
+{
+    public void HandleTimerElapsed(TrafficLight context)
+    {
+        context.ChangeState(new YellowState());
+        Console.WriteLine("Green -> Yellow");
+    }
+
+    public void HandleSensorTriggered(TrafficLight context)
+    {
+        // Optional: Do nothing or handle special cases
+    }
+}
+
+public class YellowState : ITrafficLightState
+{
+    public void HandleTimerElapsed(TrafficLight context)
+    {
+        context.ChangeState(new RedState());
+        Console.WriteLine("Yellow -> Red");
+    }
+
+    public void HandleSensorTriggered(TrafficLight context)
+    {
+        context.ChangeState(new RedState());
+        Console.WriteLine("Yellow -> Red (Sensor)");
+    }
+}
+
+public class TrafficLight
+{
+    public ITrafficLightState CurrentState { get; private set; }
+
+    public TrafficLight()
+    {
+        CurrentState = new RedState(); // Initial state
+    }
+
+    public void ChangeState(ITrafficLightState newState)
+    {
+        CurrentState = newState;
+    }
+
+    public void TimerElapsed()
+    {
+        CurrentState.HandleTimerElapsed(this);
+    }
+
+    public void SensorTriggered()
+    {
+        CurrentState.HandleSensorTriggered(this);
+    }
+}
