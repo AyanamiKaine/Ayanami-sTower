aa1950abde9aaa87054cbe12b5cc6a5d313454fa
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-10 11:00:15 +0100

    Add dynamic typing benchmark project with BenchmarkDotNet integration

---
+++ dynamic_typing_benchmark_csharp/DynamicBenchmark/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Dynamic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+
+namespace DynamicBenchmark;
+
+
+//[SimpleJob(RuntimeMoniker.Net90, baseline: true)]
+//[SimpleJob(RuntimeMoniker.NativeAot90)]
+//[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+//[RPlotExporter]
+public class Person
+{
+    //[Params("Tim", "Katarina", "")]
+    public string Name { get; set; } = "";
+
+    //[Params(0, 9, 24)]
+    public int Age { get; set; } = 0;
+
+    //[Benchmark]
+    public string SayHelloTyped()
+    {
+        return $"Hello my name is {Name} and I am {Age} years old!";
+    }
+
+    //[Benchmark]
+    public dynamic SayHelloDynamic()
+    {
+        return $"Hello my name is {Name} and I am {Age} years old!"; ;
+    }
+}
+
+[SimpleJob(RuntimeMoniker.Net90, baseline: true)]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RPlotExporter]
+public class DynamicBenchmark
+{
+
+    private dynamic _person; // Store the ExpandoObject here
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        // Create the ExpandoObject once before the benchmarks run
+        _person = new ExpandoObject();
+        _person.age = 20;
+        _person.name = "Tom";
+    }
+
+    [Benchmark]
+    public dynamic SayHelloDynamic()
+    {
+        dynamic age = 20;
+        dynamic name = "Tom";
+
+        return $"Hello my name is {name} and I am {age} years old!"; ;
+    }
+
+
+    [Benchmark]
+    public dynamic SayHelloDynamicReassigned()
+    {
+        dynamic age = 20;
+        dynamic name = "Tom";
+
+        age = "Tom";
+        name = 20;
+
+        age = 20;
+        name = "Tom";
+
+        return $"Hello my name is {name} and I am {age} years old!"; ;
+    }
+
+    [Benchmark]
+    public string SayHelloTyped()
+    {
+        int age = 20;
+        string name = "Tom";
+
+        return $"Hello my name is {name} and I am {age} years old!"; ;
+    }
+
+    [Benchmark]
+    public dynamic SayHelloExpando()
+    {
+        return $"Hello my name is {_person.name} and I am {_person.age} years old!";
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<DynamicBenchmark>();
+    }
+}
---
+++ dynamic_typing_benchmark_csharp/README.md
@@ -0,0 +1,3 @@
+# Dynamic Typing Performance
+
+C# has the dynamic type, its use to avoid static type checking and doing it instead at runtime. Its said to only use it for interop of dynamic languages like python. I dont like this. Can we use the dynamic type extensively?. How big would the performance hit be? Does the JIT help? 
