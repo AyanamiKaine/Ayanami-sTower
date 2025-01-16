f8a9e13b3b52193f36963e102c9fe8ac5ac6518b
Ayanami Kaine <ayanami@ayanamis-codex.net>
2025-01-16 07:30:50 +0100

    Add unit test for Scanner class to validate token generation

---
+++ crafting_interpreters/jlox/jloxUnitTests/ScannerUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using jlox;
+namespace jloxUnitTests;
+
+public class ScannerUnitTest
+{
+    [Fact]
+    public void SimpleExpression()
+    {
+        var expression = """var name = "Ayanami"; """;
+        var scanner = new Scanner(expression);
+
+        var actualTokens = scanner.ScanTokens();
+
+        var expectedTokens = new List<Token>()
+        {
+            new(TokenType.VAR, "var", null, 1),
+            new(TokenType.IDENTIFIER, "name", null, 1),
+            new(TokenType.EQUAL, "=", null, 1),
+            new(TokenType.STRING, "\"Ayanami\"", "Ayanami", 1),
+            new(TokenType.SEMICOLON, ";", null, 1),
+            new(TokenType.EOF, "", null, 1)
+        };
+
+        Assert.Equal(expectedTokens, actualTokens);
+    }
+}
