76c6458340d379faabbe0ecb4ed3d84190e5430d
Ayanami Kaine <ayanami@ayanamis-codex.net>
2025-01-16 21:55:07 +0100

    Add Error method to Lox class for improved error reporting

--- crafting_interpreters/jlox/jlox/Lox.cs
+++ crafting_interpreters/jlox/jlox/Lox.cs
@@ -49,6 +49,18 @@ public class Lox
         Report(line, "", message);
     }
 
+    public static void Error(Token token, string message)
+    {
+        if (token.Type == TokenType.EOF)
+        {
+            Report(token.Line, " at end", message);
+        }
+        else
+        {
+            Report(token.Line, $"at '{token.Lexeme}'", message);
+        }
+    }
+
     private static void Report(int line, string where, string message)
     {
         Console.WriteLine($"[line {line}] Error {where}: {message}");
