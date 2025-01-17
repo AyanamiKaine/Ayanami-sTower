65adc000eb1374a71f803bc2709630ff76d13288
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-17 15:48:40 +0100

    Update ASTPrinterUnitTest to handle culture-specific number formatting in expected output

--- crafting_interpreters/jlox/jloxUnitTests/ASTPrinterUnitTest.cs
+++ crafting_interpreters/jlox/jloxUnitTests/ASTPrinterUnitTest.cs
@@ -16,7 +16,12 @@ public class ASTPrinterUnitTest
             new Expr.Grouping(
                 new Expr.Literal(45.67)));
 
-        var expectedExpressionString = "(* (- 123) (group 45.67))";
+
+        // Why are numbers as expressions written and not as strings?
+        // Because of culture difference between 45.67 and 45,67
+        // If we hardcode any representation of a comma value
+        // it will be wrong in countries where the default is different.
+        var expectedExpressionString = $"(* (- {123}) (group {45.67}))";
         var actualExpressionString = ASTPrinter.Print(expression);
 
         Assert.Equal(expectedExpressionString, actualExpressionString);
