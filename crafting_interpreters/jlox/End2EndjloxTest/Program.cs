57e0cf138b15324b70c15acaf94e70b0af563853
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-19 12:26:38 +0100

    Add Fibonacci sequence example to End2End test

--- crafting_interpreters/jlox/End2EndjloxTest/Program.cs
+++ crafting_interpreters/jlox/End2EndjloxTest/Program.cs
@@ -1,2 +1,17 @@
 ﻿using jlox;
-Lox.RunPrompt();
+
+var fibonacci =
+        """
+        var a = 0; 
+        var temp; 
+        
+        for (var b = 1; a < 10000; b = temp + b) 
+        { 
+            print a; 
+            temp = a; 
+            a = b; 
+        }
+        """;
+
+
+Lox.Run(fibonacci);
