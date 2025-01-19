cf5a6a81f7addda46b2c7cb1988a249aef35b580
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-19 22:12:50 +0100

    Add property retrieval to LoxInstance with error handling for undefined properties

--- crafting_interpreters/jlox/jlox/LoxInstance.cs
+++ crafting_interpreters/jlox/jlox/LoxInstance.cs
@@ -2,7 +2,16 @@ namespace jlox;
 
 public class LoxInstance(LoxClass klass)
 {
+    private readonly Dictionary<string, dynamic> fields = [];
     private LoxClass Klass = klass;
+
+    public dynamic Get(Token name)
+    {
+        if (fields.TryGetValue(name.Lexeme, out dynamic? value))
+            return value;
+
+        throw new RuntimeError(name, $"Undefined property {name.Lexeme}.");
+    }
 
     public override string ToString()
     {
