e68c7051e1af02167f751db2fc6910270c16174a
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-20 00:46:09 +0100

    Add superclass validation in class statement handling in Interpreter

--- crafting_interpreters/jlox/jlox/Interpreter.cs
+++ crafting_interpreters/jlox/jlox/Interpreter.cs
@@ -344,6 +344,16 @@ public class Interpreter : Expr.IVisitor<object>, Statement.IVisitor<object?>
 
     object? Statement.IVisitor<object?>.VisitClassStmt(Statement.Class stmt)
     {
+        dynamic superclass = null;
+        if (stmt.superclass is not null)
+        {
+            superclass = Evaluate(stmt.superclass);
+            if (superclass is not LoxClass)
+                throw new RuntimeError(stmt.superclass.name, "Superclass must be a class.");
+        }
+
+
+
         _env.Define(stmt.name.Lexeme, null);
 
         var methods = new Dictionary<string, LoxFunction>();
