9826566ed753a7aaa4b16151a976fa35bf1d0349
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-19 22:03:58 +0100

    Implement LoxCallable interface in LoxClass and add instance creation logic

--- crafting_interpreters/jlox/jlox/LoxClass.cs
+++ crafting_interpreters/jlox/jlox/LoxClass.cs
@@ -1,8 +1,21 @@
+
 namespace jlox;
 
-public class LoxClass(string name)
+public class LoxClass(string name) : ILoxCallable
 {
     public readonly string Name = name;
+
+    public int Arity()
+    {
+        return 0;
+    }
+
+    public dynamic? Call(Interpreter interpreter, List<dynamic> arguments)
+    {
+        LoxInstance instance = new(this);
+        return instance;
+    }
+
     public override string ToString()
     {
         return Name;
