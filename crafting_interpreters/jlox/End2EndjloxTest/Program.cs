f8b498a548b76207a6dcdbf5c3abdb9b0aceacf9
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-20 11:23:46 +0100

    Add end-to-end test for class inheritance and closures in Jlox

--- crafting_interpreters/jlox/End2EndjloxTest/Program.cs
+++ crafting_interpreters/jlox/End2EndjloxTest/Program.cs
@@ -1,19 +1,61 @@
 ﻿using jlox;
 
-var inheritanceCall =
-        """
-        class Doughnut 
-        { 
-            cook() 
-            { 
-                print "Fry until golden brown."; 
-            } 
-        
-        } 
-        class BostonCream < Doughnut {} 
-        BostonCream().cook();
-        """;
+var source =
+"""
+class Doughnut {
+  init(flavor) {
+    this.flavor = flavor;
+    this.eaten = false;
+  }
+
+  eat() {
+    print "Chomp chomp " + this.flavor + " doughnut.";
+    this.eaten = true;
+  }
+
+  isEaten() {
+    return this.eaten;
+  }
+}
+
+class Cruller < Doughnut {
+  init(flavor, sprinkles) {
+    super.init(flavor);
+    this.sprinkles = sprinkles;
+  }
+
+  eat() {
+    super.eat();
+    print "Sprinkles go everywhere!";
+  }
+
+  describe() {
+      return "A cruller is a twisted doughnut.";
+  }
+}
+   
+var glazed = Doughnut("Glazed");
+glazed.eat();
+
+fun makeCounter() {
+  var i = 0;
+  fun count() {
+    i = i + 1;
+    print i;
+  }
+  return count;
+}
+
+var counter = makeCounter();
+counter(); // 1
+counter(); // 2
+counter(); // 3
+
+var anotherCounter = makeCounter();
+anotherCounter(); // 1 - should be independent
+
+""";
 
 var lox = new Lox();
 
-lox.Run(inheritanceCall);
+lox.Run(source);
