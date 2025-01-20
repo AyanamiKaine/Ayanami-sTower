58336e5d8cf9e29464df50fea62d1aba7f07519b
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-20 11:26:29 +0100

    Add OpCode enum to define bytecode operation codes in Chunk

---
+++ crafting_interpreters/jlox/clox/Chunk.cs
@@ -0,0 +1,13 @@
+namespace clox;
+
+
+/// <summary>
+/// In our bytecode format, each instruction has a one-byte operation code. Shortend to opcode.
+/// </summary>
+public enum OpCode
+{
+    /// <summary>
+    /// Return from the current function
+    /// </summary>
+    OP_RETURN,
+}
