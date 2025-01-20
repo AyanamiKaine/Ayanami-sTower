bffc1d027b0be3d208b5d706a2e670e682af63d5
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-20 13:15:53 +0100

    Add test for adding long constants to ByteChunk

--- crafting_interpreters/jlox/cloxUnitTests/ByteChunkTest.cs
+++ crafting_interpreters/jlox/cloxUnitTests/ByteChunkTest.cs
@@ -29,7 +29,22 @@ public class ByteChunkTest
         chunk.Write(OpCode.OP_RETURN);
 
         var actualOpCodes = chunk.DisassembleAllOpcodes();
-        var prettyChunk = chunk.ToString();
+        Assert.Equal(expectedOpcodes, actualOpCodes);
+    }
+
+    [Fact]
+    public void AddingConstantsLongToChunkTest()
+    {
+        List<OpCode?> expectedOpcodes = [OpCode.OP_CONSTANT_LONG, OpCode.OP_RETURN];
+
+        var chunk = new Chunk("Test Chunk");
+        var index = chunk.AddConstant(1.2);
+
+        chunk.Write(OpCode.OP_CONSTANT_LONG);
+        chunk.Write(index);
+        chunk.Write(OpCode.OP_RETURN);
+
+        var actualOpCodes = chunk.DisassembleAllOpcodes();
         Assert.Equal(expectedOpcodes, actualOpCodes);
     }
 }
