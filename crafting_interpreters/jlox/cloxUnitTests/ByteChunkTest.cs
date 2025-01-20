3656baaa7dd1f4da005711c1eb5cd5b041e22e4a
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-20 12:57:07 +0100

    Refactor ByteChunkTest to validate multiple opcodes and simplify constant addition tests

--- crafting_interpreters/jlox/cloxUnitTests/ByteChunkTest.cs
+++ crafting_interpreters/jlox/cloxUnitTests/ByteChunkTest.cs
@@ -7,67 +7,29 @@ public class ByteChunkTest
     [Fact]
     public void WriteToChunk()
     {
-        var expectedOpcode = OpCode.OP_RETURN;
+        List<OpCode?> expectedOpcodes = [OpCode.OP_RETURN];
 
         var chunk = new Chunk("Test Chunk");
         chunk.Write(OpCode.OP_RETURN);
-        var actualOpCode = chunk.Disassemble();
+        var actualOpCodes = chunk.DisassembleAllOpcodes();
 
-        Assert.Equal(expectedOpcode, actualOpCode);
+        Assert.Equal(expectedOpcodes, actualOpCodes);
     }
 
     [Fact]
-    public void AddingConstantsToChunkTest1()
+    public void AddingConstantsToChunkTest()
     {
-        var expectedOpcode = OpCode.OP_CONSTANT;
+        List<OpCode?> expectedOpcodes = [OpCode.OP_CONSTANT, OpCode.OP_RETURN];
 
         var chunk = new Chunk("Test Chunk");
         var index = chunk.AddConstant(1.2);
 
         chunk.Write(OpCode.OP_CONSTANT);
-        var actualOpCode = chunk.Disassemble();
-
         chunk.Write(index);
         chunk.Write(OpCode.OP_RETURN);
 
+        var actualOpCodes = chunk.DisassembleAllOpcodes();
 
-        Assert.Equal(expectedOpcode, actualOpCode);
-    }
-
-    [Fact]
-    public void AddingConstantsToChunkTest2()
-    {
-        var expectedOpcode = OpCode.OP_RETURN;
-
-        var chunk = new Chunk("Test Chunk");
-        var index = chunk.AddConstant(1.2);
-
-        chunk.Write(OpCode.OP_CONSTANT);
-
-        chunk.Write(index);
-        chunk.Write(OpCode.OP_RETURN);
-        var actualOpCode = chunk.Disassemble();
-
-
-        Assert.Equal(expectedOpcode, actualOpCode);
-    }
-
-    [Fact]
-    public void AddingConstantsToChunkTest3()
-    {
-        var expectedOpcode = OpCode.OP_CONSTANT;
-
-        var chunk = new Chunk("Test Chunk");
-        var index = chunk.AddConstant(1.2);
-
-        chunk.Write(OpCode.OP_CONSTANT);
-
-        chunk.Write(index);
-        var actualOpCode = chunk.Disassemble();
-
-        chunk.Write(OpCode.OP_RETURN);
-
-
-        Assert.Equal(expectedOpcode, actualOpCode);
+        Assert.Equal(expectedOpcodes, actualOpCodes);
     }
 }
