e5ba22e140e541c2b62a3c4258c6d304ec6fa104
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-20 14:42:57 +0100

    Add unit test for adding ADD instruction to Chunk

--- crafting_interpreters/jlox/cloxUnitTests/ByteChunkTest.cs
+++ crafting_interpreters/jlox/cloxUnitTests/ByteChunkTest.cs
@@ -33,6 +33,26 @@ public class ByteChunkTest
     }
 
     [Fact]
+    public void AddingAddInstructionToChunkTest()
+    {
+        List<OpCode?> expectedOpcodes = [OpCode.OP_CONSTANT, OpCode.OP_CONSTANT, OpCode.OP_ADD, OpCode.OP_RETURN];
+
+        var chunk = new Chunk("Test Chunk");
+        var index1 = chunk.AddConstant(1.2);
+        var index2 = chunk.AddConstant(5);
+
+        chunk.Write(OpCode.OP_CONSTANT);
+        chunk.Write(index1);
+        chunk.Write(OpCode.OP_CONSTANT);
+        chunk.Write(index2);
+        chunk.Write(OpCode.OP_ADD);
+        chunk.Write(OpCode.OP_RETURN);
+
+        var actualOpCodes = chunk.DisassembleAllOpcodes();
+        Assert.Equal(expectedOpcodes, actualOpCodes);
+    }
+
+    [Fact]
     public void AddingNegateOpCodeToChunkTest()
     {
         List<OpCode?> expectedOpcodes = [OpCode.OP_CONSTANT, OpCode.OP_NEGATE, OpCode.OP_RETURN];
