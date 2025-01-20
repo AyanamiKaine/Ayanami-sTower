62a8efcc02c42033ac3de26065a41a6f39eda8bf
Ayanami Kaine <patrick.grohs@hhu.de>
2025-01-20 14:42:54 +0100

    Add unit test for simple math operations in VM

--- crafting_interpreters/jlox/cloxUnitTests/VMTest.cs
+++ crafting_interpreters/jlox/cloxUnitTests/VMTest.cs
@@ -3,5 +3,22 @@ namespace cloxUnitTests;
 
 public class VMTest
 {
+    [Fact]
+    public void SimpleMathTest()
+    {
+        var chunk = new Chunk();
 
+        chunk.Write(OpCode.OP_CONSTANT);
+        chunk.Write(chunk.AddConstant(1.2));
+        chunk.Write(OpCode.OP_CONSTANT);
+        chunk.Write(chunk.AddConstant(3.4));
+        chunk.Write(OpCode.OP_CONSTANT);
+        chunk.Write(chunk.AddConstant(5.6));
+        chunk.Write(OpCode.OP_DIVIDE);
+        chunk.Write(OpCode.OP_NEGATE);
+        chunk.Write(OpCode.OP_RETURN);
+
+        var vm = new LoxVM(chunk);
+        vm.Interpret();
+    }
 }
