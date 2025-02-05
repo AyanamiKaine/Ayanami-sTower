f165daf94e63e5c121fffb9e00d372c8c6360db9
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-05 11:54:03 +0100

    Implement World class with SQLite connection and optimization features

--- StellaECS/StellaECS/World.cs
+++ StellaECS/StellaECS/World.cs
@@ -1,9 +1,88 @@
+using Microsoft.Data.Sqlite;
+
 namespace StellaECS;
 
 /// <summary>
 /// Represents the current database.
 /// </summary>
-public class World
+public class World : IDisposable
 {
+    private readonly SqliteConnection _connection;
+    private bool _disposed;
+    private readonly string _connectionString = string.Empty;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="World"/> class.
+    /// </summary>
+    /// <param name="name">The name of the database.</param>
+    public World(string name)
+    {
+        var connectionString = $"Data Source={name}.db";
+        _connection = new SqliteConnection(connectionString);
+        _connection.Open();
+
+        DatabaseOptimizations();
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="World"/> class with an in-memory database.
+    /// Useful for temporary data that does not need to persit.
+    /// </summary>
+    public World()
+    {
+        const string connectionString = "Data Source=:memory:";
+        _connection = new SqliteConnection(connectionString);
+        _connection.Open();
+
+        DatabaseOptimizations();
+    }
+
+    /// <summary>
+    /// Optimizes the database for local concurrent performance.
+    /// </summary>
+    private void DatabaseOptimizations()
+    {
+        using var command = _connection.CreateCommand();
+        command.CommandText = $@"
+                PRAGMA journal_mode=WAL;
+                PRAGMA page_size = {32768};
+                PRAGMA synchronous = normal;
+                PRAGMA temp_store = memory;
+                PRAGMA mmap_size = 30000000000;
+                ";
+        command.ExecuteNonQuery();
+    }
+
+    /// <summary>
+    /// Releases the unmanaged resources used by the World and optionally releases the managed resources.
+    /// </summary>
+    /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                _connection?.Dispose();
+            }
+            _disposed = true;
+        }
+    }
+
+    /// <summary>
+    /// Releases all resources used by the World.
+    /// </summary>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Finalizes an instance of the World class.
+    /// </summary>
+    ~World()
+    {
+        Dispose(false);
+    }
 }
