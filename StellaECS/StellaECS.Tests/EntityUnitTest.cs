d07966c15632696c91053d1d560a643e620f438c
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-05 12:28:25 +0100

    Add unit tests for entity creation and properties in EntityUnitTest

---
+++ StellaECS/StellaECS.Tests/EntityUnitTest.cs
@@ -0,0 +1,44 @@
+﻿namespace StellaECS.Tests;
+
+public class EntityUnitTest
+{
+    [Fact]
+    public void CreationShouldReturnNewEntity()
+    {
+        World world = new();
+        var entity = world.CreateEntity();
+
+        var expectedEntityID = 1;
+        var actualEntityID = entity.ID;
+
+        Assert.Equal(expectedEntityID, actualEntityID);
+    }
+
+    [Fact]
+    public void EntityDefineName()
+    {
+        World world = new();
+        var entity = world.CreateEntity("Cool Name");
+
+        var expectedEntityName = "Cool Name";
+        var actualEntityName = entity.Name;
+
+        Assert.Equal(expectedEntityName, actualEntityName);
+    }
+
+    [Fact]
+    public void DefineMultipleEntites()
+    {
+        World world = new();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            world.CreateEntity();
+        }
+
+        var expectedNumberOfEntities = 1000;
+        var actualEntityNumber = world.Size;
+
+        Assert.Equal(expectedNumberOfEntities, actualEntityNumber);
+    }
+}
