1a731c140349ff154b63f0dda47f098919dca0be
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-05 12:28:30 +0100

    Add World reference and Name property to Entity class

--- StellaECS/StellaECS/Entity.cs
+++ StellaECS/StellaECS/Entity.cs
@@ -4,10 +4,25 @@
 /// <summary>
 /// Represents an entry in the database.
 /// </summary>
-public class Entity(int id)
+public class Entity(int id, World world)
 {
     /// <summary>
     /// Represent the ID used to lookup data related to the entity in the database.
     /// </summary>
     public int ID { get; } = id;
+    /// <summary>
+    /// Gets the name of the entity.
+    /// </summary>
+    public string Name
+    {
+        get
+        {
+            return World.GetEntityName(this);
+        }
+    }
+
+    /// <summary>
+    /// Represents the world the entity belongs to.
+    /// </summary>
+    public World World { get; } = world;
 }
