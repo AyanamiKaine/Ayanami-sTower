6d4a15307b4cc6bb92f85d071e4234614b176a43
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-11 11:59:15 +0100

    Add unit test for query matching against rules in QueryUnitTest

--- stella_fuzzy_pattern_matcher/SFPM.Tests/QueryUnitTest.cs
+++ stella_fuzzy_pattern_matcher/SFPM.Tests/QueryUnitTest.cs
@@ -13,9 +13,43 @@ public class QueryUnitTest
     {
         var query = new Query();
 
+        // In this example we already populate the query with data,
+        // and dont actually look it up.
         query
             .Add("concept", "OnHit")
             .Add("attacker", "Hunter")
             .Add("damage", 12.4);
+    }
+
+
+    /// <summary>
+    /// We want to match a query, against a set of rules and
+    /// select the rule that matches the most and execute its payload.
+    /// </summary>
+    [Fact]
+    public void QueryMatchingARule()
+    {
+        var query = new Query();
+
+        query
+            .Add("concept", "OnHit")
+            .Add("attacker", "Hunter")
+            .Add("damage", 12.4);
+
+
+        List<Rule> rules = [
+                new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                ], ()=>{}),
+
+                new Rule([
+                    new Criteria<string>("attacker", attacker => { return attacker == "Hunter"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                    new Criteria<double>("damage", damage => { return damage == 12.4; }),
+                ], ()=>{}),
+            ];
+
+        query.Match(rules);
     }
 }
