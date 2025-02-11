4cc3e6da25e8d3910eea1bbd7d5ee9ad5e41fb47
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-11 12:35:05 +0100

    Implement rule matching logic in Query class to prioritize execution based on criteria count and rule priority

--- stella_fuzzy_pattern_matcher/SFPM/Query.cs
+++ stella_fuzzy_pattern_matcher/SFPM/Query.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace SFPM;
 
 /// <summary>
@@ -16,5 +18,47 @@ public class Query()
     {
         _queryData.Add(key, value);
         return this;
+    }
+
+    /// <summary>
+    /// Matches a query against a list of rules, it tries to select a rule that matches the most
+    /// (i.e. the most criteria) as its more specific. Then it runs the payload of the rule.
+    /// </summary>
+    /// <param name="rules"></param>
+    public void Match(List<Rule> rules)
+    {
+        // Sort rules by criteria count in descending order (highest count first)
+        rules.Sort((a, b) => b.CriteriaCount.CompareTo(a.CriteriaCount));
+        // Rules with the most criteria will be at the front of the list       
+        var acceptedRules = new List<Rule>();
+        var currentHighestScore = 0;
+        foreach (var rule in rules)
+        {
+            // When we already matched a rule that is the most specifc rule
+            // we have, we stop matching other rules. 
+            // Avoiding linear growth in the best case.
+            // In the worst case allowing it.
+            if (rule.CriteriaCount < currentHighestScore)
+                break;
+
+            var (matched, matchedCriteriaCount) = rule.StrictEvaluate(_queryData);
+            if (matched)
+            {
+                acceptedRules.Add(rule);
+                if (matchedCriteriaCount > currentHighestScore)
+                    currentHighestScore = matchedCriteriaCount;
+            }
+        }
+
+        if (acceptedRules.Count == 1)
+        {
+            acceptedRules[0].ExecutePayload();
+        }
+        else if (acceptedRules.Count > 1)
+        {
+            // Sort by priority in descending order and execute the highest priority rule
+            var highestPriorityRule = acceptedRules.OrderByDescending(r => r.Priority).First();
+            highestPriorityRule.ExecutePayload();
+        }
     }
 }
