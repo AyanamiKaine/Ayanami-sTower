18e4d37a3df689b3a00f1ab5e162e50c742b9399
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-11 15:13:11 +0100

    Add example project for fuzzy pattern matching with sample rules and queries

---
+++ stella_fuzzy_pattern_matcher/SFPM.Example/Program.cs
@@ -0,0 +1,58 @@
+﻿using SFPM;
+
+/*
+Here we define a list of facts we want to match against a list of rules.
+*/
+
+var query = new Query()
+    .Add("who", "Nick")
+    .Add("concept", "onHit")
+    .Add("curMap", "circus")
+    .Add("health", 0.66)
+    .Add("nearAllies", 2)
+    .Add("hitBy", "zombieClown");
+
+
+/*
+Here we define a list of rules that are composed out of a list of criteria.
+*/
+List<Rule> rules = [
+            new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                ], ()=>{
+                    Console.WriteLine("Ouch");
+                }),
+            new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                    new Criteria<int>("nearAllies", nearAllies => { return nearAllies > 1; }),
+                ], ()=>{
+                    Console.WriteLine("ow help!");
+                }),
+            new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                    new Criteria<string>("curMap", curMap => { return curMap == "circus"; }),
+                ], ()=>{
+                    Console.WriteLine("This Circus Sucks!");
+                }),
+            new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                    new Criteria<string>("hitBy", hitBy => { return hitBy == "zombieClown"; }),
+                ], ()=>{
+                    Console.WriteLine("Stupid Clown!");
+                }),
+            new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                    new Criteria<string>("hitBy", hitBy => { return hitBy == "zombieClown"; }),
+                    new Criteria<string>("curMap", curMap => { return curMap == "circus"; }),
+                ], ()=>{
+                    Console.WriteLine("I hate circus clowns!");
+                }),
+        ];
+
+// Should print "I hate circus clowns!"
+query.Match(rules);
