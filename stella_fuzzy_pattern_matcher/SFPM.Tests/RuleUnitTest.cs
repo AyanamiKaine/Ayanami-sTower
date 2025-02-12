3567c6a3ec57fe990f194837d5fb5ecd236a720c
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-12 10:43:49 +0100

    Remove redundant tests from RuleUnitTest to streamline test suite

--- stella_fuzzy_pattern_matcher/SFPM.Tests/RuleUnitTest.cs
+++ stella_fuzzy_pattern_matcher/SFPM.Tests/RuleUnitTest.cs
@@ -12,27 +12,6 @@ public class RuleUnitTest
         LogManager.ReconfigExistingLoggers();
     }
 
-
-    [Fact]
-    public void Creation()
-    {
-        var SFPM = new SFPM();
-        Assert.NotNull(SFPM);
-    }
-
-    /// <summary>
-    /// When we use the pattern match we want to be able to see how each rule is scored.
-    /// 
-    /// This is important because we want to be able to say select the rule that matches the most
-    /// does not need to match every condition in the rule. Also this makes it so rules with more
-    /// conditions (i.e. that are more specific) are valued more than general once.
-    /// </summary>
-    [Fact]
-    public void Scoring()
-    {
-
-    }
-
     [Fact]
     public void SimpleOneRuleTwoCriteriaStrictMatch()
     {
