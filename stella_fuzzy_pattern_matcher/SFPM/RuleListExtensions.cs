e6cc519ef5243f3b4e30301d750cfb9c6265cfdb
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-12 22:07:34 +0100

    Add MostSpecificRule extension method to retrieve the rule with the highest criteria count

--- stella_fuzzy_pattern_matcher/SFPM/RuleListExtensions.cs
+++ stella_fuzzy_pattern_matcher/SFPM/RuleListExtensions.cs
@@ -16,4 +16,22 @@ public static class RuleListExtensions
     {
         rules.Sort((a, b) => b.CriteriaCount.CompareTo(a.CriteriaCount));
     }
+
+    /// <summary>
+    /// Returns the rule with the highest number of criteria from the list.
+    /// </summary>
+    /// <param name="rules">The list of rules to search. Must not be empty.</param>
+    /// <returns>The rule with the most criteria from the list.</returns>
+    /// <exception cref="ArgumentException">Thrown when the rules list is empty.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when rules parameter is null.</exception>
+    /// <remarks>
+    /// This method uses LINQ's MaxBy operation to find the rule with the highest criteria count.
+    /// The operation runs in O(n) time where n is the number of rules in the list.
+    /// </remarks>
+    public static Rule MostSpecificRule(this List<Rule> rules)
+    {
+        if (rules.Count == 0)
+            throw new ArgumentException("Rules list cannot be empty.", nameof(rules));
+        return rules.MaxBy(r => r.CriteriaCount)!;
+    }
 }
