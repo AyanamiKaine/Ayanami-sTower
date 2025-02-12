90c2590a3d13801dc86f7ec01a89064802bd29d1
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-12 13:09:11 +0100

    Refactor UnitTest1 to use record structs for Player, Health, Position, Name, and Map; enhance rule evaluation with updated criteria and assertions

--- stella_fuzzy_pattern_matcher/SFPM.Flecs.Tests/UnitTest1.cs
+++ stella_fuzzy_pattern_matcher/SFPM.Flecs.Tests/UnitTest1.cs
@@ -4,11 +4,17 @@ namespace SFPM.Flecs.Tests;
 
 
 public struct NPC { };
-public struct Player { };
-public struct Health(int Value);
-public struct Position(int X, int Y);
-public struct Name(string Value);
-
+public record struct Player { };
+public record struct Health(int Value);
+public record struct Position(int X, int Y);
+public record struct Name(string Value) : IComparable<Name>
+{
+    public readonly int CompareTo(Name other) => Value.CompareTo(other.Value);
+}
+public record struct Map(string Name) : IComparable<Map>
+{
+    public readonly int CompareTo(Map other) => Name.CompareTo(other.Name);
+}
 public class UnitTest1
 {
 
@@ -19,6 +25,7 @@ public class UnitTest1
     public void BasicIdea()
     {
         World world = World.Create();
+        world.Set(new Map("circus"));
 
         var ruleExecuted = false;
         var player = world.Entity();
@@ -49,7 +56,7 @@ public class UnitTest1
                     new Criteria<string>("concept", concept => { return concept == "onHit"; }),
                 ], () =>
                 {
-                    ruleExecuted = true;
+                    ruleExecuted = false;
                 }));
 
         rule2.Set<NPC, Rule>(new Rule([
@@ -62,12 +69,16 @@ public class UnitTest1
                 }));
 
         rule3.Set<NPC, Rule>(new Rule([
-                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<Name>("who", who => { return who.Value == "Nick"; }),
                     new Criteria<string>("concept", concept => { return concept == "onHit"; }),
-                    new Criteria<string>("curMap", curMap => { return curMap == "circus"; }),
+                    new Criteria<Map>("curMap", curMap => { return curMap.Name == "circus"; }),
                 ], () =>
                 {
-                    ruleExecuted = false;
+                    // In the payload of rules we can interact with the ecs world,
+                    // here we simulate a map change.
+                    ref Map curMap = ref world.GetMut<Map>();
+                    curMap.Name = "AirPort";
+                    ruleExecuted = true;
                 }));
 
         rule4.Set<NPC, Rule>(new Rule([
@@ -79,8 +90,15 @@ public class UnitTest1
                     ruleExecuted = false;
                 }));
 
+        /*
+        We need to move the entire query logic into a function, that takes as an argument an dictonary of <string, object>, it 
+        represents a list of facts.
+        */
+
         var acceptedRules = new List<Rule>();
         var currentHighestScore = 0;
+
+
 
         world.Each<NPC, Rule>((Entity entity, ref Rule rule) =>
         {
@@ -94,8 +112,9 @@ public class UnitTest1
             */
             var (matched, matchedCriteriaCount) = rule.StrictEvaluate(new Dictionary<string, object>
             {
-                { "concept", "onHit" },
-                { "who", "Nick"}
+                { "concept",    "onHit" },
+                { "who",        player.Get<Name>()}, // Here we query the data from an entity and its component
+                { "curMap",     world.Get<Map>()}    // If the component data changes it gets automaticall reflected here
             });
             if (matched)
             {
@@ -128,5 +147,6 @@ public class UnitTest1
         }
 
         Assert.True(ruleExecuted);
+        Assert.Equal("AirPort", world.Get<Map>().Name);
     }
 }
