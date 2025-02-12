4ae9afe613c412d069a1719f39eece6a33d1ee15
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-12 22:12:40 +0100

    Add unit tests for MostSpecificRule and LeastSpecificRule methods in RuleListExtensions

---
+++ stella_fuzzy_pattern_matcher/SFPM.Tests/RuleListExtensionsUnitTest.cs
@@ -0,0 +1,75 @@
+using NLog;
+
+namespace SFPM.Tests;
+
+public class RuleListExtensionsUnitTest
+{
+    public RuleListExtensionsUnitTest()
+    {
+        // Deactive Logging for unit tests
+        LogManager.Configuration.LoggingRules.Clear();
+        LogManager.ReconfigExistingLoggers();
+    }
+
+    [Fact]
+    public void MostSpecificRuleTest()
+    {
+        List<Rule> rules = [
+                  new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                ], ()=>{
+                }),
+          new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<int>("nearAllies", nearAllies => { return nearAllies > 1; }),
+                ], ()=>{
+                }),
+          new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                    new Criteria<string>("curMap", curMap => { return curMap == "circus"; }),
+                ], ()=>{
+                }),
+          new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                ], ()=>{
+                }),
+        ];
+
+        var mostSpecificRule = rules.MostSpecificRule();
+
+        Assert.Equal(3, mostSpecificRule.CriteriaCount);
+    }
+
+    [Fact]
+    public void LeastSpecificRuleTest()
+    {
+        List<Rule> rules = [
+                  new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                ], ()=>{
+                }),
+          new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<int>("nearAllies", nearAllies => { return nearAllies > 1; }),
+                ], ()=>{
+                }),
+          new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                    new Criteria<string>("curMap", curMap => { return curMap == "circus"; }),
+                ], ()=>{
+                }),
+          new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                ], ()=>{
+                }),
+        ];
+
+        var mostSpecificRule = rules.LeastSpecificRule();
+
+        Assert.Equal(1, mostSpecificRule.CriteriaCount);
+    }
+}
