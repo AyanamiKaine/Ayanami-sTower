763ed95e23327c3fa898cb8d68992497f4689160
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-14 15:23:31 +0100

    Add unit test for matching rules in RuleListExtensionsUnitTest

--- stella_fuzzy_pattern_matcher/SFPM.Tests/RuleListExtensionsUnitTest.cs
+++ stella_fuzzy_pattern_matcher/SFPM.Tests/RuleListExtensionsUnitTest.cs
@@ -72,4 +72,43 @@ public class RuleListExtensionsUnitTest
 
         Assert.Equal(1, mostSpecificRule.CriteriaCount);
     }
+
+    [Fact]
+    public void MatchOnRulesList()
+    {
+        var ruleExecuted = false;
+
+        List<Rule> rules = [
+          new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                ], ()=>{
+                }),
+          new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<int>("nearAllies", nearAllies => { return nearAllies > 1; }),
+                ], ()=>{
+                }),
+          new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                    new Criteria<string>("concept", concept => { return concept == "onHit"; }),
+                    new Criteria<string>("curMap", curMap => { return curMap == "circus"; }),
+                ], ()=>{
+                    ruleExecuted = true;
+                }),
+          new Rule([
+                    new Criteria<string>("who", who => { return who == "Nick"; }),
+                ], ()=>{
+                }),
+        ];
+
+        rules.Match(new Dictionary<string, object>
+            {
+                { "concept",    "onHit" },
+                { "who",        "Nick"},
+                { "curMap",     "circus"}
+            });
+
+        Assert.True(ruleExecuted);
+    }
 }
