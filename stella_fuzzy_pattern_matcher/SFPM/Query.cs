ef79f43cf4f95af723235b93729107359d363275
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-14 13:19:38 +0100

    Enhance debug logging in Query class to include count of accepted rules for better traceability

--- stella_fuzzy_pattern_matcher/SFPM/Query.cs
+++ stella_fuzzy_pattern_matcher/SFPM/Query.cs
@@ -74,7 +74,7 @@ public class Query()
         }
         else if (acceptedRules.Count > 1)
         {
-            logger.ConditionalDebug("SFPM.Query.Match: More than one rule with the same number of criteria matched. Grouping them by priority, selecting the highest priority one, if multiple rules have the same priority selecting a random one.");
+            logger.ConditionalDebug($"SFPM.Query.Match: More than one rule with the same number of criteria matched({acceptedRules.Count}). Grouping them by priority, selecting the highest priority one, if multiple rules have the same priority selecting a random one.");
             // Group highest priority rules
             var highestPriorityRules = acceptedRules.GroupBy(r => r.Priority)
                                                    .OrderByDescending(g => g.Key)
