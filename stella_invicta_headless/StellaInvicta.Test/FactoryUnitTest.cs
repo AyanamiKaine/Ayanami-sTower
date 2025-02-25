47eeb9c4c1fc894afa9e292ee68e6c5283643be5
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-25 17:10:57 +0100

    Add initial unit tests for Factory and Population classes

---
+++ stella_invicta_headless/StellaInvicta.Test/FactoryUnitTest.cs
@@ -0,0 +1,10 @@
+namespace StellaInvicta.Test;
+
+public class FactoryUnitTest
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
--- stella_invicta_headless/StellaInvicta.Test/PopulationUnitTest.cs
+++ stella_invicta_headless/StellaInvicta.Test/PopulationUnitTest.cs
@@ -1,6 +1,6 @@
 ﻿namespace StellaInvicta.Test;
 
-public class UnitTest1
+public class PopulationUnitTest
 {
     [Fact]
     public void Test1()
