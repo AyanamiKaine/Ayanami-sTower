d4191df78feca8d31c700ed17c3ee22039f1e58b
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-25 17:17:38 +0100

    Add Rationality record struct to represent AI decision-making rationality

---
+++ stella_invicta_headless/StellaInvicta/Components/Rationality.cs
@@ -0,0 +1,7 @@
+namespace StellaInvicta.Components;
+
+/// <summary>
+/// Determines how rational a decision will be made by the ai.
+/// </summary>
+/// <param name="Value"></param>
+public record struct Rationality(double Value = 0);
