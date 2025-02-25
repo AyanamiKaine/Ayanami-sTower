f6d87d60ee4f6283c85b3176b0863159ad5420e2
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-25 17:54:16 +0100

    Add OwnedBy and Presence record structs to represent entity relationships

---
+++ stella_invicta_headless/StellaInvicta/Tags/Relationships/OwnedBy.cs
@@ -0,0 +1,6 @@
+namespace StellaInvicta.Tags.Relationships;
+
+/// <summary>
+/// Owned by Relationship will be <c>Ecs.Exclusive</c> can only be owned by one entity
+/// </summary>
+public struct OwnedBy;
---
+++ stella_invicta_headless/StellaInvicta/Tags/Relationships/Presence.cs
@@ -0,0 +1,10 @@
+namespace StellaInvicta.Tags.Relationships;
+
+/// <summary>
+/// Shows the relationship of that the entity 
+/// is presence at that location.
+/// 
+/// Entity planet.Add(Presence)(character)
+/// 
+/// </summary>
+public struct Presence;
