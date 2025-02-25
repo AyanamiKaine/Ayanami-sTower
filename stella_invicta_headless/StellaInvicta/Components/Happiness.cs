2eec85bcf182b23897874a7bae938df1ad497811
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-25 17:17:51 +0100

    Add Happiness record struct to represent overall satisfaction and well-being

---
+++ stella_invicta_headless/StellaInvicta/Components/Happiness.cs
@@ -0,0 +1,7 @@
+namespace StellaInvicta.Components;
+
+/// <summary>
+/// Overall satisfaction and well-being
+/// </summary>
+/// <param name="Value">Current Happiness</param>
+public record struct Happiness(double Value = 0);
