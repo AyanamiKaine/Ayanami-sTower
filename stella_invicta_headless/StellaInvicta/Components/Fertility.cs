8ce3e8d669cdb1411e195a43ce55b62f8362bca3
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-25 17:17:25 +0100

    Add Fertility record struct to represent character conception likelihood

---
+++ stella_invicta_headless/StellaInvicta/Components/Fertility.cs
@@ -0,0 +1,7 @@
+namespace StellaInvicta.Components;
+
+/// <summary>
+/// Fertility determines how likely an individual character is to conceive children.
+/// </summary>
+/// <param name="Value"></param>
+public record struct Fertility(double Value = 0);
