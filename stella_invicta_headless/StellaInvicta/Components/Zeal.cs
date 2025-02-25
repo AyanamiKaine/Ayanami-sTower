a3f661aff29b4f6a60783aa29ddc63fd3376143f
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-25 17:17:33 +0100

    Add Zeal record struct to represent AI decision-making related to religious or ideological beliefs

---
+++ stella_invicta_headless/StellaInvicta/Components/Zeal.cs
@@ -0,0 +1,7 @@
+namespace StellaInvicta.Components;
+
+/// <summary>
+/// Determines how zealous the decision of the ai are related to religious or ideological decisions.
+/// </summary>
+/// <param name="Value"></param>
+public record struct Zeal(double Value = 0);
