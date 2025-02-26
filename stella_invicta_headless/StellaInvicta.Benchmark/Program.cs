526d1cd3c7ad24902176efbe008f3c0c994944ae
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-26 12:35:44 +0100

    Update target framework to net9.0 and add benchmarking project for performance analysis

---
+++ stella_invicta_headless/StellaInvicta.Benchmark/Program.cs
@@ -0,0 +1,58 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+using Perfolizer.Horology;
+using Flecs.NET.Core;
+using StellaInvicta;
+
+namespace StellaInvicta.Benchmark;
+public record struct Name(string Value) : IComparable<Name>
+{
+    public readonly int CompareTo(Name other) => Value.CompareTo(strB: other.Value);
+}
+public record struct Map(string Name) : IComparable<Map>
+{
+    public readonly int CompareTo(Map other) => Name.CompareTo(strB: other.Name);
+}
+
+public record struct Health(int Value);
+public record struct Position(int X, int Y);
+
+[SimpleJob(runtimeMoniker: RuntimeMoniker.Net90)]
+[MemoryDiagnoser]
+[Orderer(summaryOrderPolicy: SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class StellaInvictaBenchmarks
+{
+    World world;
+
+    [IterationSetup]
+    public void Setup()
+    {
+        world = World.Create();
+        world.Import<StellaInvictaECSModule>();
+    }
+
+
+    [Benchmark]
+    public void ProgessOnce()
+    {
+        world.Progress();
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Otherwise it complains that flecs is not build in release,
+            // I dont think its true.
+            var config = ManualConfig.Create(config: DefaultConfig.Instance)
+                .WithOptions(options: ConfigOptions.DisableOptimizationsValidator);
+
+            BenchmarkRunner.Run<StellaInvictaBenchmarks>(config: config);
+        }
+    }
+}
