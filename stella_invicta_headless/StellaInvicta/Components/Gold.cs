5d81486c435ff34e68d1632a7c4ea04a6b13cf30
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-26 15:20:34 +0100

    Add Gold struct to represent gold quantity as a value type

---
+++ stella_invicta_headless/StellaInvicta/Components/Gold.cs
@@ -0,0 +1,6 @@
+namespace StellaInvicta.Components;
+/// <summary>
+/// Finite resource used for many things
+/// </summary>
+/// <param name="Quantity"></param>
+public record struct Gold(int Quantity);
