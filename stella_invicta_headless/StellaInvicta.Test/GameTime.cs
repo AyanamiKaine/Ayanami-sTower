545980371ad0dab347166e93b1677d3b09adce79
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-26 13:57:16 +0100

    Add unit test for GameTimeSystem to verify game date progression

---
+++ stella_invicta_headless/StellaInvicta.Test/GameTime.cs
@@ -0,0 +1,30 @@
+using Flecs.NET.Core;
+using StellaInvicta.Components;
+using StellaInvicta.Systems;
+using StellaInvicta.Tags.Relationships;
+
+namespace StellaInvicta.Test;
+
+public class GameTimeUnitTest
+{
+    [Fact]
+    public void GameTimeProgess()
+    {
+        World world = World.Create();
+
+        world.Import<StellaInvictaECSModule>();
+
+        world.Set<GameDate>(new(year: 0, month: 1, day: 1));
+
+        world.AddSystem(new GameTimeSystem());
+
+        var expectedGameDate = new GameDate(year: 0, month: 1, day: 30);
+
+        for (int i = 0; i < 29; i++)
+        {
+            world.Progress();
+        }
+
+        Assert.Equal(expectedGameDate, world.Get<GameDate>());
+    }
+}
