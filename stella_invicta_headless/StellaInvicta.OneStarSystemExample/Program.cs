26e343dade106780c980c7cfae9ca987c8cfb066
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-26 13:37:50 +0100

    Add AgeSystem and initialize Marina entity with age component

--- stella_invicta_headless/StellaInvicta.OneStarSystemExample/Program.cs
+++ stella_invicta_headless/StellaInvicta.OneStarSystemExample/Program.cs
@@ -1,4 +1,12 @@
 ﻿using Flecs.NET.Core;
+using StellaInvicta;
+using StellaInvicta.Components;
+using StellaInvicta.Systems;
 
 var world = World.Create();
-world.Import<StellaInvicta.StellaInvictaECSModule>();
+world.Import<StellaInvictaECSModule>();
+world.AddSystem(new AgeSystem());
+
+var Marina = world.Entity("Marina")
+    .Set<Name>(new("Marina"))
+    .Set<Age>(new(29));
