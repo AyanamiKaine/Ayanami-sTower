92d8fb107b867f4cb001fbb7f2a3b29ca0697a43
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-26 13:37:47 +0100

    Add aging functionality to CharacterUnitTest with GameTime and Age systems

--- stella_invicta_headless/StellaInvicta.Test/CharacterUnitTest.cs
+++ stella_invicta_headless/StellaInvicta.Test/CharacterUnitTest.cs
@@ -1,5 +1,7 @@
 using Flecs.NET.Core;
 using StellaInvicta.Components;
+using StellaInvicta.Systems;
+using StellaInvicta.Tags.Relationships;
 
 namespace StellaInvicta.Test;
 
@@ -9,13 +11,28 @@ public class CharacterUnitTest
     public void CharactersShouldAge()
     {
         World world = World.Create();
+
         world.Import<StellaInvictaECSModule>();
 
 
+        // Current Year
+        world.Set<GameDate>(new(year: 29, month: 12, day: 1));
+
+        world.AddSystem(new GameTimeSystem());
+        world.AddSystem(new AgeSystem());
+
         var Marina = world.Entity("Marina")
+            .Set<Birthday, GameDate>(new GameDate(year: 0, month: 1, day: 1))
             .Set<Name>(new("Marina"))
             .Set<Age>(new(29));
 
+        var expectedAge = 30;
 
+        for (int i = 0; i < 60; i++)
+        {
+            world.Progress();
+        }
+
+        Assert.Equal(expectedAge, Marina.Get<Age>().Value);
     }
 }
