abb4a6ac11b52ad92191e8d94fe9e065f4007044
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-26 11:57:59 +0100

    Add Galaxy struct to represent galaxy structure in celestial body hierarchy

---
+++ stella_invicta_headless/StellaInvicta/Tags/CelestialBodies/Galaxy.cs
@@ -0,0 +1,6 @@
+namespace StellaInvicta.Tags.CelestialBodies;
+/// <summary>
+/// Represents a galaxy structure in the celestial body hierarchy.
+/// A galaxy is a gravitationally bound system of stars, stellar remnants, interstellar gas, dust, and dark matter.
+/// </summary>
+public struct Galaxy;
