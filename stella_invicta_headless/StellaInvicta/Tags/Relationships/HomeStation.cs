16645fce677ee830156bf2064a4605c626e86860
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-26 11:43:09 +0100

    Add HomeStation tag component to define exclusive relationships for space ships

---
+++ stella_invicta_headless/StellaInvicta/Tags/Relationships/HomeStation.cs
@@ -0,0 +1,11 @@
+namespace StellaInvicta.Tags.Relationships;
+
+/// <summary>
+/// Shows to what entity a space ship is home to.
+/// We use this helper tag to easily start and bring 
+/// back space ships to their home station.
+/// 
+/// Its an exclusive relationship component.
+/// An entitiy has only one home station.
+/// </summary>
+public struct HomeStation;
