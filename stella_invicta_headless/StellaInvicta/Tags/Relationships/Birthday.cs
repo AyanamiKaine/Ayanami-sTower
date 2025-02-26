0609bb144a99971c45fa38ff48bd58008d460f9e
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-26 13:37:52 +0100

    Add Birthday struct to represent birthday relationship tags

---
+++ stella_invicta_headless/StellaInvicta/Tags/Relationships/Birthday.cs
@@ -0,0 +1,6 @@
+namespace StellaInvicta.Tags.Relationships;
+/// <summary>
+/// Represents a birthday relationship tag.
+/// used with gamedate component to indicate when someone or something was created/born.
+/// </summary>
+public struct Birthday;
