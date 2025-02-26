dde9ab715e76817eb19707cc89e68e5eee0a31fc
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-26 14:19:14 +0100

    Add unit tests for StarSystem connections and relationships

--- stella_invicta_headless/StellaInvicta.Test/StarSystemUnitTest.cs
+++ stella_invicta_headless/StellaInvicta.Test/StarSystemUnitTest.cs
@@ -1,4 +1,7 @@
 using Flecs.NET.Core;
+using NLog.LayoutRenderers;
+using StellaInvicta.Tags.CelestialBodies;
+using StellaInvicta.Tags.Relationships;
 
 namespace StellaInvicta.Test;
 
@@ -12,5 +15,40 @@ public class StarSystemUnitTest
     {
         World world = World.Create();
         world.Import<StellaInvictaECSModule>();
+
+
+        var galaxy = world.Entity("Andromeda")
+            .Add<Galaxy>();
+
+        var starSystem = world.Entity("Mexo-5")
+            .Add<Star>()
+            .Add<LocatedAt>(galaxy);
+
+        var planet = world.Entity("Tomaxa")
+            .Add<Planet>()
+            .Add<Orbits>(starSystem);
+
+    }
+
+    [Fact]
+    public void StarSystemsConnections()
+    {
+        World world = World.Create();
+        world.Import<StellaInvictaECSModule>();
+
+        var solarSystem = world.Entity("solarSystem")
+            .Add<Star>();
+
+        var alphaCentauri = world.Entity("alphaCentauri")
+            .Add<Star>()
+            .Add<ConnectedTo>(solarSystem);
+
+        // This star system is not connected to the solar system ONLY to alpha centauri
+        var NotConnectedStarSystem = world.Entity()
+            .Add<Star>()
+            .Add<ConnectedTo>(alphaCentauri);
+
+        Assert.True(solarSystem.ConnectedTo(alphaCentauri));
+        Assert.False(NotConnectedStarSystem.ConnectedTo(solarSystem));
     }
 }
