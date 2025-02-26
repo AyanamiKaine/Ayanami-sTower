cfe47e0a3241e2848c42af05fcf4ce28b3224849
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-26 15:24:47 +0100

    Add mining functionality to Asteroid entity with initial gold quantity

--- stella_invicta_headless/StellaInvicta.Test/AsteroidUnitTest.cs
+++ stella_invicta_headless/StellaInvicta.Test/AsteroidUnitTest.cs
@@ -1,5 +1,9 @@
+using System.Numerics;
 using Flecs.NET.Core;
+using StellaInvicta.Components;
 using StellaInvicta.Tags.CelestialBodies;
+using StellaInvicta.Tags.Identifiers;
+using StellaInvicta.Tags.Relationships;
 
 namespace StellaInvicta.Test;
 
@@ -11,8 +15,35 @@ public class AsteroidUnitTest
         World world = World.Create();
         world.Import<StellaInvictaECSModule>();
 
-        var asteroid = world.Entity()
-            .Add<Asteroid>();
+        var galaxy = world.Entity("Andromeda")
+            .Add<Galaxy>();
 
+        var starSystem = world.Entity("Mexo-5")
+            .Add<Star>()
+            .Set<Position, Vector3>(Vector3.Zero)
+            .Add<LocatedAt>(galaxy);
+
+        var planet = world.Entity("Tomaxa")
+            .Add<Planet>()
+            .Set<Quaternion>(new())
+            .Set<Mass>(new(1.0f))
+            .Set<Position, Vector3>(new Vector3(x: 20, y: 50, z: 70))
+            .Set<Velocity, Vector3>(new Vector3(x: 0, y: 0, z: 10))
+            .Add<Orbits>(starSystem);
+
+
+        var miningShip = world.Entity()
+            .Add<LocatedAt>(starSystem)
+            .Set<Position, Vector3>(new Vector3(x: 50, y: 0, z: 0))
+            .Add<Miner>();
+
+        var asteroid = world.Entity()
+            .Add<Asteroid>()
+            .Add<Orbits>(planet)
+            .Set<Position, Vector3>(new Vector3(x: 50, y: 0, z: 0))
+            .Set<Gold>(new(200));
+
+        // After the shiped mined some resources, 
+        // the astroid should have less gold than initally
     }
 }
