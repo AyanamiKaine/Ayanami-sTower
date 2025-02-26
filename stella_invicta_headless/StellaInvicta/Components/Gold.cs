ee08d3298b38160e047cd497d0bd0fcdd9009ded
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-26 19:55:59 +0100

    Implement IResource interface in Gold struct for resource management

--- stella_invicta_headless/StellaInvicta/Components/Gold.cs
+++ stella_invicta_headless/StellaInvicta/Components/Gold.cs
@@ -3,4 +3,19 @@ namespace StellaInvicta.Components;
 /// Finite resource used for many things
 /// </summary>
 /// <param name="Quantity"></param>
-public record struct Gold(int Quantity);
+public record struct Gold(int Quantity) : IResource
+{
+    /// <inheritdoc/>
+    public void Add(int value)
+    {
+        Quantity += value;
+    }
+
+    /// <inheritdoc/>
+    public int Subtract(int value)
+    {
+        var subtractedAmount = Math.Max(0, Quantity - value);
+        Quantity = subtractedAmount;
+        return subtractedAmount;
+    }
+}
