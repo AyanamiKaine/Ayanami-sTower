5d7688b0d8facdcef21e3cb742097dbc3d6a75bc
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-28 00:11:31 +0100

    Add LifeNeed structure to represent essential goods for population survival

---
+++ stella_invicta_headless/StellaInvicta/Tags/Identifiers/LifeNeeds.cs
@@ -0,0 +1,5 @@
+namespace StellaInvicta.Tags.Identifiers;
+/// <summary>
+/// Represents a life need. Goods that are needed every month otherwise the population will starve.
+/// </summary>
+public struct LifeNeed;
