16982a1eed54ff9496990d86903e7802a2a54f44
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-28 00:11:25 +0100

    Add PopType structure to define population types for entities

---
+++ stella_invicta_headless/StellaInvicta/Tags/Identifiers/PopType.cs
@@ -0,0 +1,5 @@
+namespace StellaInvicta.Tags.Identifiers;
+/// <summary>
+/// Used to mark an entity to be an poptype, when used in a pair .Add(PopType)(WorkerEntity) then it defines that the entity has the worker pop type.
+/// </summary>
+public struct PopType;
