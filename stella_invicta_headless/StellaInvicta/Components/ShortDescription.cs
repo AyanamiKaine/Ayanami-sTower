56536aa3a4f521334622155cae610dfaba7287af
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-27 18:44:30 +0100

    Add ShortDescription record struct to represent short descriptions

---
+++ stella_invicta_headless/StellaInvicta/Components/ShortDescription.cs
@@ -0,0 +1,7 @@
+namespace StellaInvicta.Components;
+
+/// <summary>
+/// Represents a Short description as a record struct.
+/// </summary>
+/// <param name="Value">The string value containing the Short description. Defaults to empty string if not specified.</param>
+public record struct ShortDescription(string Value = "");
