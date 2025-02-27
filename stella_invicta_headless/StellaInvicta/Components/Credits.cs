8fee4847adf684e1fe32b706231f6143a19b78a5
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-27 18:49:24 +0100

    Add Credits record struct to represent in-game currency

---
+++ stella_invicta_headless/StellaInvicta/Components/Credits.cs
@@ -0,0 +1,11 @@
+namespace StellaInvicta.Components;
+
+
+/// <summary>
+/// Represents a universally used value type for handling in-game currency (Credits).
+/// </summary>
+/// <remarks>
+/// Credits are implemented as a record struct for efficient value-based operations and immutability.
+/// </remarks>
+/// <param name="Ammount">The numerical value of credits. Defaults to 0 if not specified.</param>
+public record struct Credits(double Ammount = 0);
