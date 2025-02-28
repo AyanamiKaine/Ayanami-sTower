15abc49d89de3516da85be30d9c9fa229f785e54
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-28 12:01:25 +0100

    Refactor Quantity struct to ensure non-negative values and improve encapsulation

--- stella_invicta_headless/StellaInvicta/Components/Quantity.cs
+++ stella_invicta_headless/StellaInvicta/Components/Quantity.cs
@@ -1,7 +1,30 @@
 namespace StellaInvicta.Components;
 
 /// <summary>
-/// Represents a Quantity value as an integer.
+/// Represents a Quantity value as an integer that's never negative.
 /// </summary>
-/// <param name="Value">The size value. Defaults to 0 if not specified.</param>
-public record struct Quantity(int Value = 0);
+public record struct Quantity
+{
+    private int _value; // Removed readonly
+    
+    /// <summary>
+    /// Gets or sets the current numeric value of the quantity.
+    /// </summary>
+    /// <value>The integer value representing this quantity.</value>
+    /// <remarks>Setting a negative value will result in a value of 0.</remarks>
+    public int Value 
+    { 
+        get => _value;
+        set => _value = Math.Max(0, value);
+    }
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Quantity"/> class.
+    /// </summary>
+    /// <param name="value">The initial quantity value. Default is 0.</param>
+    /// <remarks>The quantity value will never be negative. If a negative value is provided, it will be set to 0.</remarks>
+    public Quantity(int value = 0)
+    {
+        _value = Math.Max(0, value);
+    }
+}
