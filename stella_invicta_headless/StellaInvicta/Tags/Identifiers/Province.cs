3a5a04f698d30086dd45215b89fd0f7fefaab45b
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-28 12:35:11 +0100

    Add Province struct to represent province identifiers in the game world

---
+++ stella_invicta_headless/StellaInvicta/Tags/Identifiers/Province.cs
@@ -0,0 +1,9 @@
+namespace StellaInvicta.Tags.Identifiers;
+/// <summary>
+/// Represents a province identifier structure in the game world. When used in a pair as a first value it indicates that 
+/// the second is an entity that represents the province.
+/// <remarks>
+/// Provinces are the lowest scale in the universe.
+/// </remarks>
+/// </summary>
+public struct Province;
