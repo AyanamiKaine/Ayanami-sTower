dc266822e241189202f35a3f83981496d6a1a5b5
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-28 18:21:04 +0100

    Enhance MarketUnitTest documentation and rename test method for clarity

--- stella_invicta_headless/StellaInvicta.Test/MarketUnitTest.cs
+++ stella_invicta_headless/StellaInvicta.Test/MarketUnitTest.cs
@@ -2,10 +2,18 @@ using Flecs.NET.Core;
 
 namespace StellaInvicta.Test;
 
+
+
+/// <summary>
+/// Goods are sold and bought at markets. They provide a central point of exchange.
+/// We always sell to a market (Imagine a floating space station or an entire planet) 
+/// because of simplicity. But it should be possible to destroy or create markets.
+/// to include or exclude someone etc.
+/// </summary>
 public class MarketUnitTest
 {
     [Fact]
-    public void Test1()
+    public void MarketTest()
     {
         World world = World.Create();
         world.Import<StellaInvictaECSModule>();
