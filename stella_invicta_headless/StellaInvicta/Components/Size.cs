67a0c66ceab6f2ecf96d8b87971b6a0ae048eda9
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-28 11:58:05 +0100

    Add Size record struct to represent size values

---
+++ stella_invicta_headless/StellaInvicta/Components/Size.cs
@@ -0,0 +1,7 @@
+namespace StellaInvicta.Components;
+
+/// <summary>
+/// Represents a size value as an integer.
+/// </summary>
+/// <param name="Value">The size value. Defaults to 0 if not specified.</param>
+public record struct Size(int Value = 0);
