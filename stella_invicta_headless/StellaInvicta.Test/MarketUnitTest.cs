d52b73e833abfb235469a9d170914f0a4ed54929
Ayanami Kaine <patrick.grohs@hhu.de>
2025-02-28 18:28:50 +0100

    Add identifiers to MarketUnitTest and enhance documentation for market functionality

--- stella_invicta_headless/StellaInvicta.Test/MarketUnitTest.cs
+++ stella_invicta_headless/StellaInvicta.Test/MarketUnitTest.cs
@@ -1,4 +1,5 @@
 using Flecs.NET.Core;
+using StellaInvicta.Tags.Identifiers;
 
 namespace StellaInvicta.Test;
 
@@ -9,13 +10,26 @@ namespace StellaInvicta.Test;
 /// We always sell to a market (Imagine a floating space station or an entire planet) 
 /// because of simplicity. But it should be possible to destroy or create markets.
 /// to include or exclude someone etc.
+/// 
+/// A market is a place of coordination, a market has information for buying and selling orders
+/// So for example companies create a selling order for gold at the price of 3000 credits for the
+/// quantity of 2000. Now if someone buys it the goods can be transported from the seller to the buyer.
+/// (The goods are not at the market) (The market is an institution that should facilitat trade between 
+/// entities)
 /// </summary>
 public class MarketUnitTest
 {
     [Fact]
-    public void MarketTest()
+    public void MarketBuyOrder()
     {
         World world = World.Create();
         world.Import<StellaInvictaECSModule>();
+
+        var market = world.Entity("")
+            .Add<Market>();
+
+        var buyOrder = world.Entity("")
+            .Add<Market>(market);
+
     }
 }
