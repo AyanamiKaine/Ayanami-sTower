a28749487bb5677aaf1186e585030f771792bc3c
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-02 14:13:35 +0100

    Refactor Diplomacy struct to clarify its purpose as a character's diplomacy stat, removing unnecessary parameters and remarks.

--- stella_invicta_headless/StellaInvicta/Components/Diplomancy.cs
+++ stella_invicta_headless/StellaInvicta/Components/Diplomancy.cs
@@ -1,10 +1,6 @@
 namespace StellaInvicta.Components;
 
 /// <summary>
-/// Represents a diplomatic relationship between entities with a numerical value.
+/// Represents the diplomacy stat of a character, similar to how its used in CK2/3
 /// </summary>
-/// <param name="Value">The numerical value representing the state of diplomatic relations. Defaults to 0.</param>
-/// <remarks>
-/// A positive value typically indicates friendly relations, while a negative value indicates hostile relations.
-/// </remarks>
 public record struct Diplomacy(double Value = 0);
