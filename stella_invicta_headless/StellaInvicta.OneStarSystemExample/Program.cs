5eddbbdf0fec9fcb387cccf28b03706ac4a762d5
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-02 17:10:37 +0100

    Add simulation speed to GameTimeSystem and adjust loop iteration count

--- stella_invicta_headless/StellaInvicta.OneStarSystemExample/Program.cs
+++ stella_invicta_headless/StellaInvicta.OneStarSystemExample/Program.cs
@@ -8,21 +8,24 @@ using StellaInvicta.Tags.Relationships;
 var world = World.Create();
 world.Import<StellaInvictaECSModule>();
 world.Import<Ecs.Stats>();
-world.AddSystem(new AgeSystem());
+
+
+var simulationSpeed = world.Timer("SimulationSpeed")
+    .Interval(SimulationSpeed.Default);
 
 // Set Current Year
 world.Set<GameDate>(new(year: 29, month: 12, day: 1));
+world.AddSystem(new GameTimeSystem(), simulationSpeed);
+world.AddSystem(new AgeSystem(), simulationSpeed);
 
-world.AddSystem(new GameTimeSystem());
-world.AddSystem(new AgeSystem());
 
 var random = new Random();
-for (int i = 0; i < 100000; i++)
+for (int i = 0; i < 28677; i++)
 {
     int randomYear = random.Next(0, 29); // Random year between 0 and 28
     int randomMonth = random.Next(1, 13); // Random month between 1 and 12
     int randomDay = random.Next(1, 29); // Random day between 1 and 28 (simplified)
-    
+
     world.Entity()
         .Set<Birthday, GameDate>(new GameDate(year: randomYear, month: randomMonth, day: randomDay))
         .Add<Character>()
