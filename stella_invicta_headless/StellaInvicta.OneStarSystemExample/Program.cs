16b23d5daebcbdebc215f05009e872f665003a0e
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-02 15:26:15 +0100

    Add game date initialization and random character generation to enhance simulation realism

--- stella_invicta_headless/StellaInvicta.OneStarSystemExample/Program.cs
+++ stella_invicta_headless/StellaInvicta.OneStarSystemExample/Program.cs
@@ -2,11 +2,32 @@
 using StellaInvicta;
 using StellaInvicta.Components;
 using StellaInvicta.Systems;
+using StellaInvicta.Tags.Identifiers;
+using StellaInvicta.Tags.Relationships;
 
 var world = World.Create();
 world.Import<StellaInvictaECSModule>();
+world.Import<Ecs.Stats>();
 world.AddSystem(new AgeSystem());
 
-var Marina = world.Entity("Marina")
-    .Set<Name>(new("Marina"))
-    .Set<Age>(new(29));
+// Set Current Year
+world.Set<GameDate>(new(year: 29, month: 12, day: 1));
+
+world.AddSystem(new GameTimeSystem());
+world.AddSystem(new AgeSystem());
+
+var random = new Random();
+for (int i = 0; i < 100000; i++)
+{
+    int randomYear = random.Next(0, 29); // Random year between 0 and 28
+    int randomMonth = random.Next(1, 13); // Random month between 1 and 12
+    int randomDay = random.Next(1, 29); // Random day between 1 and 28 (simplified)
+    
+    world.Entity()
+        .Set<Birthday, GameDate>(new GameDate(year: randomYear, month: randomMonth, day: randomDay))
+        .Add<Character>()
+        .Set<Name>(new($"Marina_{i}"))
+        .Set<Age>(new(29 - randomYear)); // Adjust age based on birth year
+}
+
+world.App().EnableRest().EnableStats().Run();
