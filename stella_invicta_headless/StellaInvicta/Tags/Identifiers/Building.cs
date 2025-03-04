c6a402ea67156a924cbc0b9404d374f23c193321
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-04 12:30:38 +0100

    Add Building struct to define entities as buildings with production capabilities

---
+++ stella_invicta_headless/StellaInvicta/Tags/Identifiers/Building.cs
@@ -0,0 +1,8 @@
+namespace StellaInvicta.Tags.Identifiers;
+/// <summary>
+/// Desenates a entity as a building.
+/// <remarks>
+/// Buildings provide work for pops and produce with input, output
+/// </remarks>
+/// </summary>
+public struct Building;
