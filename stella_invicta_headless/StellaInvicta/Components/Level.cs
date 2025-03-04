7a4c70b91f00aff7600a0827492b5a59bc85149f
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-05 00:11:26 +0100

    Add Level record struct to represent levels with a default value

---
+++ stella_invicta_headless/StellaInvicta/Components/Level.cs
@@ -0,0 +1,7 @@
+namespace StellaInvicta.Components;
+
+/// <summary>
+/// Used to indicate the level of something.
+/// </summary>
+/// <param name="Value"></param>
+public record struct Level(int Value = 1);
