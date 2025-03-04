0f1345e49473b6f0b80d0dcfdc5a9900360133cd
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-04 23:51:21 +0100

    Enhance Slave tag documentation to clarify its relationship with Worker tag

--- stella_invicta_headless/StellaInvicta/Tags/Identifiers/Slave.cs
+++ stella_invicta_headless/StellaInvicta/Tags/Identifiers/Slave.cs
@@ -3,5 +3,8 @@ namespace StellaInvicta.Tags.Identifiers;
 /// <summary>
 /// Represents an unfree population unit in the game system.
 /// Used for population management and economic simulation.
+/// By default it should have the IsA Worker relationship
+/// This means the tag slave can be used in places where we
+/// expect the Worker tag.
 /// </summary>
 public struct Slave;
