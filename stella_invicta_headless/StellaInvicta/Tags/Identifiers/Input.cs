b6e65a4da9a5859ff5450c3583ee4521be18ec8f
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-04 13:20:14 +0100

    Add Input struct for type-safe identification of input tags

---
+++ stella_invicta_headless/StellaInvicta/Tags/Identifiers/Input.cs
@@ -0,0 +1,10 @@
+namespace StellaInvicta.Tags.Identifiers;
+
+/// <summary>
+/// Represents an identifier for an input tag in the Stella Invicta system.
+/// </summary>
+/// <remarks>
+/// This struct is used to identify input-related tags in a type-safe manner.
+/// It is an empty struct that serves as a marker type for the tag system.
+/// </remarks>
+public struct Input;
