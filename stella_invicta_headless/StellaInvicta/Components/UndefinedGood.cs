40055af346a12f2c8ac173465075fb92a811832a
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-04 13:48:30 +0100

    Add UndefinedGood singleton implementation for undefined goods handling

---
+++ stella_invicta_headless/StellaInvicta/Components/UndefinedGood.cs
@@ -0,0 +1,29 @@
+namespace StellaInvicta.Components;
+/// <summary>
+/// Singleton UndefinedGood implementation
+/// </summary>
+public sealed class UndefinedGood : Good
+{
+    /// <summary>
+    /// Gets the singleton instance of the UndefinedGood class.
+    /// </summary>
+    /// <value>
+    /// The singleton instance of UndefinedGood.
+    /// </value>
+    public static UndefinedGood Instance { get; } = new UndefinedGood();
+
+    /// <summary>
+    /// Gets a value indicating whether this good is defined.
+    /// Always returns false for UndefinedGood.
+    /// </summary>
+    public override bool IsDefined => false;
+
+    private UndefinedGood() : base("undefined", 0) { }
+
+    /// <inheritdoc/>
+    public override IGood WithQuantity(int newQuantity)
+    {
+        // UndefinedGood always has quantity 0, regardless of attempts to change it
+        return Instance;
+    }
+}
