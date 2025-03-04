e512914732339fa11ae76b2b49459da7f8ddf62c
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-04 19:07:43 +0100

    Add unit test for province simulation in ProvinceUnitTest

---
+++ stella_invicta_headless/StellaInvicta.Test/ProvinceUnitTest.cs
@@ -0,0 +1,23 @@
+using Flecs.NET.Core;
+
+namespace StellaInvicta.Test;
+
+/// <summary>
+/// Here we want to test the simulation for one province
+/// The basic idea, is that a province has infrastructure,
+/// buildings and population. Populations work at buildings
+/// and buildings need workers, enough infrastructure and
+/// their input goods to produce output. Buildings store the output
+/// in their inventory and put them up to sale if wished. Buyers
+/// come to the building and take the output and bring it to somewhere 
+//  else
+/// </summary>
+public class ProvinceUnitTest
+{
+    [Fact]
+    public void Test1()
+    {
+        World world = World.Create();
+        world.Import<StellaInvictaECSModule>();
+    }
+}
