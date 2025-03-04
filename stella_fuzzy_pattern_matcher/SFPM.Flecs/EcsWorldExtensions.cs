1d6635e06f81a5da9475ae0f3a3c965f99b42dac
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-04 22:56:49 +0100

    Add TODO for implementing a rule order system in EcsWorldExtensions

--- stella_fuzzy_pattern_matcher/SFPM.Flecs/EcsWorldExtensions.cs
+++ stella_fuzzy_pattern_matcher/SFPM.Flecs/EcsWorldExtensions.cs
@@ -172,6 +172,11 @@ public static class EcsWorldExtensions
         world.Get<List<Rule>>().Match(queryData: queryData);
     }
 
+    // TODO:
+    // It seems its really possible to create a rule order system 
+    // see: "https://github.com/BeanCheeseBurrito/Flecs.NET/blob/main/src/Flecs.NET.Examples/Queries/Sorting.cs"
+    // So we could define rules on entities and the system would correctly order them no need to define a singleton.
+
     /// <summary>
     /// Optimizes the evaluation order of rules in the world by sorting them based on their criteria count.
     /// Rules with more criteria are evaluated first to potentially reduce the number of evaluations needed.
