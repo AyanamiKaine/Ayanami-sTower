589c53c2e7a3c0f949cb66b2e4120f47010f5eee
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-04 17:04:31 +0100

    Simplify Iron class implementation by removing redundant method

--- stella_invicta_headless/StellaInvicta/Components/Iron.cs
+++ stella_invicta_headless/StellaInvicta/Components/Iron.cs
@@ -2,11 +2,4 @@ namespace StellaInvicta.Components;
 /// <summary>
 /// Iron good, used for basic construction
 /// </summary>
-public class Iron(int quantity) : Good("Iron", quantity)
-{
-    /// <inheritdoc/>
-    public override IGood WithQuantity(int newQuantity)
-    {
-        return new Iron(newQuantity);
-    }
-}
+public class Iron(int quantity) : Good("Iron", quantity){}
