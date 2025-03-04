7d8082f19ce5642e1b387c5151ee0858d2920f49
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-04 18:23:59 +0100

    Add simulation for building inventory management and introduce tagging for performance optimization

--- stella_invicta_headless/StellaInvicta.Test/BuildingUnitTest.cs
+++ stella_invicta_headless/StellaInvicta.Test/BuildingUnitTest.cs
@@ -52,4 +52,52 @@ public class BuildingUnitTest
 
         Assert.Equal(expectedInventory, actualInventory);
     }
+
+    /// <summary>
+    /// We could add various tags for buildings so we can improve performance.
+    /// For example a mine with no input besides its workers does not need to check
+    /// the inventory.
+    /// </summary>
+    [Fact]
+    public void BuildingWithTags()
+    {
+        World world = World.Create();
+        world.Import<StellaInvictaECSModule>();
+
+
+        var building = world.Entity("IronMine-BUILDING")
+            .Add<Building>()
+            .Set<Inventory, GoodsList>([
+            ])
+            .Set<Input, GoodsList>([
+            ])
+            .Set<Output, GoodsList>([
+                new Iron(5)
+            ]);
+
+
+        var buildingSimulation = world.System<GoodsList, GoodsList, GoodsList>()
+            .With<Building>()
+            .TermAt(0).First<Inventory>().Second<GoodsList>()
+            .TermAt(1).First<Input>().Second<GoodsList>()
+            .TermAt(2).First<Output>().Second<GoodsList>()
+            .Each((Entity e, ref GoodsList inventory, ref GoodsList inputGoodsList, ref GoodsList outputGoodsList) =>
+            {
+                if (inventory >= inputGoodsList)
+                {
+                    inventory -= inputGoodsList;
+                    inventory += outputGoodsList;
+                }
+            });
+
+        buildingSimulation.Run();
+
+        GoodsList expectedInventory = [
+            new Iron(5)
+        ];
+
+        var actualInventory = building.GetSecond<Inventory, GoodsList>();
+
+        Assert.Equal(expectedInventory, actualInventory);
+    }
 }
