ce19f4e2571f277710950d9ec1e5fa9fc13db51f
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-05 10:33:34 +0100

    Add Cached struct for marking calculated data as cached for performance optimizations

---
+++ stella_invicta_headless/StellaInvicta/Tags/Identifiers/Cached.cs
@@ -0,0 +1,6 @@
+namespace StellaInvicta.Tags.Identifiers;
+/// <summary>
+/// Mostly used in pairs to mark any calculated data as cached, maybe used
+/// for performance optimizations.
+/// </summary>
+public struct Cached;
