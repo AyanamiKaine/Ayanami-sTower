5496a8e0bfef76466793942996b9a89e722b7295
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-05 12:09:36 +0100

    Refactor loan handling in BankUnitTest to improve clarity and functionality

--- stella_invicta_headless/StellaInvicta.Test/BankUnitTest.cs
+++ stella_invicta_headless/StellaInvicta.Test/BankUnitTest.cs
@@ -25,13 +25,28 @@ public class BankUnitTest
         world.Entity()
             .Set(new Loan(bank, borrower, amount, rate, term, term, true));
 
-        // Transfer funds
-        bankCredits.Ammount -= amount;
-        bank.Set(bankCredits);
-
-        var borrowerCredits = borrower.Get<Credits>();
+        var borrowerCredits = borrower.GetSecond<Expected, Credits>();
         borrowerCredits.Ammount += amount;
-        borrower.Set(borrowerCredits);
+        borrower.Set<Expected, Credits>(borrowerCredits);
+
+
+        /*
+        If the borrowers gets the loan from his bank, we dont need to transver the money to the banks account,
+        because the bank already has that money, otherwise the bank will believe it just got money to lend out
+        otherwise we send it to the new bank.
+        */
+        var borrowersBank = borrower.Target<Bank>();
+        if (borrowersBank == bank)
+        {
+            return true;
+        }
+        else
+        {
+            var borrowersBankCredits = borrowersBank.Get<Credits>();
+            borrowersBankCredits.Ammount += amount;
+            borrowersBank.Set(borrowersBankCredits);
+        }
+
 
         return true;
     }
@@ -45,12 +60,11 @@ public class BankUnitTest
 
         var bank = world.Entity("bank")
             .Add<Bank>()
-            .Set<Monthly, DepositInterestRate>(new DepositInterestRate(0.10))
+            .Set<DepositInterestRate>(new(0.10))
             .Set<Credits>(new(1));
 
         world.System<DepositInterestRate, Credits>()
             .With<Bank>()
-            .TermAt(0).First<Monthly>().Second<DepositInterestRate>()
             .Each((Entity e, ref DepositInterestRate rate, ref Credits credits) =>
             {
                 double interest = credits.Ammount * rate.Amount;
@@ -67,7 +81,8 @@ public class BankUnitTest
     /// <summary>
     /// Banks should be able to give out a certain amount
     /// of money to other entities that need it. This
-    /// creates a loan system.
+    /// creates a loan system. Bank generate profit my 
+    /// getting the interest back from the bank.
     /// </summary>
     [Fact]
     public void BanksLoansInterestRate()
@@ -78,49 +93,81 @@ public class BankUnitTest
 
         var bank = world.Entity("bank")
             .Add<Bank>()
-            .Set<Monthly, DepositInterestRate>(new DepositInterestRate(0.10))
+            .Set<DepositInterestRate>(new(0.10))
+            .Set<ReserveRatio>(new(0))
             .Set<LendingInterestRate>(new(0.1))
             .Set<Credits>(new(40000));
 
         var company = world.Entity("company")
             .Add<Company>()
             .Add<Bank>(bank)
-            .Set<ReserveRatio>(new(0))
             .Set<Expected, Credits>(new Credits(0));
 
         CreateLoan(world, bank, company, 200, 1);
 
-        world.System<Loan>()
+        // TODO: we need two systems
+        // one paying interest on the loan
+        // one paying back the loan.
+        world.System<Loan>("Paying Interest")
             .Each((Entity e, ref Loan loan) =>
             {
                 if (!loan.IsActive) return;
 
                 // Get borrower's credits
                 var borrower = loan.Borrower;
-                if (borrower.Has<Credits>())
+                if (borrower.Has<Expected, Credits>())
                 {
                     // Calculate monthly interest
                     double interestThisMonth = loan.Principal * loan.InterestRate / 12;
 
                     // Subtract from borrower
-                    var borrowerCredits = borrower.Get<Credits>();
+                    var borrowerCredits = borrower.GetSecond<Expected, Credits>();
                     borrowerCredits.Ammount -= interestThisMonth;
-                    borrower.Set(borrowerCredits);
+                    borrowerCredits.Ammount = Math.Round(borrowerCredits.Ammount, 3);
+                    borrower.Set<Expected, Credits>(borrowerCredits);
 
                     // Add to lender
                     var lender = loan.LenderBank;
                     var lenderCredits = lender.Get<Credits>();
                     lenderCredits.Ammount += interestThisMonth;
+                    lenderCredits.Ammount = Math.Round(lenderCredits.Ammount, 3);
                     lender.Set(lenderCredits);
-
-                    // Update loan term
-                    loan.RemainingTerm--;
-                    if (loan.RemainingTerm <= 0)
-                        loan.IsActive = false;
                 }
             });
         world.Progress();
 
+
+        var expectedCredits = 198.333;
+        var actualCredits = company.GetSecond<Expected, Credits>().Ammount;
+
+        Assert.Equal(expectedCredits, actualCredits);
+    }
+
+    /// <summary>
+    /// Banks should be able to give out a certain amount
+    /// of money to other entities that need it. This
+    /// creates a loan system.
+    /// </summary>
+    [Fact]
+    public void BanksCanLendOutMoney()
+    {
+        World world = World.Create();
+        world.Import<StellaInvictaECSModule>();
+
+
+        var bank = world.Entity("bank")
+            .Add<Bank>()
+            .Set<Monthly, DepositInterestRate>(new DepositInterestRate(0.10))
+            .Set<ReserveRatio>(new(0))
+            .Set<LendingInterestRate>(new(0.1))
+            .Set<Credits>(new(40000));
+
+        var company = world.Entity("company")
+            .Add<Company>()
+            .Add<Bank>(bank)
+            .Set<Expected, Credits>(new Credits(0));
+
+        CreateLoan(world, bank, company, 200, 1);
 
         var expectedCredits = 200.0;
         var actualCredits = company.GetSecond<Expected, Credits>().Ammount;
