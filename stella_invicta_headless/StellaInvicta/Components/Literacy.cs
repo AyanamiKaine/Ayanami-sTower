52e07d711797ac492d4284aa7c2247c8a1766535
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-05 01:02:03 +0100

    Change Literacy struct to use double for value and percentage for improved precision

--- stella_invicta_headless/StellaInvicta/Components/Literacy.cs
+++ stella_invicta_headless/StellaInvicta/Components/Literacy.cs
@@ -5,12 +5,12 @@ namespace StellaInvicta.Components;
 /// </summary>
 public record struct Literacy
 {
-    private float _value;
+    private double _value;
     /// <summary>
     /// Gets or sets the literacy value, representing the proficiency level as a normalized float between 0 and 1.
     /// </summary>
     /// <value>A float value between 0 (completely illiterate) and 1 (fully literate).</value>
-    public float Value
+    public double Value
     {
         readonly get => _value;
         set => _value = Math.Clamp(value, 0f, 1f);
@@ -20,13 +20,13 @@ public record struct Literacy
     /// Gets the literacy percentage, representing the proficiency level as a percentage between 0 and 100.
     /// </summary>
     /// <value>A float value between 0 (completely illiterate) and 100 (fully literate).</value>
-    public readonly float Percentage => Value * 100f;
+    public readonly double Percentage => Value * 100f;
 
     /// <summary>
     /// Initializes a new instance of the Literacy class with a specified value.
     /// </summary>
     /// <param name="value">The initial literacy value as a normalized float between 0 and 1. Defaults to 0 if not specified.</param>
-    public Literacy(float value = 0) : this()
+    public Literacy(double value = 0) : this()
     {
         Value = value;
     }
@@ -36,7 +36,7 @@ public record struct Literacy
     /// </summary>
     /// <param name="percentage">The literacy percentage (0-100)</param>
     /// <returns>A new Literacy instance</returns>
-    public static Literacy FromPercentage(float percentage)
+    public static Literacy FromPercentage(double percentage)
     {
         return new Literacy(percentage / 100f);
     }
