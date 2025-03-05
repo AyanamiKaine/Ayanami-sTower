e9763ac993b7520b4ddb5362d48a2965f5bd5f7c
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-05 10:43:38 +0100

    Update documentation for LendingInterestRate to clarify usage and behavior

--- stella_invicta_headless/StellaInvicta/Components/LendingInterestRate.cs
+++ stella_invicta_headless/StellaInvicta/Components/LendingInterestRate.cs
@@ -1,5 +1,9 @@
 namespace StellaInvicta.Components;
 /// <summary>
-/// Interest rate for loans.
+/// Current interest rate for loans.
+/// If a new loan is made it should use this
+/// inital interest rate, if this rate changes
+/// the interest rate of existing loans should
+/// NOT change.
 /// </summary>
 public record struct LendingInterestRate(double Amount);
