db5d64f72f07a89e26f929b1d599e10889278e87
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-07 18:52:29 +0100

    Add mining functionality to MiningUnitTest with initial inventory setup and system processing

--- stella_invicta_headless/StellaInvicta.Test/MiningUnitTest.cs
+++ stella_invicta_headless/StellaInvicta.Test/MiningUnitTest.cs
@@ -1,10 +1,14 @@
 using Flecs.NET.Core;
+using StellaInvicta.Components;
+using StellaInvicta.Tags.Identifiers;
 
 namespace StellaInvicta.Test;
 
 
 public class MiningUnitTest
 {
+
+    struct Mining { };
 
     /// <summary>
     /// Here we want to create a system of mining, so instead of manaully going over entities
@@ -19,6 +23,31 @@ public class MiningUnitTest
         World world = World.Create();
         world.Import<StellaInvictaECSModule>();
 
-        Assert.Fail();
+        var asteroid = world.Entity()
+            .Set<GoodsList>([]);
+
+        var miningShip = world.Entity()
+            .Add<Mining>(asteroid)
+            .Set<Inventory, GoodsList>([
+                new Iron(20)
+            ]);
+
+        world.System<Entity, GoodsList>()
+            .TermAt(0).First<Mining>().Second<Entity>()
+            .TermAt(0).First<Inventory>().Second<GoodsList>()
+            .Each((Entity miningEntity, ref Entity EntityToBeMined, ref GoodsList miningEntityInventory) =>
+            {
+                miningEntityInventory += EntityToBeMined.Get<GoodsList>();
+            });
+
+        world.Progress();
+
+        var expectedInventory = new GoodsList()
+        {
+            new Iron(20)
+        };
+
+        Assert.Equal(expectedInventory, miningShip.GetSecond<Inventory, GoodsList>());
+        Assert.Fail("Sadly this is not done yet, because we need to think about: How much should be mined, etc.");
     }
 }
