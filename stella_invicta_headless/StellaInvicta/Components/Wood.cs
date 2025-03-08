0220b1eefa44a0684dcad20ef46621239af54b68
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-08 01:32:28 +0100

    Add Wood class to represent wood resource

---
+++ stella_invicta_headless/StellaInvicta/Components/Wood.cs
@@ -0,0 +1,6 @@
+namespace StellaInvicta.Components;
+/// <summary>
+/// Wood we all know and love.
+/// Shiver me timbers.
+/// </summary>
+public class Wood(int quantity) : Good("Wood", quantity) { }
