40935c0b0d8f7fe7984f1ecad1b33e93404183f0
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-11 15:19:34 +0100

    Add "Content Queue" navigation item to the application

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/App.axaml.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/App.axaml.cs
@@ -102,6 +102,7 @@ public partial class App : Application
             nav.Child<NavigationViewItem>(item => item.Child<TextBlock>(t => t.SetText("Home")));
             nav.Child<NavigationViewItem>(item => item.Child<TextBlock>(t => t.SetText("Knowledge Vault")));
             nav.Child<NavigationViewItem>(item => item.Child<TextBlock>(t => t.SetText("Literature")));
+            nav.Child<NavigationViewItem>(item => item.Child<TextBlock>(t => t.SetText("Content Queue")));
 
             /*
              The study page will be something more complex
