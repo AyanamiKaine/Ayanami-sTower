472dc3381764ba8c07d91e1947289a09f828e5c3
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-14 14:07:25 +0100

    Refactor AddCloze to implement IUIComponent; streamline window creation and content management

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/Windows/AddCloze.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/Windows/AddCloze.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Templates;
+using Avalonia.Flecs.Controls;
 using Avalonia.Flecs.Controls.ECS;
 using Avalonia.Flecs.StellaLearning.Data;
 using Avalonia.Flecs.Util;
@@ -15,51 +16,40 @@ namespace Avalonia.Flecs.StellaLearning.Windows;
 /// <summary>
 /// Represents the window to add spaced repetition items of the type cloze
 /// </summary>
-public static class AddCloze
+public class AddCloze : IUIComponent
 {
+    private Entity _root;
+    /// <inheritdoc/>
+    public Entity Root => _root;
 
     /// <summary>
     /// Create the Add Cloze Window
     /// </summary>
-    /// <param name="entities"></param>
+    /// <param name="world"></param>
     /// <returns></returns>
-    public static Entity Create(NamedEntities entities)
+    public AddCloze(World world)
     {
-        var addClozeWindow = entities.GetEntityCreateIfNotExist("AddClozeWindow")
-            .Set(new Window())
-            .SetWindowTitle("Add Cloze")
+        _root = world.UI<Window>((window) =>
+        {
+            window
+            .SetTitle("Add Cloze")
             .SetWidth(400)
-            .SetHeight(400);
-
-        var scrollViewer = entities.Create()
-            .ChildOf(addClozeWindow)
-            .Set(new ScrollViewer())
-            .SetRow(1)
-            .SetColumnSpan(3);
-
-        entities["MainWindow"].OnClosed((_, _) =>
-        {
-            //When the main window is closed close the add file window as well
-            addClozeWindow.CloseWindow();
-        });
-
-        addClozeWindow.OnClosing((s, e) =>
-        {
-            // As long as the main window is visible dont 
-            // close the window but hide it instead
-            if (entities["MainWindow"].Get<Window>().IsVisible)
+            .SetHeight(400)
+            .Child<ScrollViewer>((scrollViewer) =>
             {
-                ((Window)s!).Hide();
-                e.Cancel = true;
-            }
+                scrollViewer
+                .SetRow(1)
+                .SetColumnSpan(3)
+                .Child(DefineWindowContents(world));
+            });
+
+            window.OnClosed((sender, args) => _root.Destruct());
+
+            window.Show();
         });
-
-        DefineWindowContents(entities).ChildOf(scrollViewer);
-
-        return addClozeWindow;
     }
 
-    private static Entity DefineWindowContents(NamedEntities entities)
+    private static Entity DefineWindowContents(World world)
     {
         static void AddCloze(string item, ObservableCollection<string> clozes)
         {
@@ -70,72 +60,92 @@ public static class AddCloze
             }
         }
 
-        var layout = entities.Create()
-            .Set(new StackPanel())
+        ObservableCollection<string> clozes = [];
+
+        return world.UI<StackPanel>((stackPanel) =>
+        {
+            UIBuilder<TextBox>? nameTextBox = null;
+            UIBuilder<TextBox>? clozeBox = null;
+            UIBuilder<ItemsControl>? clozeList = null;
+
+            stackPanel
             .SetOrientation(Layout.Orientation.Vertical)
             .SetSpacing(10)
             .SetMargin(20);
 
-
-        var nameTextBox = entities.Create()
-            .ChildOf(layout)
-            .Set(new TextBox())
-            .SetWatermark("Name");
-
-        var clozeBox = entities.Create()
-                .ChildOf(layout)
-                .Set(new TextBox() { AcceptsReturn = true, TextWrapping = TextWrapping.Wrap })
-                .SetWatermark("Cloze Text");
-
-
-        ObservableCollection<string> clozes = [];
-
-        var markAsClozeButton = entities.Create()
-            .Set(new Button())
-            .SetContent("Mark as Cloze")
-            .OnClick((_, _) =>
+            stackPanel.Child<TextBox>((textBox) =>
             {
-                var cloze = clozeBox.Get<TextBox>().SelectedText;
-                AddCloze(cloze, clozes);
+                nameTextBox = textBox;
+                textBox.SetWatermark("Name");
             });
 
-        object? flyout = clozeBox.Get<TextBox>().ContextFlyout = new Flyout() { Content = markAsClozeButton.Get<Button>() };
-
-        var clozeList = entities.GetEntityCreateIfNotExist("ClozeList")
-            .ChildOf(layout)
-            .Set(new ItemsControl())
-            .Set(clozes)
-            .SetItemTemplate(DefineTagTemplate(entities))
-            .SetItemsSource(clozes);
-
-        entities.Create()
-            .ChildOf(layout)
-            .Set(new Button())
-            .SetContent("Create Cloze")
-            .OnClick((sender, args) =>
+            stackPanel.Child<TextBox>((textBox) =>
             {
-                if (clozes.Count == 0 || string.IsNullOrEmpty(nameTextBox.GetText()))
-                {
-                    nameTextBox.SetWatermark("Name is required");
-                    return;
-                }
+                clozeBox = textBox;
+                textBox.SetWatermark("Cloze Text")
+                .SetTextWrapping(TextWrapping.Wrap);
+                textBox.Get<TextBox>().AcceptsReturn = true;
 
-                entities["SpacedRepetitionItems"].Get<ObservableCollection<SpacedRepetitionItem>>().Add(new SpacedRepetitionCloze()
+                // Create the mark as cloze button for the context flyout
+                var markAsClozeButton = world.UI<Button>((button) =>
                 {
-                    Name = nameTextBox.GetText(),
-                    FullText = clozeBox.GetText(),
-                    ClozeWords = [.. clozes],
-                    SpacedRepetitionItemType = SpacedRepetitionItemType.Cloze
+                    button.Child<TextBlock>((textBlock) => textBlock.SetText("Mark as Cloze"));
+                    button.OnClick((_, _) =>
+                    {
+                        var cloze = textBox.Get<TextBox>().SelectedText;
+                        AddCloze(cloze, clozes);
+                    });
                 });
 
-                nameTextBox.SetText("");
-                clozeBox.SetText("");
-                clozes.Clear();
+                // Set up the context flyout
+                textBox.Get<TextBox>().ContextFlyout = new Flyout() { Content = markAsClozeButton.Get<Button>() };
             });
-        return layout;
-    }
 
-    private static FuncDataTemplate<string> DefineTagTemplate(NamedEntities entities)
+            // Create cloze list with items control
+            stackPanel.Child<ItemsControl>((itemsControl) =>
+            {
+                clozeList = itemsControl
+                .SetItemTemplate(DefineTagTemplate(world, clozes))
+                .SetItemsSource(clozes);
+            });
+
+            // Create button
+            stackPanel.Child<Button>((button) =>
+            {
+                button.Child<TextBlock>((textBlock) =>
+                {
+                    textBlock.SetText("Create Cloze");
+                });
+
+                button.OnClick((sender, args) =>
+                {
+                    if (nameTextBox is null || clozeBox is null)
+                    {
+                        return;
+                    }
+
+                    if (clozes.Count == 0 || string.IsNullOrEmpty(nameTextBox.GetText()))
+                    {
+                        nameTextBox!.SetWatermark("Name is required");
+                        return;
+                    }
+
+                    world.Get<ObservableCollection<SpacedRepetitionItem>>().Add(new SpacedRepetitionCloze()
+                    {
+                        Name = nameTextBox.GetText(),
+                        FullText = clozeBox.GetText(),
+                        ClozeWords = [.. clozes],
+                        SpacedRepetitionItemType = SpacedRepetitionItemType.Cloze
+                    });
+
+                    nameTextBox.SetText("");
+                    clozeBox.SetText("");
+                    clozes.Clear();
+                });
+            });
+        });
+    }
+    private static FuncDataTemplate<string> DefineTagTemplate(World world, ObservableCollection<string> clozes)
     {
         return new FuncDataTemplate<string>((tag, _) =>
         {
@@ -157,7 +167,7 @@ public static class AddCloze
 
             removeButton.Click += ((sender, args) =>
             {
-                entities["ClozeList"].Get<ObservableCollection<string>>().Remove(tag);
+                clozes.Remove(tag);
             });
 
             stackPanel.Children.Add(nameText);
