291a539ee054a4463bf097cbc8ac68207b9dfb52
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-18 11:34:15 +0100

    Refactor App.axaml.cs to streamline UI element creation using fluent API

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.Example/App.axaml.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Example/App.axaml.cs
@@ -1,11 +1,10 @@
-
-using System;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Flecs.NET.Core;
-using Avalonia.Flecs.Controls.ECS.Events;
+using Avalonia.Flecs.Controls;
 using Avalonia.Flecs.Controls.ECS;
+
 namespace Avalonia.Flecs.Example;
 
 public partial class App : Application
@@ -17,55 +16,61 @@ public partial class App : Application
         AvaloniaXamlLoader.Load(this);
         _world.Import<Avalonia.Flecs.Controls.ECS.Module>();
 
-        var window = _world.Entity("MainWindow")
-            .Set(new Window())
-            .SetWindowTitle("Avlonia.Flecs Example")
-            .SetHeight(400)
-            .SetWidth(400);
-
-        var dockPanel = _world.Entity("MainWindowDockPanel")
-            .ChildOf(window)
-            .Set(new DockPanel());
-
-        var menu = _world.Entity("MainMenu")
-            .ChildOf(dockPanel)
-            .Set(new Menu());
-
-        _world.Entity("FileMenuItem")
-            .ChildOf(menu)
-            .Set(new MenuItem())
-            .SetHeader("_File");
-
-        _world.Entity("EditMenuItem")
-                    .ChildOf(menu)
-                    .Set(new MenuItem())
-                    .SetHeader("_Edit");
-
-        _world.Entity("SelectionMenuItem")
-              .ChildOf(menu)
-              .Set(new MenuItem())
-              .SetHeader("_Selection");
-
-        var textBlock = _world.Entity("HelloWorldTextBlock")
-            .ChildOf(dockPanel)
-            .Set(new TextBlock())
-            .SetText("Hello World!");
-
-        var button = _world.Entity("Button")
-            .ChildOf(dockPanel)
-            .Set(new Button())
-            .SetContent("Click Me!");
-
-        button.OnClick((sender, args) =>
+        var mainWindow = _world.UI<Window>(window =>
         {
-            if (sender is not Button b)
-            {
-                return;
-            }
+            window.SetTitle("Avlonia.Flecs Example")
+                  .SetHeight(400)
+                  .SetWidth(400);
 
-            b.Content = "CHANGED CONTENT";
-            Console.WriteLine("THE BUTTON WAS CLICK WHOAH!");
+            window.Child<DockPanel>(dockPanel =>
+            {
+                // Add menu at the top of the DockPanel
+                dockPanel.Child<Menu>(menu =>
+                {
+                    menu.SetDock(Dock.Top);
+
+                    menu.Child<MenuItem>(fileMenuItem =>
+                    {
+                        fileMenuItem.SetHeader("_File");
+                    });
+
+                    menu.Child<MenuItem>(editMenuItem =>
+                    {
+                        editMenuItem.SetHeader("_Edit");
+                    });
+
+                    menu.Child<MenuItem>(selectionMenuItem =>
+                    {
+                        selectionMenuItem.SetHeader("_Selection");
+                    });
+                });
+
+                // Add text block
+                dockPanel.Child<TextBlock>(textBlock =>
+                {
+                    textBlock.SetText("Hello World!")
+                             .SetVerticalAlignment(Layout.VerticalAlignment.Center)
+                             .SetHorizontalAlignment(Layout.HorizontalAlignment.Center);
+                });
+
+                // Add button at the bottom
+                dockPanel.Child<Button>(button =>
+                {
+                    button.SetDock(Dock.Bottom)
+                          .With(btn => btn.Content = "Click Me!")
+                          .OnClick((sender, args) =>
+                          {
+                              if (sender is Button b)
+                              {
+                                  b.Content = "CHANGED CONTENT";
+                                  System.Console.WriteLine("THE BUTTON WAS CLICK WHOAH!");
+                              }
+                          });
+                });
+            });
         });
+
+        _world.Entity("MainWindow").Set(mainWindow.Get<Window>());
     }
 
     public override void OnFrameworkInitializationCompleted()
@@ -79,6 +84,5 @@ public partial class App : Application
 #if DEBUG
         this.AttachDevTools();
 #endif
-
     }
 }
