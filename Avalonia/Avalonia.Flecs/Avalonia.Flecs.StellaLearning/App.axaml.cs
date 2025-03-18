d4e70313f287f4abd0734517157bcd08079bbc27
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-18 15:12:27 +0100

    Simplify navigation display mode handling by removing redundant condition check

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/App.axaml.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/App.axaml.cs
@@ -166,7 +166,7 @@ public partial class App : Application
                         }
                     });
                 }
-                else if (args.DisplayMode == NavigationViewDisplayMode.Compact)
+                else 
                 {
                     nav.Children((Entity child) =>
                     {
