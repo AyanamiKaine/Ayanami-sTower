1079bf65e79e2797255b9794df670964b5b0c7df
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-19 11:25:38 +0100

    Fix memory management on window close by clearing root instead of destructing

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/Windows/AddCloze.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/Windows/AddCloze.cs
@@ -38,9 +38,8 @@ public class AddCloze : IUIComponent
                 .SetColumnSpan(3)
                 .Child(DefineWindowContents(world));
             });
-            //TODO: This is the wrong way of doing it and will result in invalid memory somewhere.
-            //window.OnClosed((sender, args) => _root.Destruct());
-
+            
+            window.OnClosed((sender, args) => _root.Clear());
             window.Show();
         });
     }
