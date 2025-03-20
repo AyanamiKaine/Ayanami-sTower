0f156ed9dc066168c5d00bdf838fc74c3a29aeb5
Ayanami Kaine <ayanami@ayanamis-codex.net>
2025-03-20 08:37:14 +0100

    Invoke Destruct method on window close for proper memory management

--- Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/Windows/EditQuiz.cs
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.StellaLearning/Windows/EditQuiz.cs
@@ -44,8 +44,7 @@ public class EditQuiz : IUIComponent
                 .SetColumnSpan(3)
                 .Child(DefineWindowContents(world));
             });
-            //TODO: This is the wrong way of doing it and will result in invalid memory somewhere.
-            //window.OnClosed((sender, args) => _root.Destruct());
+            window.OnClosed((sender, args) => _root.Destruct());
 
             window.Show();
         });
