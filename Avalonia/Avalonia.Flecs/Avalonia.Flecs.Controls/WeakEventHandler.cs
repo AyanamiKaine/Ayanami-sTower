56c1eb57da688cd98ce9b0ab8e2de9417b53d4d2
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-21 13:44:12 +0100

    Add custom weak event handler class for event management

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/WeakEventHandler.cs
@@ -0,0 +1,40 @@
+using System.Reflection;
+
+namespace Avalonia.Flecs.Controls;
+
+/// <summary>
+/// Custom weak event handler class
+/// </summary>
+/// <typeparam name="TEventArgs"></typeparam>
+public class WeakEventHandler<TEventArgs> where TEventArgs : EventArgs
+{
+    private readonly WeakReference _targetRef;
+    private readonly MethodInfo _method;
+
+    /// <summary>
+    /// Custom weak event handler class
+    /// </summary>
+    /// <param name="callback"></param>
+    public WeakEventHandler(EventHandler<TEventArgs> callback)
+    {
+        _targetRef = new WeakReference(callback.Target);
+        _method = callback.Method;
+    }
+    /// <summary>
+    /// s
+    /// </summary>
+    public EventHandler<TEventArgs> Handler
+    {
+        get
+        {
+            return (sender, e) =>
+        {
+            var target = _targetRef.Target;
+            if (target != null)
+            {
+                _method.Invoke(target, new object[] { sender!, e });
+            }
+        };
+        }
+    }
+}
