bc625cf9c06b263a71c960568df7e34026365d1b
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-21 13:44:15 +0100

    Add initial test class for WeakEventHandler functionality

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls.Tests/WeakEventHandlerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+using System.Threading;
+using Xunit;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Avalonia.Flecs.Controls;
+
+namespace Avalonia.Flecs.Controls.Tests;
+
+public class WeakEventHandlerTests
+{
+
+}
