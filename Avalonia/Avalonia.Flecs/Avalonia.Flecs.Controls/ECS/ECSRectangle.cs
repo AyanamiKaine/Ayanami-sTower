c96ec6753bdb30a62c7319e267c5c59bc12eb27f
Ayanami Kaine <patrick.grohs@hhu.de>
2025-03-24 18:22:56 +0100

    Add ECSRectangle module to register Rectangle component in the ECS framework

---
+++ Avalonia/Avalonia.Flecs/Avalonia.Flecs.Controls/ECS/ECSRectangle.cs
@@ -0,0 +1,35 @@
+using Flecs.NET.Core;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Shapes;
+namespace Avalonia.Flecs.Controls.ECS
+{
+    /// <summary>
+    /// This ECS Module is used to register the Rectangle component
+    /// </summary>
+    public class ECSRectangle : IFlecsModule
+    {
+        /// <summary>
+        /// Initializes the Rectangle component
+        /// </summary>
+        /// <param name="world"></param>
+        public void InitModule(World world)
+        {
+            world.Module<ECSRectangle>();
+            world.Component<Rectangle>("Rectangle")
+                .OnSet((Entity e, ref Rectangle rectangle) =>
+                {
+                    if (!e.Has<object>())
+                    {
+                        e.Set<object>(rectangle);
+                    }
+                    else if (e.Get<object>().GetType() == typeof(Rectangle))
+                    {
+                        e.Set<object>(rectangle);
+                    }
+                    e.Set<Shape>(rectangle);
+                })
+                .OnRemove((Entity e, ref Rectangle shape) => e.Remove<Shape>());
+        }
+    }
+}
