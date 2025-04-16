be83d757f522e30cd87b9bb555bc309b5e67c0c8
Ayanami Kaine <patrick.grohs@hhu.de>
2025-04-16 12:44:45 +0200

    feat: Add Obsidian note properties model and unit tests

---
+++ StellaLearning/StellaLearning.Tests/ObsidianNotePropertiesTests.cs
@@ -0,0 +1,37 @@
+﻿using StellaLearning.Util.NoteHandler;
+
+namespace StellaLearning.Tests;
+
+public class ObsidianNotePropertiesTests
+{
+    [Fact]
+    public void ParseObsidianNotePropertiesUnitTest()
+    {
+        var obsidianNote =
+        """
+        ---
+        Created: 2024-02-12 22:45
+        aliases:
+        - Why Victoria3 crashed on intel cpus
+        tags:
+        - Blog-Post
+        ---
+        """;
+
+
+        var properties = ObsidianNoteProperties.Parse(obsidianNote);
+
+
+        Assert.Equal(DateTime.Parse("2024-02-12 22:45"), properties.Created);
+        Assert.Equal
+        (
+            ["Why Victoria3 crashed on intel cpus"],
+            properties.Aliases
+        );
+        Assert.Equal
+        (
+            ["Blog-Post"],
+            properties.Tags
+        );
+    }
+}
