b9be7d637c583a7c0587d6b5c7e65e1fd887258d
Ayanami Kaine <patrick.grohs@hhu.de>
2025-04-19 23:07:55 +0200

    feat: Define execution phases in Engine class for improved ECS rendering management

--- Applications/NihilEx/Engine.cs
+++ Applications/NihilEx/Engine.cs
@@ -1,8 +1,51 @@
-﻿namespace NihilEx;
+﻿using Flecs.NET.Core;
+
+namespace AyanamisTower.NihilEx;
 /// <summary>
 /// TODO
 /// </summary>
 public class Engine
 {
+    /// <summary>
+    /// Stores the defined phases for the engine's execution pipeline.
+    /// </summary>
+    public Dictionary<string, Entity> Phases = [];
+    /// <summary>
+    /// Gets the Flecs world associated with this engine instance.
+    /// </summary>
+    public World World { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Engine"/> class with the specified Flecs world.
+    /// </summary>
+    /// <param name="world">The Flecs world to associate with this engine.</param>
+    public Engine(World world)
+    {
+        World = world;
+        InitDefaultPhases();
+    }
+
+    private void InitDefaultPhases()
+    {
+
+        var preRender = World
+            .Entity("PreRender")
+            .Add(Ecs.Phase)
+            .DependsOn(Ecs.OnUpdate);
+        Phases.Add("PreRender", preRender);
+
+        var onRender = World
+            .Entity("OnRender")
+            .Add(Ecs.Phase)
+            .DependsOn(preRender);
+
+        Phases.Add("OnRender", onRender);
+
+        var postRender = World
+            .Entity("PostRender")
+            .Add(Ecs.Phase)
+            .DependsOn(onRender);
+
+        Phases.Add("PostRender", postRender);
+    }
 }
