d2253edb4aebe834beccf8b80a83a604a7c45879
Ayanami Kaine <patrick.grohs@hhu.de>
2025-04-19 22:51:31 +0200

    feat: Add PhaseTest class to validate render phase execution order and remove obsolete UnitTest1

---
+++ Tests/NihilEx.Test/PhaseTest.cs
@@ -0,0 +1,31 @@
+﻿using Flecs.NET.Core;
+
+namespace AyanamisTower.NihilEx.Test;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+public class PhaseTest
+{
+    /// <summary>
+    /// First updates run and after that, preRender, onRender and atlast postRender
+    /// </summary>
+    [Fact]
+    public void RenderPhaseCorrectOrder()
+    {
+        var world = World.Create();
+        world.Import<ECS.PhaseModule>();
+
+        var systemRun = false;
+
+        var system = world.System()
+            .Kind(world.Entity("PreRender"))
+            .Each((_) =>
+            {
+                systemRun = true;
+            });
+
+        world.Progress();
+
+        Assert.True(systemRun);
+    }
+}
