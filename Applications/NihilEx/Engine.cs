8f1ad014519c23d7c31f29b75df355e01b04be12
Ayanami Kaine <patrick.grohs@hhu.de>
2025-04-20 11:23:26 +0200

    feat: Add Plane, Matrix3x2, and Matrix4x4 component initializations in InitComponents

--- Applications/NihilEx/Engine.cs
+++ Applications/NihilEx/Engine.cs
@@ -61,11 +61,52 @@ public class Engine
 
     private void InitComponents()
     {
+
+        World.Component<Vector2>("Vector2")
+                    .Member<float>("X")
+                    .Member<float>("Y");
+
         World.Component<Vector3>("Vector3")
                     .Member<float>("X")
                     .Member<float>("Y")
                     .Member<float>("Z");
 
+
+        World.Component<Vector4>("Vector4")
+                    .Member<float>("X")
+                    .Member<float>("Y")
+                    .Member<float>("Z")
+                    .Member<float>("W");
+
+        World.Component<Plane>("Plane")
+                    .Member<Vector3>("Normal")
+                    .Member<float>("D");
+
+        World.Component<Matrix3x2>("Matrix3x2")
+                    .Member<float>("M11")
+                    .Member<float>("M12")
+                    .Member<float>("M21")
+                    .Member<float>("M22")
+                    .Member<float>("M31")
+                    .Member<float>("M32");
+
+        World.Component<Matrix4x4>("Matrix4x4")
+            .Member<float>("M11")
+            .Member<float>("M12")
+            .Member<float>("M13")
+            .Member<float>("M14")
+            .Member<float>("M21")
+            .Member<float>("M22")
+            .Member<float>("M23")
+            .Member<float>("M24")
+            .Member<float>("M31")
+            .Member<float>("M32")
+            .Member<float>("M33")
+            .Member<float>("M34")
+            .Member<float>("M41")
+            .Member<float>("M42")
+            .Member<float>("M43")
+            .Member<float>("M44");
 
         World.Component<Quaternion>("Quaternion")
             .Member<float>("X")
