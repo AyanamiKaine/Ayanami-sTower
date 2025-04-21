d23a638fb004c870857e5bc5321be3337437e76a
Ayanami Kaine <patrick.grohs@hhu.de>
2025-04-21 23:12:06 +0200

    feat: Add WindowMouseLeaveEvent structure to represent mouse leave events with button and position information

---
+++ Applications/NihilEx/ECS/Events/WindowMouseLeaveEvent.cs
@@ -0,0 +1,14 @@
+using System;
+using static SDL3.SDL;
+
+namespace AyanamisTower.NihilEx.ECS.Events;
+
+/// <summary>
+/// Represents an event that occurs when the mouse enters a window.
+/// </summary>
+/// <param name="MouseButton">The mouse button flags indicating which button was pressed.</param>
+/// <param name="Down">Indicates whether the button is pressed down.</param>
+/// <param name="X">The X coordinate of the mouse cursor. Relative to the mouse position on the screen.</param>
+/// <param name="Y">The Y coordinate of the mouse cursor. Relative to the mouse position on the screen.</param>
+/// <param name="Clicks">The number of clicks performed.</param>
+public record struct WindowMouseLeaveEvent(MouseButtonFlags MouseButton, bool Down, float X, float Y, byte Clicks);
