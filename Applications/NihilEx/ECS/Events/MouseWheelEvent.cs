859d387c8bb4bb1b2aeb791607479cfac7ea05ba
Ayanami Kaine <patrick.grohs@hhu.de>
2025-04-21 22:51:23 +0200

    feat: Add MouseWheelEvent structure to represent mouse wheel events with scroll information

---
+++ Applications/NihilEx/ECS/Events/MouseWheelEvent.cs
@@ -0,0 +1,12 @@
+using System;
+using static SDL3.SDL;
+
+namespace AyanamisTower.NihilEx.ECS.Events;
+
+/// <summary>
+/// Represents a mouse wheel event with scroll information.
+/// </summary>
+/// <param name="ScrollX">The horizontal scroll amount.</param>
+/// <param name="ScrollY">The vertical scroll amount.</param>
+/// <param name="Direction">The direction of the mouse wheel movement.</param>
+public record struct MouseWheelEvent(float ScrollX, float ScrollY, MouseWheelDirection Direction);
