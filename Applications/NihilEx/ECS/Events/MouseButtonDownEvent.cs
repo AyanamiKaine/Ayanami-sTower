b91f4687125e9fc6caf6d183b40366068b2ed9ce
Ayanami Kaine <patrick.grohs@hhu.de>
2025-04-21 19:16:10 +0200

    feat: Add MouseButtonDownEvent structure to represent mouse button down events

---
+++ Applications/NihilEx/ECS/Events/MouseButtonDownEvent.cs
@@ -0,0 +1,13 @@
+using System;
+using static SDL3.SDL;
+
+namespace AyanamisTower.NihilEx.ECS.Events;
+
+/// <summary>
+/// Represents a SDL mouse button down event.
+/// </summary>
+/// <param name="MouseButton">Which button was pressed</param>
+/// <param name="X">The X coordinate where the mouse button was pressed.</param>
+/// <param name="Y">The Y coordinate where the mouse button was pressed.</param>
+/// <param name="Clicks">The number of clicks (1 for single-click, 2 for double-click, etc).</param>
+public record struct MouseButtonDownEvent(MouseButtonFlags MouseButton, float X, float Y, byte Clicks);
