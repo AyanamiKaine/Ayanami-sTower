6d48ff2595410ed3974a9172bde6b192a225b13b
Ayanami Kaine <patrick.grohs@hhu.de>
2025-04-21 22:51:26 +0200

    feat: Add MouseMotionEvent structure to represent mouse motion events with state and position information

---
+++ Applications/NihilEx/ECS/Events/MouseMotionEvent.cs
@@ -0,0 +1,14 @@
+using System;
+using static SDL3.SDL;
+
+namespace AyanamisTower.NihilEx.ECS.Events;
+
+/// <summary>
+/// Represents a mouse motion event with information about the mouse state and position.
+/// </summary>
+/// <param name="MouseState">The state of the mouse buttons.</param>
+/// <param name="X">The X coordinate of the mouse cursor.</param>
+/// <param name="Y">The Y coordinate of the mouse cursor.</param>
+/// <param name="XRel">The relative motion in the X direction.</param>
+/// <param name="YRel">The relative motion in the Y direction.</param>
+public record struct MouseMotionEvent(MouseButtonFlags MouseState, float X, float Y, float XRel, float YRel);
