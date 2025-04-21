864d77e959d789d32c7d1094f6804c7e468f3434
Ayanami Kaine <patrick.grohs@hhu.de>
2025-04-21 19:16:08 +0200

    feat: Implement KeyUpEvent structure to handle keyboard key release events

---
+++ Applications/NihilEx/ECS/Events/KeyUpEvent.cs
@@ -0,0 +1,11 @@
+using System;
+using SDL3;
+
+namespace AyanamisTower.NihilEx.ECS.Events;
+
+/// <summary>
+/// Represents a key up event triggered when a keyboard key is released.
+/// </summary>
+/// <param name="Keycode">The code of the released key.</param>
+/// <param name="Modifiers">The modifier keys active during the event.</param>
+public record struct KeyUpEvent(SDL.Keycode Keycode, SDL.Keymod Modifiers);
