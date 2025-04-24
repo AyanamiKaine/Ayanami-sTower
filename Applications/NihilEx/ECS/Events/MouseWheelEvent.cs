5f168092551368749c00cf7bbcb8fc60d829a4b5
Ayanami Kaine <ayanami@ayanamis-codex.net>
2025-04-24 10:43:56 +0200

    refactor: Improve formatting of MouseWheelEvent for better readability

--- Applications/NihilEx/ECS/Events/MouseWheelEvent.cs
+++ Applications/NihilEx/ECS/Events/MouseWheelEvent.cs
@@ -9,4 +9,8 @@ namespace AyanamisTower.NihilEx.ECS.Events;
 /// <param name="ScrollX">The horizontal scroll amount.</param>
 /// <param name="ScrollY">The vertical scroll amount.</param>
 /// <param name="Direction">The direction of the mouse wheel movement.</param>
-public record struct MouseWheelEvent(float ScrollX, float ScrollY, SDL_MouseWheelDirection Direction);
+public record struct MouseWheelEvent(
+    float ScrollX,
+    float ScrollY,
+    SDL_MouseWheelDirection Direction
+);
