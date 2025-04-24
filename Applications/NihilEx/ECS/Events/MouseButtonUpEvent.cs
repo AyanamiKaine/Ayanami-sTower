1f836b39d630ef4ed44cf70ef5ac7f80484a2adf
Ayanami Kaine <ayanami@ayanamis-codex.net>
2025-04-24 10:43:49 +0200

    refactor: Update MouseButtonUpEvent to use MouseButton type for improved clarity

--- Applications/NihilEx/ECS/Events/MouseButtonUpEvent.cs
+++ Applications/NihilEx/ECS/Events/MouseButtonUpEvent.cs
@@ -1,4 +1,5 @@
 using System;
+using AyanamisTower.NihilEx.SDLWrapper;
 using static SDL3.SDL;
 
 namespace AyanamisTower.NihilEx.ECS.Events;
@@ -10,4 +11,4 @@ namespace AyanamisTower.NihilEx.ECS.Events;
 /// <param name="X">The X coordinate where the mouse button was pressed.</param>
 /// <param name="Y">The Y coordinate where the mouse button was pressed.</param>
 /// <param name="Clicks">The number of clicks (1 for single-click, 2 for double-click, etc).</param>
-public record struct MouseButtonUpEvent(SDL_MouseButtonFlags MouseButton, float X, float Y, byte Clicks);
+public record struct MouseButtonUpEvent(MouseButton MouseButton, float X, float Y, byte Clicks);
