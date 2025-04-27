50bc4700fa9b77c1775f559c77c46d718c835ffc
Ayanami Kaine <patrick.grohs@hhu.de>
2025-04-28 00:36:31 +0200

    refactor: Comment out unused vertex shader loading code for clarity

--- Example/SDLWrapper.GPUExamples/BasicTriangle/Program.cs
+++ Example/SDLWrapper.GPUExamples/BasicTriangle/Program.cs
@@ -47,6 +47,8 @@ public static class Program
 
         Console.WriteLine($"Created GPU device with driver: {gpuDevice.DriverName}");
 
+        /*
+
         GpuShader vertexShader = GpuShader.LoadShader(
             gpuDevice,
             "Shader/RawTriangle.vert",
@@ -55,6 +57,9 @@ public static class Program
             0,
             0
         );
+        
+        */
+        GpuShader vertexShader = GpuShader.CompileAndLoadShader(gpuDevice, "RawTriangle.vert.hlsl");
 
         GpuShader fragmentShader = GpuShader.LoadShader(
             gpuDevice,
