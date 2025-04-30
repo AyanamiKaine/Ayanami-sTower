03fad1f132978f450091fbefaef208e908fc4cc3
Ayanami Kaine <patrick.grohs@hhu.de>
2025-04-30 17:04:29 +0200

    feat: Add HelloWorld example project with basic script interface and implementation

---
+++ Example/ScriptsExamples/BasicHelloExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using CSScriptLib;
+
+/// <summary>
+/// Examples interface for a script to implement
+/// </summary>
+public interface ICalc
+{
+    /// <summary>
+    /// Adds two numbers
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    int Add(int a, int b);
+}
+
+internal static class Program
+{
+    private static void Main()
+    {
+        CSScript.EvaluatorConfig.Engine = EvaluatorEngine.CodeDom;
+
+        ICalc script = (ICalc)
+            CSScript.Evaluator.LoadCode(
+                @"using System;
+                                          public class Script : ICalc
+                                          {
+                                              public int Add(int a, int b)
+                                              {
+                                                  return a + b;
+                                              }
+                                          }"
+            );
+        int result = script.Add(1, 2);
+
+        Console.WriteLine(result);
+    }
+}
