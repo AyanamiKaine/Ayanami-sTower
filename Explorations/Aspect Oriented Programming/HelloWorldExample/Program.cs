ac29cf7ba8a514cc839ec09850fc365da885e8ab
Ayanami Kaine <patrick.grohs@hhu.de>
2025-05-11 14:15:34 +0200

    feat: Add Aspect Oriented Programming exploration with initial examples and README

---
+++ Explorations/Aspect Oriented Programming/HelloWorldExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using Metalama.Patterns.Contracts;
+
+namespace HelloWorldExample;
+
+/// <summary>
+/// Showing the usage of contracts
+/// </summary>
+public class Customer
+{
+    /// <summary>
+    /// Here we define a contract that says the BirthYear can only be
+    /// between 1900 and 2100
+    /// </summary>
+    [Range(1900, 2100)]
+    public int BirthYear { get; set; }
+}
+
+/// <summary>
+/// Program
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// main entry
+    /// </summary>
+    public static void Main()
+    {
+        try
+        {
+            var tom = new Customer()
+            {
+                BirthYear = 200 // WILL THROW
+            };
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        try
+        {
+            var tom = new Customer()
+            {
+                BirthYear = 2000 // WONT THROW
+            };
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ Explorations/Aspect Oriented Programming/README.md
@@ -0,0 +1,5 @@
+# Aspect Oriented Programming with [meta lama](https://metalama.net/)
+
+## Notes
+
+Aspect Oriented Programming comes with some goodie like type constrains (contracts) that I always wished for to be simply included into C# with annotations. Meta Lama provides these in the box. Using many or even all features of it seems way to overkill. But I think there are some features that I wish to always include as an option. Here we are exploring the idea of Aspect Oriented Programming becomming a main part of my C# coding style.
