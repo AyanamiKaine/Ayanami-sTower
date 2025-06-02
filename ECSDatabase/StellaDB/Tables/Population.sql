6475eb70da5091a7c658b07a45862353bb418e16
Ayanami Kaine <patrick.grohs@hhu.de>
2025-06-02 23:10:31 +0200

    adding three new common fields to the population table

--- ECSDatabase/StellaDB/Tables/Population.sql
+++ ECSDatabase/StellaDB/Tables/Population.sql
@@ -1,4 +1,7 @@
 CREATE TABLE IF NOT EXISTS Population (
     EntityId INTEGER NOT NULL UNIQUE, 
+    consciousness REAL DEFAULT 0.0,         -- Political awareness (0-10)
+    militancy REAL DEFAULT 0.0,             -- Revolutionary tendency (0-10)
+    literacy REAL DEFAULT 0.0,              -- Education level (0-1)
     FOREIGN KEY (EntityId) REFERENCES Entity(Id) ON DELETE CASCADE
 );
