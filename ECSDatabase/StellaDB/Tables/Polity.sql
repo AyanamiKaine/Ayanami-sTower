0be4f29354ff66f664ff8534e26062d88f15acef
Ayanami Kaine <patrick.grohs@hhu.de>
2025-06-02 16:11:17 +0200

    Adding a seat of power locationid where the main core of the polity resides.

--- ECSDatabase/StellaDB/Tables/Polity.sql
+++ ECSDatabase/StellaDB/Tables/Polity.sql
@@ -4,7 +4,9 @@
 
 CREATE TABLE IF NOT EXISTS Polity (
     EntityId INTEGER NOT NULL UNIQUE, 
+    SeatOfPowerLocationID INTEGER,     -- Foreign Key to a entity that represents a location.
     Abbreviation VARCHAR(50),          -- e.g., 'GER', 'USA', 'CIS', 'FO'
     LeaderTitle VARCHAR(500), -- e.g., 'Emperor', 'Chancellor', 'Supreme Leader', 'Grand Moff', 'Vigo', 'Ascendant', "Council of Justice"
     FOREIGN KEY (EntityId) REFERENCES Entity(Id) ON DELETE CASCADE
+    FOREIGN KEY (EntityId) REFERENCES Entity(Id)
 );
