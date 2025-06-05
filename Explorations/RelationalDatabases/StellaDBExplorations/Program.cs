756b23ef1a6afd743d7b8a9c7777af6fd0c195a0
Ayanami Kaine <patrick.grohs@hhu.de>
2025-06-05 10:26:16 +0200

    explore more

--- Explorations/RelationalDatabases/StellaDBExplorations/Program.cs
+++ Explorations/RelationalDatabases/StellaDBExplorations/Program.cs
@@ -8,9 +8,16 @@ using Entity = AyanamisTower.StellaDB.Entity;
 List<Entity> entities = [];
 List<long> entityIds = [];
 
+if (File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Exploration.db")))
+    File.Delete(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Exploration.db"));
+
 var world = new World("Exploration", inMemory: false, enabledOptimizations: true);
 
-int numberOfRuns = 1;
+/*
+The query will determine the performance, bad query == bad performance!
+*/
+
+int numberOfRuns = 10;
 int numberOfEntities = 10000;
 using (var scope = new TransactionScope())
 {
@@ -39,19 +46,19 @@ Console.WriteLine($"One Query Total Time: {stopWatch.ElapsedMilliseconds} ms");
 Console.WriteLine($"One Query Average: {(double)stopWatch.ElapsedTicks / numberOfRuns / TimeSpan.TicksPerMicrosecond:F2} μs per operation");
 
 // Test 2: Each iteration in its own transaction
-stopWatch.Restart();
+var stopWatch2 = Stopwatch.StartNew();
+
 using var scope2 = new TransactionScope();
 
 for (int i = 0; i < numberOfRuns; i++)
 {
-
     foreach (var entity in entities)
     {
         entity.Update<Age>(new() { Value = 10 + i });
     }
 }
-stopWatch.Stop();
+stopWatch2.Stop();
 scope2.Complete();
 
-Console.WriteLine($"Many Queries Total Time: {stopWatch.ElapsedMilliseconds} ms");
-Console.WriteLine($"Many Queries Average: {(double)stopWatch.ElapsedTicks / numberOfRuns / TimeSpan.TicksPerMicrosecond:F2} μs per operation");
+Console.WriteLine($"Many Queries Total Time: {stopWatch2.ElapsedMilliseconds} ms");
+Console.WriteLine($"Many Queries Average: {(double)stopWatch2.ElapsedTicks / numberOfRuns / TimeSpan.TicksPerMicrosecond:F2} μs per operation");
