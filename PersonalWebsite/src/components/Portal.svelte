3fc9065c00b91f959c4af0d1a27edc21ff9883b2
Ayanami Kaine <patrick.grohs@hhu.de>
2025-07-21 13:15:52 +0200

    feat: add Portal component for dynamic DOM manipulation

---
+++ PersonalWebsite/src/components/Portal.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let element;
+
+	onMount(() => {
+		document.body.appendChild(element);
+
+		return () => {
+			if (element.parentNode === document.body) {
+				document.body.removeChild(element);
+			}
+		};
+	});
+</script>
+
+<div bind:this={element}>
+	<slot />
+</div>
