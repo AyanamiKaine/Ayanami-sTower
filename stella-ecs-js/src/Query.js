6882ce8c051291382dede59e00caba5cb88660cc
Ayanami Kaine <patrick.grohs@hhu.de>
2025-07-30 16:40:55 +0200

    adding comment

--- stella-ecs-js/src/Query.js
+++ stella-ecs-js/src/Query.js
@@ -6,5 +6,7 @@ export class Query {
     with(componentClass) {}
     without(componentClass) {}
     optional(componentClass) {}
+    where(predicate) { }
+    // When we build a query it should return an enumerable object where we can get the entity and the individual components (entity, component1, component2, ...)
     build() {}
 }
