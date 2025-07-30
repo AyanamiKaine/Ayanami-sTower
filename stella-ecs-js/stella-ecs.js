670c398f4e06ac377c88e01b8508d2091c79c7d3
Ayanami Kaine <patrick.grohs@hhu.de>
2025-07-30 17:05:58 +0200

    exporting the public symbols

---
+++ stella-ecs-js/stella-ecs.js
@@ -0,0 +1,5 @@
+export { ComponentStorage } from "./src/ComponentStorage";
+export { Entity } from "./src/Entity";
+export { Query } from "./src/Query";
+export { SparsedSet } from "./src/SparsedSet";
+export { World } from "./src/World";
