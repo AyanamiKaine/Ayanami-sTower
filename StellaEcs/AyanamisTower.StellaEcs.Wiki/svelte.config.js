bd4de64c76f4fc12ae783a2291534d6bcb16e928
Ayanami Kaine <patrick.grohs@hhu.de>
2025-08-30 10:28:13 +0200

    feat: Add MDX and Svelte integrations to Svelte configuration

--- StellaEcs/AyanamisTower.StellaEcs.Wiki/svelte.config.js
+++ StellaEcs/AyanamisTower.StellaEcs.Wiki/svelte.config.js
@@ -1,5 +1,8 @@
 import { vitePreprocess } from '@astrojs/svelte';
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
 
 export default {
-	preprocess: vitePreprocess(),
-}
+    preprocess: vitePreprocess(),
+    integrations: [mdx(), svelte()],
+};
