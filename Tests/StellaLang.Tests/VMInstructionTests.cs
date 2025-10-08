6bbbf5ded3434c03e5b1fad6ba186af8bca98ff0
Ayanami Kaine <patrick.grohs@hhu.de>
2025-10-08 15:56:07 +0200

    Removing junk init new version.

---
+++ Tests/StellaLang.Tests/VMInstructionTests.cs
@@ -0,0 +1,10 @@
+namespace StellaLang.Tests;
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+/// <summary>
+/// Here we are testing the instructions of the VM
+/// </summary>
+public class VMInstructionTests
+{
+
+}
