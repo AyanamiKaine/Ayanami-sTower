4363df990f3564f7ccf95b1d1947392eac4e0c7d
Ayanami Kaine <patrick.grohs@hhu.de>
2025-10-09 21:51:54 +0200

    test: Add unit tests for VM instructions including PushCell, PushDouble, DUP, SWAP, and others

--- Tests/StellaLang.Tests/VMInstructionTests.cs
+++ Tests/StellaLang.Tests/VMInstructionTests.cs
@@ -7,25 +7,6 @@ namespace StellaLang.Tests;
 public class VMInstructionTests
 {
     [Fact]
-    public void AddVMInstructionTest()
-    {
-        var vm = new VM();
-
-        var code = new CodeBuilder()
-            .PushCell(42)
-            .PushCell(100)
-            .Add()
-            .Build();
-
-        vm.Execute(code);
-
-        var currentTopStackValue = vm.DataStack.PeekLong();
-        const long expectedTopValue = 142;
-
-        Assert.Equal(expectedTopValue, currentTopStackValue);
-    }
-
-    [Fact]
     public void PushCellVMInstructionTest()
     {
         var vm = new VM();
@@ -87,4 +68,186 @@ public class VMInstructionTests
         Assert.Equal(expectedTopValue, currentTopStackValue);
     }
 
+    [Fact]
+    public void SWAPVMInstructionTest()
+    {
+        var vm = new VM();
+
+        var code = new CodeBuilder()
+            .PushCell(100)
+            .PushCell(141)
+            .Swap()
+            .Build();
+
+        vm.Execute(code);
+
+        var currentTopStackValue = vm.DataStack.PeekLong();
+        long expectedTopValue = 100;
+
+        Assert.Equal(expectedTopValue, currentTopStackValue);
+
+        // After duplication, the second value should also be 100
+        vm.Execute(new CodeBuilder()
+            .Drop()
+            .Build());
+
+        expectedTopValue = 141;
+        currentTopStackValue = vm.DataStack.PeekLong();
+
+        Assert.Equal(expectedTopValue, currentTopStackValue);
+    }
+
+    [Fact]
+    public void AddVMInstructionTest()
+    {
+        var vm = new VM();
+
+        var code = new CodeBuilder()
+            .PushCell(42)
+            .PushCell(100)
+            .Add()
+            .Build();
+
+        vm.Execute(code);
+
+        var currentTopStackValue = vm.DataStack.PeekLong();
+        const long expectedTopValue = 142;
+
+        Assert.Equal(expectedTopValue, currentTopStackValue);
+    }
+
+    [Fact]
+    public void SUBVMInstructionTest()
+    {
+        var vm = new VM();
+
+        var code = new CodeBuilder()
+            .PushCell(42)
+            .PushCell(100)
+            .Sub()
+            .Build();
+
+        vm.Execute(code);
+
+        var currentTopStackValue = vm.DataStack.PeekLong();
+        const long expectedTopValue = -58;
+
+        Assert.Equal(expectedTopValue, currentTopStackValue);
+    }
+
+    [Fact]
+    public void MULVMInstructionTest()
+    {
+        var vm = new VM();
+
+        var code = new CodeBuilder()
+            .PushCell(42)
+            .PushCell(100)
+            .Mul()
+            .Build();
+
+        vm.Execute(code);
+
+        var currentTopStackValue = vm.DataStack.PeekLong();
+        const long expectedTopValue = 4200;
+
+        Assert.Equal(expectedTopValue, currentTopStackValue);
+    }
+
+    [Fact]
+    public void FAddVMInstructionTest()
+    {
+        var vm = new VM();
+
+        var code = new CodeBuilder()
+            .FPushDouble(42.5)
+            .FPushDouble(100.0)
+            .FAdd()
+            .Build();
+
+        vm.Execute(code);
+
+        var currentTopStackValue = vm.FloatStack.PeekDouble();
+        const double expectedTopValue = 142.5;
+
+        Assert.Equal(expectedTopValue, currentTopStackValue);
+    }
+
+    [Fact]
+    public void FMulVMInstructionTest()
+    {
+        var vm = new VM();
+
+        var code = new CodeBuilder()
+            .FPushDouble(42.5)
+            .FPushDouble(100.0)
+            .FMul()
+            .Build();
+
+        vm.Execute(code);
+
+        var currentTopStackValue = vm.FloatStack.PeekDouble();
+        const double expectedTopValue = 4250.0;
+
+        Assert.Equal(expectedTopValue, currentTopStackValue);
+    }
+
+    [Fact]
+    public void NEGVMInstructionTest()
+    {
+        var vm = new VM();
+
+        var code = new CodeBuilder()
+            .PushCell(42)
+            .Neg()
+            .Build();
+
+        vm.Execute(code);
+
+        var currentTopStackValue = vm.DataStack.PeekLong();
+        const long expectedTopValue = -42;
+
+        Assert.Equal(expectedTopValue, currentTopStackValue);
+    }
+
+
+    [Fact]
+    public void EQVMInstructionTest()
+    {
+        var vm = new VM();
+
+        var code = new CodeBuilder()
+            .PushCell(42)
+            .PushCell(42)
+            .Eq()
+            .Build();
+
+        vm.Execute(code);
+
+        var currentTopStackValue = vm.DataStack.PeekLong();
+        const long expectedTopValue = 1;
+
+        Assert.Equal(expectedTopValue, currentTopStackValue);
+    }
+
+    [Fact]
+    public void STOREFETCHVMInstructionTest()
+    {
+        var vm = new VM();
+
+        var code = new CodeBuilder()
+            .PushCell(1234)      // Value to store
+            .PushCell(0)         // Address to store at
+            .Store()             // STORE instruction
+            .PushCell(0)         // Address to fetch from
+            .Fetch()             // FETCH instruction
+            .Build();
+
+        vm.Execute(code);
+
+        var currentTopStackValue = vm.DataStack.PeekLong();
+        const long expectedTopValue = 1234;
+
+        Assert.Equal(expectedTopValue, currentTopStackValue);
+    }
 }
