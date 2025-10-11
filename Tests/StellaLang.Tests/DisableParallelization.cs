61351f1945a7600b5da43fdeaf74adea091542f3
Ayanami Kaine <patrick.grohs@hhu.de>
2025-10-11 18:36:52 +0200

    fix: Disable xUnit parallelization to prevent intermittent test failures

--- Tests/StellaLang.Tests/DisableParallelization.cs
+++ Tests/StellaLang.Tests/DisableParallelization.cs
@@ -1,6 +1,5 @@
 using Xunit;
 
 // Disable xUnit parallelization for the entire test assembly.
-// Many tests redirect Console.Out and Console.SetOut; running tests in parallel
-// can cause intermittent failures when multiple tests attempt to capture output.
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
+// If many tests fail unpredictably, consider disable parallelization again.
+[assembly: CollectionBehavior(DisableTestParallelization = false)]
