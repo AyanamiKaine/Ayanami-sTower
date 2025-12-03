66e825e0fb1054943896dd7d71dd3eea774fee24
Ayanami Kaine <patrick.grohs@hhu.de>
2025-12-03 15:20:01 +0100

    feat: Initialize HierarchicalTaskNetworkEx project with basic structure and documentation

---
+++ hierarchical_task_network_ex/lib/hierarchical_task_network_ex.ex
@@ -0,0 +1,18 @@
+defmodule HierarchicalTaskNetworkEx do
+  @moduledoc """
+  Documentation for `HierarchicalTaskNetworkEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HierarchicalTaskNetworkEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ hierarchical_task_network_ex/test/hierarchical_task_network_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule HierarchicalTaskNetworkExTest do
+  use ExUnit.Case
+  doctest HierarchicalTaskNetworkEx
+
+  test "greets the world" do
+    assert HierarchicalTaskNetworkEx.hello() == :world
+  end
+end
